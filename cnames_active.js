e7249bbaca38c1f2374796277e703317ae86fe4a
indus <stefan.keim@posteo.de>
2015-04-08 05:22:52 +0200

    cname lists

---
+++ cnames_active.js
@@ -0,0 +1,26 @@
+﻿/*
+ ***** ACTIVE SUBDOMAINS
+ * ***********************
+ * 
+ * for the moment the following subdomains are active
+ * file a pull request to add your own GitHub Page
+ * 
+ * 
+ **** EXPLANATION
+ * ****************
+ * 
+ * KEY:         The subdomain you request (e.g.: "foo" would give the custom domain "foo.js.org" ).
+ * 
+ * VALUE:       The regular domain provided by GitHub (e.g.: "foo.github.io" for a User-/Organisation Page or "foo.github.io/bar" for a Project Page).
+ * 
+ * IMPORTANT:   To authorise yourself as the owner of the GitHub Page you must have a valid CNAME file (with the requestd domain, e.g: "foo.js.org") to your repository before the pull request
+ *              The requested subdomain must match either your GitHub username or the name of your project repository. Execeptions are possible for the sake of clarity (e.g. if your project has the name "foojs" you can request "foo").
+ *              Before you start a request for your personal fork of "jquery" or "angular" you should read the section "naming conflicts" in the wiki!
+ * 
+ * NOTICE:      It would be nice if you could keep the alphabetical order.
+ * 
+ */
+
+var cnames_active = {
+    "dns": "js-org.github.io/dns"
+}
