6c42c13936ef11932194eb602ed64cb58fb6e4e6
Prof Gra <christophegragnic@gmail.com>
2015-04-14 22:36:24 +0200

    Wrap instructions in comments

--- cnames_active.js
+++ cnames_active.js
@@ -11,11 +11,16 @@
  *
  * KEY:         The subdomain you request (e.g.: "foo" would give the custom domain "foo.js.org" ).
  *
- * VALUE:       The regular domain provided by GitHub (e.g.: "foo.github.io" for a User-/Organisation Page or "foo.github.io/bar" for a Project Page).
+ * VALUE:       The regular domain provided by GitHub (e.g.: "foo.github.io" for a User-/Organisation Page
+ *              or "foo.github.io/bar" for a Project Page).
  *
- * IMPORTANT:   To authorise yourself as the owner of the GitHub Page you must have a valid CNAME file (with the requestd domain, e.g: "foo.js.org") to your repository before the pull request
- *              The requested subdomain must match either your GitHub username or the name of your project repository. Execeptions are possible for the sake of clarity (e.g. if your project has the name "foojs" you can request "foo").
- *              Before you start a request for your personal fork of "jquery" or "angular" you should read the section "naming conflicts" in the wiki!
+ * IMPORTANT:   To authorise yourself as the owner of the GitHub Page you must have a valid CNAME file
+ *              (with the requestd domain, e.g: "foo.js.org") to your repository before the pull request.
+ *              The requested subdomain must match either your GitHub username or the name of your project
+ *              repository. Execeptions are possible for the sake of clarity (e.g. if your project has the
+ *              name "foojs" you can request "foo").
+ *              Before you start a request for your personal fork of "jquery" or "angular" you should read
+ *              the section "naming conflicts" in the wiki!
  *
  * NOTICE:      It would be nice if you could keep the alphabetical order.
  *
