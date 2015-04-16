61daeb9d11fda117b026bed0832504f29a740d5d
Joshua Galvin <joshuagalvin@gmail.com>
2015-04-16 11:28:36 -0700

    Added college quest.

--- cnames_active.js
+++ cnames_active.js
@@ -32,6 +32,7 @@ var cnames_active = {
     "alt": "goatslacker.github.io/alt/"
     , "anenth": "anenth.github.io"
     , "bogdanteodoru": "bogdanteodoru.github.io"
+    , "collegequest": "schoolhouserock.github.io/SchoolHouseRock"
     , "cordova-multiplatform-template": "ckgrafico.github.io/Cordova-Multiplatform-Template"
     , "dns": "js-org.github.io/dns"
     , "emulisp": "grahack.github.io/EmuLisp"
