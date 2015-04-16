858579d266f57e770933d0545803d8c7e85b2baa
Vincenzo Chianese <vincenz.chianese@yahoo.it>
2015-04-16 22:39:49 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -62,7 +62,7 @@ var cnames_active = {
     , "sugarshin": "sugarshin.github.io/sugarshin.js.org"
     , "tagster": "goschevski.github.io/tagster"
     , "timerizer": "callumacrae.github.io/timerizerJS"
-    , "vncz": "xvincentx.github.io"
+    , "vncz": "xvincentx.github.io/vncz/"
     , "weaver": "maxkfranz.github.io/weaver"
     , "xto6": "mohebifar.github.io/xto6"
     , "zodiac": "indus.github.io/Zodiac"
