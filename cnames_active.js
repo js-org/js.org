7f9e2ba88bd293b31ed633b5df634cbf3c666578
Jacob Zimmerman <jczimm.alias@gmail.com>
2015-04-17 13:13:12 -0400

    add omega.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -100,4 +100,5 @@ var cnames_active = {
     , "xto6": "mohebifar.github.io/xto6"
     , "zodiac": "indus.github.io/Zodiac"
     , "zombie": "assaf.github.io/zombie"
+    , "omega": "jczimm.github.io/omega"
 }
