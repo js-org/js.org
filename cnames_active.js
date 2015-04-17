60a2c515d9098c2366a4b2b261e8f6fcbe9775d0
Chy59 <mickael.blondeau59@gmail.com>
2015-04-17 11:20:33 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -80,4 +80,5 @@ var cnames_active = {
     , "xto6": "mohebifar.github.io/xto6"
     , "zodiac": "indus.github.io/Zodiac"
     , "zombie": "assaf.github.io/zombie"
+    , "chy": "chy59.github.io/chy"
 }
