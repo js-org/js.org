25af4901549e21f8fdf9b887346ebf9d428d1239
Matthew Mueller <mattmuelle@gmail.com>
2015-04-17 00:47:36 -0700

    added: cheerio

--- cnames_active.js
+++ cnames_active.js
@@ -34,6 +34,7 @@ var cnames_active = {
     , "alt": "goatslacker.github.io/alt"
     , "anenth": "anenth.github.io"
     , "bogdanteodoru": "bogdanteodoru.github.io"
+    , "cheerio": "cheeriojs.github.io/cheerio",
     , "collegequest": "schoolhouserock.github.io/CollegeQuest"
     , "cordova-multiplatform-template": "ckgrafico.github.io/Cordova-Multiplatform-Template"
     , "cqrs": "adrai.github.io/cqrs"
