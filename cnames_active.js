6a481758799d531650c0b210dcf4a5b8722a29f3
Rahmat Budiharso <rbudiharso@gmail.com>
2015-04-21 13:40:38 +0700

    add jakarta.js.org subdomain

--- cnames_active.js
+++ cnames_active.js
@@ -69,6 +69,7 @@ var cnames_active = {
     , "happy": "e24.github.io/happy"
     , "hello": "hello-js-org.github.io"
     , "ivml": "influencetech.github.io/ivml"
+    , "jakarta": "jakartajs.github.io/jakartajs"
     , "jbone": "kupriyanenko.github.io/jbone"
     , "jjlc": "k-yak.github.io/JJLC"
     , "jodytate": "jodytate.github.io"
