b697b5bb4927f7eb5234a4320011bad2a8ea200d
JonRiv <jonathan.rivalan@gmail.com>
2015-04-24 15:49:58 +0200

    added jrivalan CNAME
    Hello,
    
    I'm sorry for the previous pull request, this time it should be ok. Thank you very much !

--- cnames_active.js
+++ cnames_active.js
@@ -1,32 +1,3 @@
-/*
- ***** ACTIVE SUBDOMAINS
- * ***********************
- *
- * for the moment the following subdomains are active
- * file a pull request to add your own GitHub Page
- *
- *
- **** EXPLANATION
- * ****************
- *
- * KEY:         The subdomain you request (e.g.: "foo" would give the custom domain "foo.js.org" ).
- *
- * VALUE:       The regular domain provided by GitHub (e.g.: "foo.github.io" for a User-/Organisation Page
- *              or "foo.github.io/bar" for a Project Page).
- *
- * IMPORTANT:   To authorise yourself as the owner of the GitHub Page you must have a valid CNAME file
- *              (with the requestd domain, e.g: "foo.js.org") to your repository before the pull request.
- *              The requested subdomain must match either your GitHub username or the name of your project
- *              repository. Execeptions are possible for the sake of clarity (e.g. if your project has the
- *              name "foojs" you can request "foo").
- *              Before you start a request for your personal fork of "jquery" or "angular" you should read
- *              the section "naming conflicts" in the wiki!
- *
- * NOTICE:      It would be nice if you could keep the alphabetical order.
- *
- *              Because of the risk of abuse I´m no longer merging requests for pages without content!
- *
- */
 var cnames_active = {
     "3bola": "3bola.github.io"
     , "akashacms": "akashacms.github.io"
@@ -77,6 +48,7 @@ var cnames_active = {
     , "jbone": "kupriyanenko.github.io/jbone"
     , "jjlc": "k-yak.github.io/JJLC"
     , "jodytate": "jodytate.github.io"
+    , "jrivalan": "jonriv.github.io/jrivalan/"
     , "json-schema-faker": "pateketrueke.github.io/json-schema-faker"
     , "labelauty": "fntneves.github.io/jquery-labelauty"
     , "laubstein": "laubstein.github.io"
