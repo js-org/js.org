1bdeec2eeea774c3171e1c2b694a02be4b198132
Kevin Lamshöft <Gheek@users.noreply.github.com>
2015-05-05 10:49:15 +0200

    hyde.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -76,6 +76,7 @@ var cnames_active = {
     , "gully": "nmabhinandan.github.io/gully"
     , "happy": "e24.github.io/happy"
     , "hello": "hello-js-org.github.io"
+    , "hyde": "gheek.github.io/hyde"
     , "ivml": "influencetech.github.io/ivml"
     , "jakarta": "jakartajs.github.io/jakartajs"
     , "jakejarrett": "jakejarrett.github.io"
