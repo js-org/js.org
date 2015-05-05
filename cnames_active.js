39848628a9699bb0cfefe3cccd0495077f9be2e1
Waiter Zen <waiter.james1991@gmail.com>
2015-05-05 09:54:45 -0700

    add my subdomain

--- cnames_active.js
+++ cnames_active.js
@@ -168,4 +168,6 @@ var cnames_active = {
     , "youtim": "mortonfox.github.io/YouTim"
     , "zodiac": "indus.github.io/Zodiac"
     , "zombie": "assaf.github.io/zombie"
+    , "waiter": "waiterzen.github.io/waiter"
+    , "waiterzen":"waiterzen.github.io"
 }
