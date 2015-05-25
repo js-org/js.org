4af21e1374ec4326956f750bf7cbc0b4e2585196
Scott King <kingsc850@gmail.com>
2015-05-25 19:27:03 -0400

    added my domain

--- cnames_active.js
+++ cnames_active.js
@@ -51,7 +51,7 @@ var cnames_active = {
     , "crunch": "vukicevic.github.io/crunch"
     , "date": "matthewmueller.github.io/date"
     , "dns": "js-org.github.io/dns"
-    , "dragon-slayer:" "kingscott.github.io/dragon-slayer"
+    , "dragonslayer:" "kingscott.github.io/dragon-slayer"
     , "elliot": "elliotboney.github.io"
     , "emulisp": "grahack.github.io/EmuLisp"
     , "euclid": "anandthakker.github.io/euclid"
