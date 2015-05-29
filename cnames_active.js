6f31ad96d52f624f550d482ad8631f4dde962092
Shuo Zheng <shuo.s.zheng@gmail.com>
2015-05-29 16:36:51 -0400

    added iio.js

--- cnames_active.js
+++ cnames_active.js
@@ -88,6 +88,7 @@ var cnames_active = {
     , "hello": "hello-js-org.github.io"
     , "hooloo": "hooloo.github.io"
     , "hyde": "gheek.github.io/hyde"
+    , "iio": "iioinc.github.io/iio.js"
     , "is": "arasatasaygin.github.io/is.js"
     , "ivml": "influencetech.github.io/ivml"
     , "j2p": "j2p.github.io"
