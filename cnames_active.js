f010c39b7012e83187743d4b1826980c27655258
Mei Weng Brough-Smyth <mei.broughsmyth@student.qut.edu.au>
2015-05-31 02:13:41 +1000

    add themapmaker.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -196,4 +196,5 @@ var cnames_active = {
     , "youtim": "mortonfox.github.io/YouTim"
     , "zodiac": "indus.github.io/Zodiac"
     , "zombie": "assaf.github.io/zombie"
+    , "themapmaker": "rhokaustralia.github.io/melb-maker-map/"
 }
