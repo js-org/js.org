71f5d2e9c38251486df0026d95875d41c1183fc6
Karl <karl.coelho1@gmail.com>
2015-05-30 19:17:53 +0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -102,6 +102,7 @@ var cnames_active = {
     , "juno-106": "stevengoldberg.github.io/juno-106"
     , "jvandyke": "jvandyke.github.io"
     , "kingdom": "clayendisk.github.io/kingdom.js"
+    , "karl": "karlcoelho.github.io/portfolio"
     , "labelauty": "fntneves.github.io/jquery-labelauty"
     , "laubstein": "laubstein.github.io"
     , "leandro": "leandrowd.github.io"
