91e61a152c97193f9684842e9c84944b532aa333
Jinjin Peng <jingsam@users.noreply.github.com>
2015-05-30 23:02:46 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -196,4 +196,5 @@ var cnames_active = {
     , "youtim": "mortonfox.github.io/YouTim"
     , "zodiac": "indus.github.io/Zodiac"
     , "zombie": "assaf.github.io/zombie"
+    , "jingsam": "jingsam.github.io"
 }
