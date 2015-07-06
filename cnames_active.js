9037959f5f67562acc24e60625e307c62728e6d0
Sergey Voloshin <voloshin1986@gmail.com>
2015-07-06 14:53:39 +0300

    Add voloshins.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -253,4 +253,5 @@ var cnames_active = {
     , "youtube-box": "lucasmonteverde.github.io/youtube-box"
     , "zodiac": "indus.github.io/Zodiac"
     , "zombie": "assaf.github.io/zombie"
+    , "voloshins": "voloshins.github.io"
 }
