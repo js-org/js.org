1502d6c427c5e75ced0cda2a04125f76eaeabca8
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-10-25 14:18:51 -0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -120,6 +120,7 @@ var cnames_active = {
     ,"frzr": "pakastin.github.io/frzr" //CF
     ,"gamedevcontestal": "fromdenisvieira.github.io/gamedevcontestal"
     ,"geekr": "ruanyl.github.io/geekr"
+    ,"getlinkjs": "getlinkjs.github.io"
     ,"ghsamm": "ghsamm.github.io"
     ,"giant-piano": "moroshko.github.io/giant-piano"
     ,"goodseller": "goodseller.github.io"
