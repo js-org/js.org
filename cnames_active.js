7af00af5e398f6f73bae8e7b415f8e2bcf9fc5c0
Deepu K Sasidharan <d4udts@gmail.com>
2016-01-23 10:41:31 +0800

    add sub domain `deepu`
    add sub domain `deepu` for deepu105.github.io

--- cnames_active.js
+++ cnames_active.js
@@ -97,6 +97,7 @@ var cnames_active = {
     ,"daysfromnow": "pedrokost.github.io/daysfromnow"
     ,"dbo": "z3ta.github.io/dbo"
     ,"deck-of-cards": "pakastin.github.io/deck-of-cards" //CF
+    ,"deepu": "deepu105.github.io"
     ,"delegacias-fortaleza": "juliosampaio.github.io/delegacias-fortaleza"
     ,"deputy": "ry4nolson.github.io/Deputy"
     ,"dgelong": "alexeyraspopov.github.io/dgelong"
