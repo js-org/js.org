d9b58aca445d223e894c934689e6789d3850a2ef
Fez Vrasta <federico.zivolo@gmail.com>
2016-04-06 14:42:00 +0200

    added Popper.js

--- cnames_active.js
+++ cnames_active.js
@@ -286,6 +286,7 @@ var cnames_active = {
     ,"planisphere": "dai-shi.github.io/planisphere-site"
     ,"pliers": "pliersjs.github.io"
     ,"polythene": "arthurclemens.github.io/Polythene"
+    ,"popper": "fezvrasta.github.io/popper.js"
     ,"qs": "kirjs.github.io/qs.js"
     ,"querybuilder": "mistic100.github.io/jQuery-QueryBuilder"
     ,"quiz-app": "odevlord.github.io/Quiz-App"
