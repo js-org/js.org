a8e3670cf697bf79085be3d5778ef5605288ea7d
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-05-06 22:47:25 +0200

    jargon.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -209,6 +209,7 @@ var cnames_active = {
     ,"jakejarrett": "jakejarrett.github.io"
     ,"jalbertbowden": "jalbertbowden.github.io"
     ,"james": "jamesrowen.github.io/james"
+    ,"jargon": "hugogiraudel.github.io/SJSJ"
     ,"jbone": "kupriyanenko.github.io/jbone"
     ,"jets": "nexts.github.io/Jets.js" //CF
     ,"jjlc": "k-yak.github.io/JJLC"
