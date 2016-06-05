86898fdc125795c139f9d904864a0aa2ad8a7376
Aleksandr Tymchenko <ast@anahoret.com>
2016-06-05 17:33:13 +0300

    tymchenkooleksandr

--- cnames_active.js
+++ cnames_active.js
@@ -434,6 +434,7 @@ var cnames_active = {
     ,"ts2jsdoc": "spatools.github.io/ts2jsdoc"
     ,"tsfp": "zhenwenc.github.io/tsfp"
     ,"tweetnacl": "dchest.github.io/tweetnacl-js" //CF
+    ,"tymchenkooleksandr": "tymchenkooleksandr.github.io" //CF
     ,"typeahead": "corejavascript.github.io/typeahead.js" //CF
     ,"u": "hanul.github.io/UJS"
     ,"udnisap": "udnisap.github.io" //CF
