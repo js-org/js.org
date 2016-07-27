bb65ffa4a94c39153f4d3648542cb0bda7cb1658
42ua <42ua@mail.ru>
2016-07-27 15:17:21 +0300

    grep.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -201,6 +201,7 @@ var cnames_active = {
     ,"graphics2d": "keyten.github.io/Graphics2D"
     ,"grapnel": "engineeringmode.github.io/Grapnel.js"
     ,"greg": "gregorydgarcia.github.io"
+    ,"grep": "42ua.github.io/grep"
     ,"gridiron": "noderaider.github.io/gridiron" //CF
     ,"gridsplit": "assetinfo.github.io"
     ,"gruft": "nikola.github.io/gruft"
