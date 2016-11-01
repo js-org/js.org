e9e632611db30bcc904d9e33819317535f03a907
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-02 01:07:32 +0530

    Added 100dayz

--- cnames_active.js
+++ cnames_active.js
@@ -36,6 +36,7 @@
 
 var cnames_active = {
     "": "js-org.github.io"
+    ,"100dayz": "deadcoder0904.github.io/100dayz"
     ,"101": "7anshuai.github.io/js101" //noCF? (don´t add this in a new PR)
     ,"7anshuai": "7anshuai.github.io" //noCF? (don´t add this in a new PR)
     ,"8art": "erayarslan.github.io/8art" //noCF? (don´t add this in a new PR)
