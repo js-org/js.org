2381e3ff6cdc966b99400985ec20f9f38226d0fc
Maxime Thirouin <m@moox.io>
2016-11-06 10:56:03 +0100

    Add react-toulouse.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -437,6 +437,7 @@ var cnames_active = {
     ,"react-responsive-carousel": "leandrowd.github.io/react-responsive-carousel" //noCF? (don´t add this in a new PR)
     ,"react-shared": "rvikmanis.github.io/react-shared" //noCF? (don´t add this in a new PR)
     ,"react-styleguidist": "sapegin.github.io/react-styleguidist" //noCF? (don´t add this in a new PR)
+    ,"react-toulouse": "moox.github.io/react-toulouse"
     ,"reactabular": "bebraw.github.io/reactabular"
     ,"reactdesktop": "gabrielbull.github.io/react-desktop" //noCF? (don´t add this in a new PR)
     ,"reader": "ruanyl.github.io/js-reader" //noCF? (don´t add this in a new PR)
