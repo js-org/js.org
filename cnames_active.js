a12a7bdff8b932f7946f11690b095a019f190e2f
Casper Beyer <caspervonb@icloud.com>
2016-12-11 03:36:20 +0800

    Remove thingamajig.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -569,7 +569,6 @@ var cnames_active = {
     ,"sunsistemo": "sunsistemo.github.io/sunsistemo"
     ,"supernova": "janbiasi.github.io/supernova" //noCF? (don´t add this in a new PR)
     ,"swichit": "dongryphon.github.io/switchit"
-    ,"thingamajig": "hasharray.github.io/thingamajig"
     ,"tagster": "goschevski.github.io/tagster" //noCF? (don´t add this in a new PR)
     ,"talker": "secondstreet.github.io/talker.js" //noCF? (don´t add this in a new PR)
     ,"tap-flux": "technologyadvice.github.io/tap-flux" //noCF? (don´t add this in a new PR)
