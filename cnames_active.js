15307e33bddb83c2d4a5d13d5038d89a3b11198f
Zach Rose <zach@zachrose.biz>
2017-01-26 07:39:08 -0800

    Add booth.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -93,6 +93,7 @@ var cnames_active = {
     ,"blogpage": "zanseven007.github.io/blog-page" //noCF
     ,"bodybuilder": "danpaz.github.io/bodybuilder" //noCF? (don´t add this in a new PR)
     ,"bombsweeper": "pakastin.github.io/bombsweeper"
+    ,"booth": "zachrose.github.io/booth"
     ,"bootstrap-confirmation": "mistic100.github.io/Bootstrap-Confirmation" //noCF? (don´t add this in a new PR)
     ,"bornaeon": "bornaeon.github.io" //noCF
     ,"box": "capacitorset.github.io/box-js" //noCF
