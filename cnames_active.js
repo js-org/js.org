f17ea4627b21e6c8c37b49fd8d4ca50fcad6377f
Eric Hao <hhao99@gmail.com>
2017-02-17 14:59:03 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -36,6 +36,7 @@
 
 var cnames_active = {
     "": "js-org.github.io"
+    ,"hhao99":"hhao99.github.io"
     ,"medit": "echosoar.github.io/medit"
     ,"100dayz": "deadcoder0904.github.io/100dayz"
     ,"101": "7anshuai.github.io/js101" //noCF? (don´t add this in a new PR)
