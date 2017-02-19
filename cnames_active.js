8143306d14e65c4246ab98712a49f52349506cfe
DJZhao <djzhao627@users.noreply.github.com>
2017-02-19 11:25:01 +0800

    djzhao.js.org
    add djzhao.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -720,4 +720,5 @@ var cnames_active = {
     ,"zazu": "zazujs.github.io" //noCF? (don´t add this in a new PR)
     ,"zodiac": "indus.github.io/Zodiac" //noCF? (don´t add this in a new PR)
     ,"zombie": "assaf.github.io/zombie" //noCF? (don´t add this in a new PR)
+    ,"djzhao": "djzhao627.github.io"
 }
