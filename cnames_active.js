aaace8c4d3593eabb4f7638044128efe761b77c1
David Taylor Jr <davidtaylorjr1985@gmail.com>
2017-02-26 17:45:36 -0500

    Add davidtaylorjr.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -162,6 +162,7 @@ var cnames_active = {
 ,"danmaku": "weizhenye.github.io/Danmaku"
 ,"danmol": "dan12mol.github.io" //noCF? (don´t add this in a new PR)
 ,"date": "matthewmueller.github.io/date" //noCF? (don´t add this in a new PR)
+,"davidtaylorjr": "davidtaylorjr.github.io"
 ,"daysfromnow": "pedrokost.github.io/daysfromnow" //noCF? (don´t add this in a new PR)
 ,"dbo": "z3ta.github.io/dbo" //noCF? (don´t add this in a new PR)
 ,"deck-of-cards": "pakastin.github.io/deck-of-cards"
