381557f0effba3c93fdf18cb257b397aa098c82c
Stefan Keim <indus@users.noreply.github.com>
2017-03-07 16:14:23 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -618,7 +618,7 @@ var cnames_active = {
 ,"slang": "ramadis.github.io/slang" //noCF? (don´t add this in a new PR)
 ,"slideout": "mango.github.io/slideout"
 ,"slides": "deadcoder0904.github.io/slides-official-website"
-,"slim": "eavichay.github.io/slim.js/"
+,"slim": "eavichay.github.io/slim.js"
 ,"smoove": "abemedia.github.io/jquery-smoove"
 ,"sn0w": "greensn0w.github.io" //noCF? (don´t add this in a new PR)
 ,"social-likes": "sapegin.github.io/social-likes" //noCF? (don´t add this in a new PR)
