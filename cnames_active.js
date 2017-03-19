3266fd67b7b6548c89d18ad7a6d865d4971db0e7
Integrator <alexander-shvets@users.noreply.github.com>
2017-03-19 16:36:49 +0200

    add flatui
    transpiling.github.io/svelte-flat-ui

--- cnames_active.js
+++ cnames_active.js
@@ -241,6 +241,7 @@ var cnames_active = {
 ,"firenze": "fahad19.github.io/firenze" //noCF? (don´t add this in a new PR)
 ,"fis": "fex-team.github.io/fis-site" //noCF? (don´t add this in a new PR)
 ,"five": "jackdcrawford.github.io/five"
+,"flatui": "transpiling.github.io/svelte-flat-ui"  
 ,"fld-grd": "mrksbnch.github.io/fld-grd"
 ,"flexmasonry": "kavanmevada.github.io/flex_masonry" //noCF
 ,"flowchart": "adrai.github.io/flowchart.js" //noCF? (don´t add this in a new PR)
