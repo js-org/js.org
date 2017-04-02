99c931858c61d7098a2a735f41e0d90197aa1cd5
Ben Pevsner <ivebencrazy@users.noreply.github.com>
2017-04-01 21:50:46 -0700

    Add `zuck.js.org`
    blanket-warriors.github.io/Zuck => zuck.js.org
    
    - There is reasonable content on the page: (see: No Content)
    ^ static files are in the docs folder of the project https://github.com/Blanket-Warriors/Zuck/tree/master/docs
     - I have read and accepted the ToS

--- cnames_active.js
+++ cnames_active.js
@@ -770,4 +770,5 @@ var cnames_active = {
 ,"zazu": "zazujs.github.io" //noCF? (don´t add this in a new PR)
 ,"zodiac": "indus.github.io/Zodiac" //noCF? (don´t add this in a new PR)
 ,"zombie": "assaf.github.io/zombie" //noCF? (don´t add this in a new PR)
+,"zuck": "blanket-warriors.github.io/Zuck"
 }
