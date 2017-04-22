dba1bd15a98bbd5f2b63472cf55274d48cfd1393
Yukino Song <syqlds@126.com>
2017-04-23 00:34:47 +0800

    Add ef.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -222,6 +222,7 @@ var cnames_active = {
 ,"dynamicsnode": "crisfervil.github.io/DynamicsNode"
 ,"eahmed234": "eahmed234.github.io"
 ,"ed2k": "sunnyli.github.io/ed2k.js"
+,"ef": "classicoldsong.github.io/ef.js.org"
 ,"effectful": "awto.github.io/effectfuljs"
 ,"elastic-builder": "sudo-suhas.github.io/elastic-builder" 
 ,"elf": "mytharcher.github.io/elf.js.org" //noCF? (don´t add this in a new PR)
