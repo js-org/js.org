448fe8b9a8fae717c90a815c9ea93a333fe53706
Eduardo San Martin Morote <posva@users.noreply.github.com>
2017-05-12 19:51:33 +0200

    vue-mdc

--- cnames_active.js
+++ cnames_active.js
@@ -798,6 +798,7 @@ var cnames_active = {
 ,"voloshins": "voloshins.github.io" //noCF? (don´t add this in a new PR)
 ,"vorpal": "dthree.github.io/vorpal" //noCF? (don´t add this in a new PR)
 ,"vscode-apielements":"xvincentx.github.io/vscode-apielements"
+,"vue-mdc":"posva.net/vue-mdc.js.org"
 ,"vuikit":"vuikit.github.io"
 ,"vuongdothanhhuy": "vuongdothanhhuy.github.io" //noCF? (don´t add this in a new PR)
 ,"wargamer": "hkwu.github.io/wargamer"
