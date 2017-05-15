768ee58c05b12170fd95cfbfd805623d7a65fa01
Malash <i@malash.me>
2017-05-16 02:34:03 +0800

    Add noflux

--- cnames_active.js
+++ cnames_active.js
@@ -496,6 +496,7 @@ var cnames_active = {
 ,"node-atlas": "haeresis.github.io/NodeAtlas"
 ,"node-browser_process": "caspervonb.github.io/node-browser_process" //noCF? (don´t add this in a new PR)
 ,"nodegarden": "pakastin.github.io/nodegarden"
+,"noflux": "nofluxjs.gitbooks.io/noflux"
 ,"noobscroll": "arguiot.github.io/NoobScroll"
 ,"noted": "carter-slade.github.io/Noted-" //noCF? (don´t add this in a new PR)
 ,"noteit": "saha96.github.io/NoteIt" //noCF? (don´t add this in a new PR)
