c0aea2ac075bc3620933a5f7588185dae5b330a8
Ankith Joseph <ankithjoseph@hotmail.com>
2017-07-18 20:46:24 +0530

    ankithjoseph.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -60,6 +60,7 @@ var cnames_active = {
 ,"anggao": "anggao.github.io" //noCF? (don´t add this in a new PR)
 ,"angular-jsf": "json-schema-faker.github.io/angular-jsf" //noCF? (don´t add this in a new PR)
 ,"angularstompdk": "davinkevin.github.io/AngularStompDK" //noCF? (don´t add this in a new PR)
+,"ankithjoseph": "ankithjoseph.github.io"
 ,"animo": "thrivingkings.github.io/animo"
 ,"annotate": "taitems.github.io/web-pdf-annotation" //noCF? (don´t add this in a new PR)
 ,"annoyingmouse": "annoyingmouse.github.io" //noCF? (don´t add this in a new PR)
