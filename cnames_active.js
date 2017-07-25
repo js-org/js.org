3c4115ba28d3e25c04f593a3556acd7f083d0efb
Hòa <m@hoadev.com>
2017-07-25 14:02:20 +0700

    add domain hoa.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -921,6 +921,7 @@ var cnames_active = {
 ,"zodiac": "indus.github.io/Zodiac" //noCF? (don´t add this in a new PR)
 ,"zombie": "assaf.github.io/zombie" //noCF? (don´t add this in a new PR)
 ,"zuck": "blanket-warriors.github.io/Zuck"
+,"hoa": "thehoa.github.io"
 /*
 * please don't add your subdomain records down here!
 * insert them in alphabetical order to help reduce merge conflicts.
