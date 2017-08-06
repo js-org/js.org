7cec5cea755cc83452849313e61a03ed3771109d
Fardin <fardin0855@gmail.com>
2017-08-06 22:15:49 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -932,6 +932,7 @@ var cnames_active = {
 ,"zodiac": "indus.github.io/Zodiac" //noCF? (don´t add this in a new PR)
 ,"zombie": "assaf.github.io/zombie" //noCF? (don´t add this in a new PR)
 ,"zuck": "blanket-warriors.github.io/Zuck"
+,"bonzibuddy:" "fardindadev.github.io/bonzibuddy"
 /*
 * please don't add your subdomain records down here!
 * insert them in alphabetical order to help reduce merge conflicts.
