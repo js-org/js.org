e6551a10bc035c14ef3a7854180467bb6bd13ed3
Sanjay Rawat <RawSanj@users.noreply.github.com>
2017-08-09 20:20:41 +0530

    Added rawsanj sub-domain name.

--- cnames_active.js
+++ cnames_active.js
@@ -931,7 +931,8 @@ var cnames_active = {
   "zeit": "finnzeit.github.io",
   "zodiac": "indus.github.io/Zodiac", // noCF? (don´t add this in a new PR)
   "zombie": "assaf.github.io/zombie", // noCF? (don´t add this in a new PR)
-  "zuck": "blanket-warriors.github.io/Zuck"
+  "zuck": "blanket-warriors.github.io/Zuck",
+  "rawsanj": "rawsanj.github.io"
 
   /*
   * please don"t add your subdomain records down here!
