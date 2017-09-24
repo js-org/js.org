024f94e8b81e9d40bde3efdda0c688b10e3247b8
Luke Dyson <luke.dyson@flyinganvil.co.uk>
2017-09-24 10:04:00 +0100

    Update cnames_active.js
    Changing dyson to dualcyclone

--- cnames_active.js
+++ cnames_active.js
@@ -254,11 +254,11 @@ var cnames_active = {
   "dragon": "sabertazimi.github.io/dragonjs", // noCF? (don´t add this in a new PR)
   "draw": "skillzzjesse.github.io/draw",
   "du": "ruanyl.github.io/du", // noCF? (don´t add this in a new PR)
+  "dualcyclone": "dualcyclone.github.io/postit-todo-react-redux",
   "duffn": "duffn.github.io", // noCF? (don´t add this in a new PR)
   "dunedin": "dunedinjs.github.io",
   "dutchakdev": "dutchakdev.github.io", // noCF? (don´t add this in a new PR)
   "dynamicsnode": "crisfervil.github.io/DynamicsNode",
-  "dyson": "dualcyclone.github.io/postit-todo-react-redux",
   "eahmed234": "eahmed234.github.io",
   "ed2k": "sunnyli.github.io/ed2k.js",
   "ef": "classicoldsong.github.io/ef.js.org",
