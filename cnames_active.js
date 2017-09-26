3c6f46f508cf313e2cfe5ade5b82be73cbaddd24
Fawad Mirzad <fawmi@users.noreply.github.com>
2017-09-26 13:21:50 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -835,6 +835,7 @@ var cnames_active = {
   "stepan": "stevermeister.github.io", // noCF? (don´t add this in a new PR)
   "stephenmccall": "stephenmccall.github.io", // noCF? (don´t add this in a new PR)
   "steven": "stpeng.github.io",
+  "sticky": "https://fawmi.github.io/sticky-pro"
   "stockings": "kgroat.github.io/stockings",
   "storage": "uzitech.github.io/storage.js",
   "storybook": "storybooks.github.io/storybook",
