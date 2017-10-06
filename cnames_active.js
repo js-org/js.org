e2754f185aee8ee75b22d09987906bf355ab0369
cirebon <32560737+cirebon@users.noreply.github.com>
2017-10-06 10:55:34 +0700

    Update cnames_active.js
    Request for cirebon.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -171,6 +171,7 @@ var cnames_active = {
   "christo": "christoga.github.io/js-org", // noCF? (don´t add this in a new PR)
   "chronos": "espinielli.github.io/chronos", // noCF? (don´t add this in a new PR)
   "cineasta": "cineasta-js.github.io/cineasta-docs",
+  "cirebon": "cirebon.github.io",
   "citation": "larsgw.github.io/citation.js",
   "city": "city.github.io",
   "ck": "cikai.github.io/blog",
