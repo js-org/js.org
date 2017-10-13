5bcc0139cdf294f0367d5c17292b5e0e3c1db518
rohit0803 <rohitagrawal0003@gmail.com>
2017-10-14 04:24:34 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -48,6 +48,7 @@ var cnames_active = {
   "action-u": "kevinast.github.io/action-u",
   "adon988": "adon988.github.io",
   "aghasemi": "aghasemi.github.io",
+  "agrawal": "agrawal.github.io",
   "agrawalnaman": "agrawalnaman.github.io",
   "ajaxable": "artf.github.io/ajaxable",
   "akase": "cedmax.github.io/akase", // noCF? (don´t add this in a new PR)
@@ -762,7 +763,6 @@ var cnames_active = {
   "rocket": "KevinOfNeu.github.io/rocket",
   "rockjins": "rockjins.github.io",
   "roger": "klopango.github.io",
-  "rohit": "agrawal.github.io",
   "rosmaro": "lukaszmakuch.github.io/rosmaro",
   "router-advanced": "oldergod.github.io/router-advanced",
   "rp": "rpocklin.github.io", // noCF? (don´t add this in a new PR)
