cf5c23dacbb9ff40a06a7e968386cf812538c21e
Paul Makles <mcrocks999@users.noreply.github.com>
2017-10-22 13:24:00 +0100

    solder.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -832,6 +832,7 @@ var cnames_active = {
   "snekfetch": "devsnek.github.io/snekfetch"
   "social-likes-next": "sapegin.github.io/social-likes-next", // noCF? (don´t add this in a new PR)
   "social-likes": "sapegin.github.io/social-likes", // noCF? (don´t add this in a new PR)
+  "solder": "mcrocks999.github.io/solder.js",
   "solome": "solome.github.io",
   "sonny": "sonnylazuardi.github.io", // noCF? (don´t add this in a new PR)
   "sox": "sox-team.github.io/sox", // noCF? (don´t add this in a new PR)
