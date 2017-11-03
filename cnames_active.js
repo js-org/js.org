5bb172059dc692ab90929df3f3975cf46dce60ba
d.zhuribeda <d.zhuribeda@smartweb.com.ua>
2017-11-03 11:58:50 +0200

    added tanok domain

--- cnames_active.js
+++ cnames_active.js
@@ -899,6 +899,7 @@ var cnames_active = {
   "shd": "pro-devel.github.io",
   "tagster": "goschevski.github.io/tagster", // noCF? (don´t add this in a new PR)
   "talker": "secondstreet.github.io/talker.js", // noCF? (don´t add this in a new PR)
+  "tanok": "brabadu.github.io/tanok",
   "tap-flux": "technologyadvice.github.io/tap-flux", // noCF? (don´t add this in a new PR)
   "tapestry-wp": "shortlist-digital.github.io/tapestry-wp",
   "tara": "shakyatara.github.io",
