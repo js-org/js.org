432aa875ff85f27fe81df04f9a4b687de15383cf
Stefan Keim <indus@users.noreply.github.com>
2017-11-15 19:45:48 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -111,7 +111,7 @@ var cnames_active = {
   "bash": "bashjs.github.io", // noCF? (don´t add this in a new PR)
   "basicgame": "basicgame.github.io/basicGame.js", // noCF? (don´t add this in a new PR)
   "bastion": "snkrsnkampa.github.io/Bastion-Site",  
-  "bbn": "jammer99.github.io",
+  "bbn": "jammer99.github.io/bbn",
   "bc": "mazko.github.io/bc.js",
   "be": "davidep87.github.io/bejs.github.io",
   "begin": "advanced-webapps-class.github.io/begin", // noCF? (don´t add this in a new PR)
