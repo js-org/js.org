a715866388d682f0756ce8c52e847a2c47ba0a6b
Stefan Keim <indus@users.noreply.github.com>
2017-11-21 07:42:36 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -575,7 +575,6 @@ var cnames_active = {
   "mw": "agauniyal.github.io/mw",
   "mythbusters": "kikobeats.github.io/js-mythbusters",
   "n-j-m": "n-j-m.github.io", // noCF? (don´t add this in a new PR)
-  "n": "nick-e.co.uk",
   "nanimation": "imthenachoman.github.io/nAnimation", // noCF? (don´t add this in a new PR)
   "naughtychecker": "gautamkrishnar.github.io/naughtychecker.js",
   "nautilus": "ignigena.github.io/nautilus",
