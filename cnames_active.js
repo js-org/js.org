8afe228d5441f86122a23abcbc3184e2ffeded1a
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-11-30 13:19:33 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -806,6 +806,7 @@ var cnames_active = {
   "ruhuman": "ruhuman.github.io", // noCF? (don´t add this in a new PR)
   "ruwan": "rpgee.github.io",
   "s3swa": "s3swa-ict-upgifter.github.io/snow-eater",
+  "s4swa": "s4swa.github.io",
   "saadmir": "saadmir.github.io", // noCF? (don´t add this in a new PR)
   "sagui": "saguijs.github.io", // noCF? (don´t add this in a new PR)
   "saigon": "saigonjs.github.io",
