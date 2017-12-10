8925a3b57c587d60a1d4253b4b0e459bdf9c0baf
KO <ko25july@gmail.com>
2017-12-10 10:40:21 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -948,6 +948,7 @@ var cnames_active = {
   "terminal": "gottox.github.io/terminal.js", // noCF? (don´t add this in a new PR)
   "textillate": "jschr.github.io/textillate", // noCF? (don´t add this in a new PR)
   "texy": "egoist.github.io/texy",
+  "thai": "ko25july.github.io/thai",
   "thea": "idokutela.github.io/thea",
   "thegrid": "antoniandre.github.io/grid",
   "themoviedb": "chrisenytc.github.io/themoviedb", // noCF? (don´t add this in a new PR)
