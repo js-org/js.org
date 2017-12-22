e2e1a8db2fac7155f60bc6154f860621519cf36d
lijialiang <lijialiang@yy.com>
2017-12-22 16:32:45 +0800

    [add] yyued.github.io/hub.js/

--- cnames_active.js
+++ cnames_active.js
@@ -1085,7 +1085,8 @@ var cnames_active = {
   "zeit": "vinhpz.github.io",
   "zodiac": "indus.github.io/Zodiac", // noCF? (don´t add this in a new PR)
   "zombie": "assaf.github.io/zombie", // noCF? (don´t add this in a new PR)
-  "zp": "tilda.ws"
+  "zp": "tilda.ws",
+  "hub": "yyued.github.io/hub.js/"
   /*
   * please don"t add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
