f2e116793d0d44d083a969b5a23f565f29fcdce5
Paul Le Cam <paul@ulem.net>
2018-01-01 14:15:47 +0000

    react-leaflet.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -775,6 +775,7 @@ var cnames_active = {
   "react-entanglement": "react-entanglement.github.io",
   "react-form": "react-form.netlify.com",
   "react-hint": "slmgc.github.io/react-hint",
+  "react-leaflet": "paullecam.github.io/react-leaflet",
   "react-move": "react-move.netlify.com",
   "react-native-floating-labels": "mayank-patel.github.io/react-native-floating-labels",
   "react-pivottable": "plotly.github.io/react-pivottable",
