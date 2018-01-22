64efb47eb04af877430b334a581133f2790f8d44
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2018-01-22 16:31:06 +0100

    pwa-cookbook.js.org
    - There is reasonable content on the page
    - I have read and accepted the ToS

--- cnames_active.js
+++ cnames_active.js
@@ -767,6 +767,7 @@ var cnames_active = {
   "profanity-finder": "gautamkrishnar.github.io/profanity-finder.js",
   "proteic": "proteus-h2020.github.io/proteic",
   "pure": "fengzilong.github.io/pure",
+  "pwa-cookbook": "sylvainpolletvillard.github.io/pwa-cookbook",
   "qs": "kirjs.github.io/qs.js", // noCF? (don´t add this in a new PR)
   "querybuilder": "mistic100.github.io/jQuery-QueryBuilder", // noCF? (don´t add this in a new PR)
   "question": "teamtofu.github.io/questionify",
