2203462edf6929b3ab6401a7575cdd75c9a1b665
mcdnf <392567445@qq.com>
2018-01-30 16:51:40 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -575,7 +575,6 @@ var cnames_active = {
   "maxnachlinger": "maxnachlinger.github.io", // noCF? (don´t add this in a new PR)
   "maxtracking": "maxtracking.github.io", // noCF? (don´t add this in a new PR)
   "mc": "magicarbon.github.io/mc", // noCF? (don´t add this in a new PR)
-  "mc": "mcdnf.com",
   "mde": "lukehorvat.github.io/mde-soundboard",
   "mechan": "dusterthefirst.github.io/mechan.js",
   "medan": "medan-js.github.io", // noCF
