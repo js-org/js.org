86080785dd42fedd5e79989ac9a6c4ac2706302c
You2php <34645738+You2php@users.noreply.github.com>
2018-01-31 13:05:57 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1131,7 +1131,8 @@ var cnames_active = {
   "zeit": "vinhpz.github.io",
   "zodiac": "indus.github.io/Zodiac", // noCF? (don´t add this in a new PR)
   "zombie": "assaf.github.io/zombie", // noCF? (don´t add this in a new PR)
-  "zp": "tilda.ws"
+  "zp": "tilda.ws",
+  "2tube": "you2php.github.io/you2php", // noCF? (don´t add this in a new PR)
   /*
   * please don"t add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
