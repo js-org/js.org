f7a77dbf3d5c4a0621185540b6468d57d481f06a
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2018-02-19 21:21:41 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -133,7 +133,7 @@ var cnames_active = {
   "be": "davidep87.github.io/bejs.github.io",
   "begin": "advanced-webapps-class.github.io/begin", // noCF? (don´t add this in a new PR)
   "bestof": "michaelrambeau.github.io/bestofjs",
-  "bicbacboe":"bicbacboe.github.io/Client",
+  "bicbacboe":"bicbacboe.github.io/Bic-Bac-Boe",
   "bigtest": "thefrontside.github.io/bigtest.js.org",
   "bildepunkt": "bildepunkt.github.io", // noCF? (don´t add this in a new PR)
   "bind-action-dispatchers": "cchamberlain.github.io/bind-action-dispatchers", // noCF? (don´t add this in a new PR)
