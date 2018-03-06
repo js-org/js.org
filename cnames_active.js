2fd2322c956e6942a7ae7f18ba51095385d468f4
Bjornskjald <github@bjorn.ml>
2018-03-06 21:55:25 +0100

    opted out from cloudflare

--- cnames_active.js
+++ cnames_active.js
@@ -656,7 +656,7 @@ var cnames_active = {
   "mininote": "htdt.github.io/mininote",
   "miny": "pablopunk.github.io/miny",
   "mis101bird": "mis101bird.github.io", // noCF? (don´t add this in a new PR)
-  "miscord": "bjornskjald.github.io/miscord",
+  "miscord": "bjornskjald.github.io/miscord", // noCF
   "mithril": "mithriljs.github.io/mithril.js",
   "mithril-ja": "shibukawa.github.io/mithril-ja", // noCF? (don´t add this in a new PR)
   "mmcq": "nikola.github.io/MMCQ", // noCF? (don´t add this in a new PR)
