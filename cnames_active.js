60a9594e97fa4e3b6c5c60b00940bb6cc40f5e91
Dhruvdutt Jadhav <dhruvdutt.jadhav@gmail.com>
2018-03-13 20:45:27 +0530

    Clean duplicate key

--- cnames_active.js
+++ cnames_active.js
@@ -936,12 +936,11 @@ var cnames_active = {
   "ruwan": "rpgee.github.io",
   "s3swa": "s3swa-ict-upgifter.github.io/snow-eater",
   "s4swa": "s4swa.github.io",
-  "salvacam": "salvacam.github.io",
-  "salvattore": "rnmp.github.io/salvattore",
   "saadmir": "saadmir.github.io", // noCF? (don´t add this in a new PR)
   "sagui": "saguijs.github.io", // noCF? (don´t add this in a new PR)
   "saigon": "saigonjs.github.io",
   "sailesh": "saileshkasaju.github.io/crud-react-redux-mongo",
+  "salvacam": "salvacam.github.io",
   "salvattore": "rnmp.github.io/salvattore",
   "sam": "jdubray.github.io/sam", // noCF? (don´t add this in a new PR)
   "samlify": "tngan.github.io/samlify",
