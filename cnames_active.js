2cc14fc99cbc260cb5d4ee3fcca4dfc7d48cdbb5
Andre Shahan <34530026+ancs@users.noreply.github.com>
2018-03-18 15:08:31 +0700

    Change repo url

--- cnames_active.js
+++ cnames_active.js
@@ -75,7 +75,7 @@ var cnames_active = {
   "alt": "goatslacker.github.io/alt", // noCF? (don´t add this in a new PR)
   "alveron": "rofrischmann.github.io/alveron",
   "amaple": "amjs-team.github.io/amaple",
-  "andre": "anc.github.io/site",
+  "andre": "ancs.github.io/site",
   "anenth": "anenth.github.io", // noCF? (don´t add this in a new PR)
   "anggao": "anggao.github.io", // noCF? (don´t add this in a new PR)
   "angular-jsf": "json-schema-faker.github.io/angular-jsf", // noCF? (don´t add this in a new PR)
