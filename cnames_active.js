86b81c17de27529d166109e0b651f0516bebe488
Focus Chen <i@focuschen.com>
2018-05-23 05:34:56 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -501,6 +501,7 @@ var cnames_active = {
   "hubble": "hubble.netlify.com",
   "huck": "huckjs.github.io/huck",
   "human": "human-js.gitbooks.io", // noCF? (don´t add this in a new PR)
+  "hx": "hxco-web.github.io/www.hxis.me",
   "hxxbit": "hxxbit.github.io",
   "hyde": "gheek.github.io/hyde", // noCF? (don´t add this in a new PR)
   "hyf": "yafey.github.io",
