c473465b14e21c373a059399f32baefe84434bd3
Hanke Liou <hanke.liou@circ.top>
2018-06-06 03:58:30 +0800

    hotbuy

--- cnames_active.js
+++ cnames_active.js
@@ -505,6 +505,7 @@ var cnames_active = {
   "hk": "akura-co.github.io/hk", // noCF? (don´t add this in a new PR)
   "hoa": "thehoa.github.io",
   "hooloo": "hooloo.github.io", // noCF? (don´t add this in a new PR)
+  "hotbuy": "hotbuy.github.io",
   "hub": "yyued.github.io/hub.js",
   "hubble": "hubble.netlify.com",
   "huck": "huckjs.github.io/huck",
