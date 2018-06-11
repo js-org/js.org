4e1342b15647e4c17a1cfc9065af04e892c8a9dd
Stefan Keim <indus@users.noreply.github.com>
2018-06-11 08:17:20 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -55,7 +55,7 @@ var cnames_active = {
   "accordion": "awps.github.io/Accordion.JS",
   "acebooks": "mohamed0group.github.io/acebooks",
   "acegroup": "mohamed0group.github.io/acegroup",
-  "acho": "achojs.github.io/acho",p
+  "acho": "achojs.github.io/acho",
   "acme": "makepost.github.io/acme-commander",
   "action-u": "kevinast.github.io/action-u",
   "active-resource": "nicklandgrebe.github.io/activeresource.js",
