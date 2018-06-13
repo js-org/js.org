f8140450e9496475b41181dffbe21f7649aa420a
nfwyst <nfwyst@gmail.com>
2018-06-13 20:39:38 +0800

    update

--- cnames_active.js
+++ cnames_active.js
@@ -1349,6 +1349,7 @@ var cnames_active = {
   "zombie": "assaf.github.io/zombie", // noCF? (don´t add this in a new PR)
   "zp": "tilda.ws",
   "zty": "zetaoyang.github.io",
+  "nfwyst": "nfwyst.github.io",
   /*
   * please don"t add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
