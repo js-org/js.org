067748d1bfeee0651ae320ce228d4ca5922706ad
Rodger Jordas <rmjordas@gmail.com>
2018-06-16 03:22:08 +0800

    Add alys.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -79,6 +79,7 @@ var cnames_active = {
   "almin": "almin.github.io",
   "alt": "goatslacker.github.io/alt", // noCF? (don´t add this in a new PR)
   "alveron": "rofrischmann.github.io/alveron",
+  "alys": "rmjordas.github.io/alys",
   "amaple": "amjs-team.github.io/amaple",
   "amorphous": "ariabuckles.github.io/amorphous",
   "andre": "caxvis.github.io",
