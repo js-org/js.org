e5cacc6686e3f93bbe79d255db5861f52c27711a
charlottelin1125 <evolcandi@yahoo.com.tw>
2018-06-20 10:41:47 +0800

    add cnames_actives.js

--- cnames_active.js
+++ cnames_active.js
@@ -218,6 +218,7 @@ var cnames_active = {
   "central-node": "central-node.github.io", // noCF? (don´t add this in a new PR)
   "chain-able": "fluents.github.io/chain-able-site",
   "chatexchange": "jacob-gray.github.io/ChatExchangeJS",
+  "charlotte": "charlottelin1125.github.io/gh-pages",
   "chartconstructor": "quarksworks.github.io/chartConstructor",
   "checklist": "hellogreg.github.io/checklist",
   "cheerio": "cheeriojs.github.io/cheerio",
