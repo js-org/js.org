e211323ebb0ff44bf2d1fa775505892f42ba0f28
Bijiabo <hu@cafa.me>
2018-06-23 23:29:09 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1357,7 +1357,7 @@ var cnames_active = {
   "zombie": "assaf.github.io/zombie", // noCF? (don´t add this in a new PR)
   "zp": "tilda.ws",
   "zty": "zetaoyang.github.io",
-  "iot": "product-engineer.github.io/SITE", // noCF
+  "iot": "product-engineer.github.io/iot.js.org", // noCF
   /*
   * please don"t add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
