982422cde57d32fa70ffb5c64e6d22d6f9a5b393
Roman O <ceo.roman@gmail.com>
2018-07-30 09:53:26 +0300

    netology-group.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -817,6 +817,7 @@ var cnames_active = {
   "nemaniarjun": "nemaniarjun.github.io",
   "nemo": "paypal.github.io/nemo", // noCF? (don´t add this in a new PR)
   "neo4": "janpeter.github.io/neo4js",
+  "netology-group": "netology-group.github.io",
   "neutrino": "neutrinojs.netlify.com", // noCF
   "nflow": "nflow-js.github.io", // noCF? (don´t add this in a new PR)
   "nfwyst": "nfwyst.github.io",
