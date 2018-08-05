68e0a5ddff990680ae3218d78366649f9c7faf33
杨俊宁 <1003719811@qq.com>
2018-08-06 04:28:38 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1414,6 +1414,7 @@ var cnames_active = {
   "yt": "tangge1119.github.io",
   "yusril": "yusrilhs.github.io", // noCF
   "yvm": "hosting.gitbook.com", // noCF
+  "young": "youngjuning.github.io",
   "zanyuyu": "zanseven007.github.io/resume", // noCF
   "zaporizhzhia": "zaporizhzhiajs.github.io",
   "zazu": "zazujs.github.io", // noCF? (don´t add this in a new PR)
