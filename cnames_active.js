ab28c354a63da8fc044bd39e8272a409d7510523
Bojan Djurkovic <dbojan@gmail.com>
2018-08-09 21:43:02 -0300

    add mali.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -714,6 +714,7 @@ var cnames_active = {
   "majestic": "moityjs.github.io/majestic",
   "maker": "microsoft.github.io/maker.js",
   "maki": "makixx.github.io",
+  "mali": "malijs.github.io",
   "manytimepad": "dipanshkhandelwal.github.io/ManyTimePad",
   "mappa": "cvalenzuela.github.io/Mappa",
   "marble": "jsguy.github.io/marble",
