9fc4bd3afcb60d6fff625524b6aa4e73151896b0
Stefan Keim <indus@users.noreply.github.com>
2018-08-20 12:13:52 +0200

    Update cnames_active.js
    #2245 (due to some conflict)

--- cnames_active.js
+++ cnames_active.js
@@ -131,6 +131,7 @@ var cnames_active = {
   "awoo": "awoojs.github.io/website",
   "axelgreavette": "axelgreavette.github.io",
   "azdanov": "azdanov.netlify.com", //noCF
+  "bachors": "bachors.github.io",
   "backlog": "backlog-js.github.io/backlog.js.org", // noCF? (don´t add this in a new PR)
   "backstage": "backstagejs.github.io/backstage",
   "backwards": "backwards-n.github.io",
