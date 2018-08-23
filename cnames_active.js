87281e1d35a0175da94d4d1662c20ddf7bc7ef9e
yjw0628 <yjw0628@gmail.com>
2018-08-23 14:26:45 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1440,6 +1440,7 @@ var cnames_active = {
   "youtim": "mortonfox.github.io/YouTim", // noCF? (don´t add this in a new PR)
   "youtube-box": "lucasmonteverde.github.io/youtube-box", // noCF? (don´t add this in a new PR)
   "yt": "tangge1119.github.io",
+  "yjw": "yjw0628.github.io/yjw.js.org", // noCF
   "yusril": "yusrilhs.github.io", // noCF
   "yvm": "hosting.gitbook.com", // noCF
   "zanyuyu": "zanseven007.github.io/resume", // noCF
