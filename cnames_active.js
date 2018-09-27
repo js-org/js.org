9cf8d97bd1f804c4c081e2b1aac19dd066efc321
Archie Baer <42045366+archiebaer@users.noreply.github.com>
2018-09-27 07:38:26 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -869,6 +869,7 @@ var cnames_active = {
   "mvidalgarcia": "mvidalgarcia.github.io", // noCF? (don´t add this in a new PR)
   "mw": "agauniyal.github.io/mw",
   "mysketch": "dipanshkhandelwal.github.io/MySketch",
+  "mysite": "archiebaer.github.io/mysite",
   "mythbusters": "mythbustersjs.netlify.com",
   "n-j-m": "n-j-m.github.io", // noCF? (don´t add this in a new PR)
   "namelessman": "namelessman.github.io",
