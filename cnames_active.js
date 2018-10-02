33cf31cf2bfe4dbc1faa9e8c1432587fa6e39842
Archie Baer <42045366+archiebaer@users.noreply.github.com>
2018-10-02 16:40:40 +0100

    I think my server only accepts IPs
    I don't really know.

--- cnames_active.js
+++ cnames_active.js
@@ -872,7 +872,7 @@ var cnames_active = {
   "mvidalgarcia": "mvidalgarcia.github.io", // noCF? (don´t add this in a new PR)
   "mw": "agauniyal.github.io/mw",
   "mysketch": "dipanshkhandelwal.github.io/MySketch",
-  "mysite": "broomfieldhosting.co.uk",
+  "mysite": "104.238.170.144",
   "mythbusters": "mythbustersjs.netlify.com",
   "n-j-m": "n-j-m.github.io", // noCF? (don´t add this in a new PR)
   "namelessman": "namelessman.github.io",
