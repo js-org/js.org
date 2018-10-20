332dbc1d5f61f5fcba0edb59d3190b7b1a9a93f1
腐草 <37659731+FusuHub@users.noreply.github.com>
2018-10-20 20:57:26 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -507,7 +507,7 @@ var cnames_active = {
   "fuck": "gerd2002.github.io/fuck.js",
   "funbook": "egoist.github.io/funbook",
   "funfuncs": "floffah.github.io/funfuncs",
-  "fusuhub": " fusuhub.github.io",
+  "fucao": " fusuhub.github.io",
   "fyi": "tobihrbr.github.io/fyi",
   "g": "nodebox.github.io/g.js",
   "gal": "galmail.github.io", // noCF? (don´t add this in a new PR)
