ff96b013b28ff1e0f034edc35145c8dc39aa2823
Nicolas Giard <github@ngpixel.com>
2018-10-21 15:48:45 -0400

    update cname for wiki.js

--- cnames_active.js
+++ cnames_active.js
@@ -1506,7 +1506,7 @@ var cnames_active = {
   "weekly": "xdimh.github.io/weekly",
   "whiteboard": "yhozen.github.io/whiteboard",
   "wice": "yulioaj290.github.io/wice.js",
-  "wiki": "requarks.github.io/wiki-site",
+  "wiki": "wikijs.netlify.com", // noCF
   "wildfire": "cheng-kang.github.io/wildfire",
   "within": "eric-brechemier.github.io/within", // noCF? (don´t add this in a new PR)
   "wooyun": "jiji262.github.io/wooyun_articles",
