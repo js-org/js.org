cde0980b8b2b859fc53dfadd089d21008c837558
AshamedDonut <35849406+Zentor@users.noreply.github.com>
2018-10-23 20:01:18 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -36,6 +36,7 @@
  */
 
 var cnames_active = {
+  "senpai": "zentor.github.io",
   "": "js-org.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
