199d39f1da5fdd5df94cfa54aa46f8541bcac971
Stefan Keim <indus@users.noreply.github.com>
2018-10-31 07:54:47 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -769,7 +769,6 @@ var cnames_active = {
   "lychee": "Artificial-Engineering.github.io/lycheeJS-website",
   "lyra": "amansahil.github.io/lyra.js.org",
   "m8bot": "mapreiff.github.io/m8-bot-site",
-  "ma124": "ma124.github.io",
   "macosnotif": "macosnotifjs.mattcowley.co.uk", // noCF
   "madankumar": "jmadankumar.github.io",
   "magnet": "magnetjs.github.io/Magnet", // noCF? (don´t add this in a new PR)
