ed042e7b3243d252d4ab0a120bb707b6d6825c94
FHGDev <gideon.lingle1@gmail.com>
2018-11-14 12:30:50 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -36,6 +36,7 @@
  */
 
 var cnames_active = {
+  "disnode": "fhgdev.github.io/discnode",
   "": "js-org.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
