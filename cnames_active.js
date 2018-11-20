e0933d9be918690adfb3483933d85df385c5c888
Stefan Keim <indus@users.noreply.github.com>
2018-11-20 08:39:02 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1577,8 +1577,7 @@ var cnames_active = {
   "zoe": "zoe-js.netlify.com",
   "zombie": "assaf.github.io/zombie", // noCF? (don´t add this in a new PR)
   "zp": "tilda.ws",
-  "zty": "zetaoyang.github.io",
-  "iori": "iori20091101.github.io"
+  "zty": "zetaoyang.github.io"
   /*
   * please don"t add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
