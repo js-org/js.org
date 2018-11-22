27ebe12cca619147d900e7915728555e19fee8e9
Jake Bolam <jake.bolam@gmail.com>
2018-11-22 11:59:26 -0500

    add with-immutable-props-to-js

--- cnames_active.js
+++ cnames_active.js
@@ -1546,6 +1546,7 @@ var cnames_active = {
   "wiki": "wikijs.netlify.com", // noCF
   "wildfire": "cheng-kang.github.io/wildfire",
   "within": "eric-brechemier.github.io/within", // noCF? (don´t add this in a new PR)
+  "with-immutable-props-to-js": "tophat.github.io",
   "wooyun": "jiji262.github.io/wooyun_articles",
   "wordywordy": "sverweij.github.io/wordywordy",
   "writebar": "alexander-shvets.github.io/writebar",
