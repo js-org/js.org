18a3bd413b7414cf509d48463736930b1a96d0a6
Jason <jason@scurker.com>
2018-11-27 12:34:54 -0600

    add currency.js

--- cnames_active.js
+++ cnames_active.js
@@ -301,6 +301,7 @@ var cnames_active = {
   "csv": "adaltas.github.io/node-csv-docs",
   "cucumber-mink": "adezandee.github.io/cucumber-mink", // noCF? (don´t add this in a new PR)
   "curi": "pshrmn.github.io/curi",
+  "currency": "scurker.github.io/currency.js",
   "curseapp": "mcrocks999.github.io/curseapp.js",
   "cybernaut": "clebert.github.io/cybernaut",
   "cycle": "cyclejs.github.io",
