4d884402df97a9984730ab678464b9162d7336e5
梨子(Riko) <18373361+rikakomoe@users.noreply.github.com>
2018-12-12 01:15:14 +0800

    copywriting-correct

--- cnames_active.js
+++ cnames_active.js
@@ -289,6 +289,7 @@ var cnames_active = {
   "cookiebox": "metaa.gitlab.io/cookiebox.js",
   "coord": "itsacorn.github.io/jscoord",
   "copyer": "tsycode.github.io/copyer.js",
+  "copywriting-correct": "rikakomoe.github.io/copywriting-correct",
   "cordova-multiplatform-template": "ckgrafico.github.io/Cordova-Multiplatform-Template", // noCF? (don´t add this in a new PR)
   "cork": "davej.github.io/CorkJS",
   "corki": "dvtate.github.io/corki",
