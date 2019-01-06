4d10041443648070553651dfbb16ddf871d5ddc7
Behrad Ranjbar <behi198@gmail.com>
2019-01-06 14:02:07 +0330

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -169,6 +169,7 @@ var cnames_active = {
   "bc": "mazko.github.io/bc.js",
   "bci": "pwstegman.github.io/bcijs",
   "be": "davidep87.github.io/bejs.github.io",
+  "behrad": "behi198.github.io/behrad",
   "begin": "advanced-webapps-class.github.io/begin", // noCF? (don´t add this in a new PR)
   "berzan": "newbie13xd.github.io/berzan",
   "besafe": "ma124.github.io/BeSafe",
