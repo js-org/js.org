ce52ca68ca4b93528f92bfe2289c61eddb60d000
michaloslav <31165844+michaloslav@users.noreply.github.com>
2019-01-30 14:21:03 +0100

    deleted spaces

--- cnames_active.js
+++ cnames_active.js
@@ -863,7 +863,7 @@ var cnames_active = {
   "milesgitgud": "milesgitgud.github.io/homepage",
   "mimic": "500tech.github.io/mimic",
   "mina": "CenturyUna.github.io/mina",
-  "Mind Your Time": "https://michaloslav.github.io/Mind-Your-Time/",
+  "MindYourTime": "https://michaloslav.github.io/Mind-Your-Time/",
   "minecraft-data.prismarine": "prismarinejs.github.io/minecraft-data",
   "mineflayer.prismarine": "prismarinejs.github.io/mineflayer",
   "minesweeper": "derflatulator.github.io/minesweeper",
