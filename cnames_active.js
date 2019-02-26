acd132f5fb56a4f84e29abde212bb9ded616a108
JM <jmdisuanco@gmail.com>
2019-02-26 22:44:06 +0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1089,6 +1089,7 @@ var cnames_active = {
   "pigeon-maps": "mariusandra.github.io/pigeon-maps",
   "piii": "piiijs.github.io/piii.js.org",
   "pinf": "pinf.github.io", // noCF? (don´t add this in a new PR)
+  "pinipig": "jmdisuanco.github.io/pinipig"
   "pipes": "pipesjs.github.io", // noCF? (don´t add this in a new PR)
   "pitermarx": "pitermarx.github.io", // noCF? (don´t add this in a new PR)
   "pivot": "wnda.github.io/pivot",
