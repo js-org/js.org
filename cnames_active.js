d422cec45ce3278f9ad951c29f27822aa8b9e31d
Marat Dyatko <31615495+dyatko@users.noreply.github.com>
2019-03-05 09:48:37 +0100

    arkit.js.org
    https://github.com/dyatko/arkit

--- cnames_active.js
+++ cnames_active.js
@@ -125,6 +125,7 @@ var cnames_active = {
   "arief": "1997arief.github.io",
   "arime": "ninbryan.github.io/arime", // noCF? (don´t add this in a new PR)
   "artalk": "qwqcode.github.io/Artalk",
+  "arkit": "dyatko.github.io/arkit",
   "artery": "arteryjs.github.io/gh-pages", // noCF? (don´t add this in a new PR)
   "arthurmbandeira": "arthurmbandeira.github.io",
   "ass": "weizhenye.github.io/ASS",
