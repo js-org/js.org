a53196e040f21b3fadfafb3d7b2c810b17b0885e
Stefan Keim <indus@users.noreply.github.com>
2019-03-14 13:10:06 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1589,7 +1589,7 @@ var cnames_active = {
   "vrv": "cloudflare-to-cloudflare.readthedocs.io",
   "vscode-apielements": "xvincentx.github.io/vscode-apielements",
   "vssue": "vssue.netlify.com",
-  "vstatic": "vstatic-webserver.github.io",
+  "vstatic": "vstatic-webserver.github.io/website",
   "vue-aplayer": "sevenoutman.github.io/vue-aplayer",
   "vue-coffee": "vue-coffee-guide.netlify.com",
   "vue-core": "triotech-fr.github.io/vue-core",
