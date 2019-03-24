fb6f728da977af45505fcd4b7cbf7ef496781d4b
@iiilfaaa <at.iiilfaaa@gmail.com>
2019-03-25 05:59:22 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -660,6 +660,7 @@ var cnames_active = {
   "ignite": "ignitejscl.github.io",
   "iiilfaaa": "iiilfaaa.github.io",
   "iio": "iioinc.github.io/iio.js", // noCF? (don´t add this in a new PR)
+  "ilfa": "iiilfaaa.github.io/www",
   "illuxi": "razod.github.io/illuxi",
   "imagecapture": "googlechrome.github.io/imagecapture-polyfill",
   "imcvampire": "imcvampire.github.io",
