6928a33eb36d7cc2ff0b93c836e992b724909227
Laveesh Gupta <loveeshgoyal@gmail.com>
2019-03-30 00:26:04 +0530

    updated the cname entry

--- cnames_active.js
+++ cnames_active.js
@@ -797,7 +797,7 @@ var cnames_active = {
   "laubstein": "laubstein.github.io", // noCF? (don´t add this in a new PR)
   "laue": "qingwei-li.github.io/laue",
   "lava": "palladium.github.io/Lava.js",
-  "laveesh": "laveesh.github.io/myrw"
+  "laveesh": "laveesh.github.io"
   "lcookie": "lf112.github.io/lcookie",
   "le": "letui.github.io",
   "leafless": "mofax.github.io/leafless",
