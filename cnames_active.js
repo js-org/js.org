2e7b271e70532f65ec6cb23b5577859f28227b77
Mohammad Hasani <20781126+thebrodmann@users.noreply.github.com>
2019-04-03 03:51:46 +0430

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -373,6 +373,7 @@ var cnames_active = {
   "delegacias-fortaleza": "juliosampaio.github.io/delegacias-fortaleza", // noCF? (don´t add this in a new PR)
   "delet": "ds-development.github.io/delet",
   "denisvieira": "fromdenisvieira.github.io", // noCF? (don´t add this in a new PR)
+  "deox": "deox.netlify.com",
   "depo": "boltxyz.github.io/depo",
   "deputy": "ry4nolson.github.io/Deputy", // noCF? (don´t add this in a new PR)
   "detect-resize": "noderaider.github.io/detect-resize", // noCF? (don´t add this in a new PR)
