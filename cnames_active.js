8a632a8771aa9d406cdffa187b724600c3178a5f
PravallikaK <pravallika.chowdary1988@gmail.com>
2019-04-03 12:43:11 -0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1158,6 +1158,7 @@ var cnames_active = {
   "pptx": "meshesha.github.io/jspptxjs.github.io",
   "pubg": "ickerio.github.io/pubg.js",
   "published": "fiverr.github.io/published",
+  "pravallikakolukuluri": "https://pravallikak.github.io/PravallikaKolukuluri",
   "pretty-print-json": "center-key.github.io/pretty-print-json",
   "prettylog": "moosecoop.github.io/PrettyLog/",
   "pristine": "sha256.github.io/Pristine", //noCF
