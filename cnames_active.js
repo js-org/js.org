e478e87cf2d66270c791f378eba568dd4a01baab
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2019-04-18 19:46:10 +0100

    alphabetical

--- cnames_active.js
+++ cnames_active.js
@@ -304,11 +304,11 @@ var cnames_active = {
   "cn.redux": "camsong.github.io/redux-in-chinese", // noCF? (don´t add this in a new PR)
   "cn.rx": "rxjs-cn.github.io/RxJS-Docs-CN", // noCF
   "cnc": "cncjs.github.io/cncjs.org",
+  "codebooky": "hiteshsubnani0128.github.io/codebooky",
   "codemade": "codemade.github.io", // noCF? (don´t add this in a new PR)
   "codepan": "egoist.github.io/codepan",
   "codesurfer": "code-surfer.netlify.com", // noCF
   "codinsky": "izhaki.github.io/codinsky",
-  "codebooky":"hiteshsubnani0128.github.io/codebooky",
   "coffea": "caffeinery.github.io/coffea",
   "coir": "channg.github.io/coir",
   "collegequest": "schoolhouserock.github.io/CollegeQuest", // noCF? (don´t add this in a new PR)
