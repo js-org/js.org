93b8c64dee04b13690e743bf44e2da42f177f435
Mohammad Aziz <mdaziz067@gmail.com>
2019-05-05 13:03:30 +0530

    Update URL for aziz.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -162,7 +162,7 @@ var cnames_active = {
   "awoo": "awoojs.github.io/website",
   "axelgreavette": "axelgreavette.github.io",
   "azdanov": "azdanov.netlify.com", //noCF
-  "aziz": "iAziz786.github.io/aziz.js.org",
+  "aziz": "aziz.now.sh",
   "bara": "barajs.github.io/bara",
   "bachors": "bachors.github.io",
   "backlog": "backlog-js.github.io/backlog.js.org", // noCF? (don´t add this in a new PR)
