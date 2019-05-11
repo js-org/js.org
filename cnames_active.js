9a62bbde5248ad4e60bf35b6cba41e501fbb53c6
Mohammad Aziz <mdaziz067@gmail.com>
2019-05-11 11:02:59 +0530

    Add aziz.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -162,6 +162,7 @@ var cnames_active = {
   "awoo": "awoojs.github.io/website",
   "axelgreavette": "axelgreavette.github.io",
   "azdanov": "azdanov.netlify.com", //noCF
+  "aziz": "iaziz786.netlify.com",
   "bara": "barajs.github.io/bara",
   "bachors": "bachors.github.io",
   "backlog": "backlog-js.github.io/backlog.js.org", // noCF? (don´t add this in a new PR)
