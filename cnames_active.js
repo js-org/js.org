2c8c3d63e6e2acf55a9825782805eaf5ba598da7
Daniel K <FredyC@users.noreply.github.com>
2019-05-18 10:18:03 +0200

    mobx-react.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -977,6 +977,7 @@ var cnames_active = {
   "mmcq": "nikola.github.io/MMCQ", // noCF? (don´t add this in a new PR)
   "mo": "mirmousaviii.github.io/mo",
   "mobx": "mobxjs.github.io/mobx",
+  "mobx-react": "mobx-react.netlify.com",
   "mock-middleware": "luobotang.github.io/mock-middleware",
   "mockjs-lite": "52cik.github.io/mockjs-lite", // noCF
   "mockyeah": "mockyeah.github.io/mockyeah",
