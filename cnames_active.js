b78861821cc17165ce94e55c16aca738fe64d9d8
Sanjeev yadav <sanjeevy133@protonmail.com>
2019-05-22 13:32:21 +0530

    arcn.js.org
    React native boiler plate code based on Atomic design

--- cnames_active.js
+++ cnames_active.js
@@ -133,6 +133,7 @@ var cnames_active = {
   "argo": "albertosantini.github.io/argo", // noCF? (don´t add this in a new PR)
   "arief": "1997arief.github.io",
   "arime": "ninbryan.github.io/arime", // noCF? (don´t add this in a new PR)
+  "arcn": "alexakasanjeev.github.io/arcn",
   "artalk": "qwqcode.github.io/Artalk",
   "artery": "arteryjs.github.io/gh-pages", // noCF? (don´t add this in a new PR)
   "arthurmbandeira": "arthurmbandeira.github.io",
