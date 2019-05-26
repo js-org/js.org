406384748f6823a90bf4ffd46ff8a222016327f5
Albert Abdulmanov <albert.abdu@gmail.com>
2019-05-26 17:01:57 +0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -90,6 +90,7 @@ var cnames_active = {
   "akshay": "akshaygulhane.github.io",
   "akshaybhalotia": "akshaybhalotia.github.io",
   "alasql": "alasql.github.io", // noCF? (don´t add this in a new PR)
+  "albertmolodec": "albertmolodec.github.io", // noCF? (don´t add this in a new PR)
   "alfred": "amilajack.github.io/alfred",
   "algebra": "nicolewhite.github.io/algebra.js", // noCF? (don´t add this in a new PR)
   "ali": "alibouhrouche.github.io",
