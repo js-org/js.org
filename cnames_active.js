a0f5059208971af30de3dcca61906dc63bcd231c
Jeff Tian <jeff.tian@outlook.com>
2019-05-26 23:01:10 +0800

    qbit.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1234,6 +1234,7 @@ var cnames_active = {
   "purpzie": "purpzie.github.io/site",
   "pwa-cookbook": "sylvainpolletvillard.github.io/pwa-cookbook",
   "pwa-workshop": "sylvainpolletvillard.github.io/pwa-workshop",
+  "qbit": "fudan-mse.github.io/oms-frontend",
   "qs": "kirjs.github.io/qs.js", // noCF? (don´t add this in a new PR)
   "quark": "smarchetti.github.io/quark",
   "querybuilder": "mistic100.github.io/jQuery-QueryBuilder", // noCF? (don´t add this in a new PR)
