d1bc908aac8a8b1cacc8f9a25d6e544c9d669773
小青年 <zhaomenghuan@agree.com.cn>
2019-05-29 18:31:17 +0800

    aemp.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -99,6 +99,7 @@ var cnames_active = {
   "alys": "rmjordas.github.io/alys",
   "amaple": "amjs-team.github.io/amaple",
   "amorphous": "ariabuckles.github.io/amorphous",
+  "aemp": "afexteam.github.io/aemp",
   "amp": "afexteam.github.io/amp",
   "anenth": "anenth.github.io", // noCF? (don´t add this in a new PR)
   "angular-jsf": "json-schema-faker.github.io/angular-jsf", // noCF? (don´t add this in a new PR)
