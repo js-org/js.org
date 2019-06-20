d108e6f3f32fa2954fe8cccfb9d9f1e092e86997
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2019-06-20 19:48:32 +0100

    Alphabetical

--- cnames_active.js
+++ cnames_active.js
@@ -1221,8 +1221,8 @@ var cnames_active = {
   "plan": "agauniyal.github.io/plan",
   "planisphere": "dai-shi.github.io/planisphere-site", // noCF? (don´t add this in a new PR)
   "planner": "openplannerteam.github.io/planner.js",
-  "platz": "fylipp.github.io/platz.js",
   "plasma": "henrymartins.github.io/plasma", // noCF
+  "platz": "fylipp.github.io/platz.js",
   "player": "player.netlify.com",
   "plexr": "plexrbot.github.io",
   "pliers": "pliersjs.github.io", // noCF? (don´t add this in a new PR)
