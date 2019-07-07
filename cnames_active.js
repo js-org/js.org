db735d6294fe3c698eaabd6f066b8b7b5690e194
Nikita Karamov <nick@karamoff.ru>
2019-07-07 03:14:40 +0300

    Add vavilon.js website

--- cnames_active.js
+++ cnames_active.js
@@ -1733,6 +1733,7 @@ var cnames_active = {
   "vanessa": "vanessa219.github.io/vanessa",
   "vanilla": "haeresis.github.io/vanilla-js-dom",
   "vanilla-picker": "sphinxxxx.github.io/vanilla-picker",
+  "vavilon": "vavilon-js.github.io",
   "vayne": "vaynejs.github.io",
   "vbuild": "egoist.github.io/vbuild",
   "ve-table": "xiguaxigua.github.io/ve-table",
