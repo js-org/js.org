633b3102bc41518d11bea9abca57d8a3488a56ba
Gurjit Singh <g.s@outlook.in>
2019-07-23 19:42:21 +0530

    singh.js.org
    points to gxjit.github.io

--- cnames_active.js
+++ cnames_active.js
@@ -1532,6 +1532,7 @@ var cnames_active = {
   "sina": "sinabakh.github.io/sina",
   "single-page-web-app": "lukejpreston.github.io/single-page-web-app",
   "single-spa": "canopytax.github.io/single-spa.js.org",
+  "singh": "gxjit.github.io",
   "sirkit": "seckwei.github.io/SirKit_CircuitSim", // noCF? (don´t add this in a new PR)
   "sistersbio": "KDiaCodes.github.io/SistersBio",
   "sizle": "christoga.github.io/sizle", // noCF? (don´t add this in a new PR)
