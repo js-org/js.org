fcfe3a970587fde01995bc06818dd641fe0ed214
Edwin <edwinyosorahardjo@hotmail.com>
2019-07-28 20:04:49 +1000

    bible.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -215,6 +215,7 @@ var cnames_active = {
   "bestof": "michaelrambeau.github.io/bestofjs",
   "between": "na-west1.surge.sh",
   "bfd": "mrsheldon.github.io/bfd.js",
+  "bible": "edwinyosorahardjo.github.io/bible",
   "bind-action-dispatchers": "cchamberlain.github.io/bind-action-dispatchers", // noCF? (don´t add this in a new PR)
   "birb": "purpzie.github.io/birb.js.org",
   "biu": "aprilorange.github.io/biu", // noCF? (don´t add this in a new PR)
