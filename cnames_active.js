6ecd024934e0de1c520041561dc40584522e1642
contributionls <52841657+contributionls@users.noreply.github.com>
2019-07-29 08:03:38 +0800

    matters2ipfs.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1899,7 +1899,8 @@ var cnames_active = {
   "zombie": "assaf.github.io/zombie", // noCF? (don´t add this in a new PR)
   "zonayed": "zonayedpca.github.io/js.zonayed.me",
   "zty": "zetaoyang.github.io",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "matters2ipfs": "contributionls.github.io/matters2ipfs",
   /*
   * please don't add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
