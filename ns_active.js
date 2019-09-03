a2e5aed8b805fac4268696c2575ac93aa79dc4d6
Stefan Keim <indus@users.noreply.github.com>
2019-09-03 21:08:17 +0200

    Update ns_active.js

--- ns_active.js
+++ ns_active.js
@@ -13,6 +13,6 @@
  */
  
  var ns_active = {
-  "shebang": ["dns1.p01.nsone.net", "dns2.p01.nsone.net", "dns3.p01.nsone.net", "dns4.p01.nsone.net"],
-  "engine262": ["a.zeit-world.org", "c.zeit-world.net", "d.zeit-world.co.uk", "e.zeit-world.com"]
+  "engine262": ["a.zeit-world.org", "c.zeit-world.net", "d.zeit-world.co.uk", "e.zeit-world.com"],
+  "shebang": ["dns1.p01.nsone.net", "dns2.p01.nsone.net", "dns3.p01.nsone.net", "dns4.p01.nsone.net"]
  }
