66130f57c825f1518f255ed5d700e4a5497de569
Divy Srivastava <dj.srivastava23@gmail.com>
2019-09-05 17:53:40 +0530

    Add torpedo.js
    Add https://divysrivastava.github.io/torpedo.js/docs as torpedo.js.org.
    Thank you!

--- cnames_active.js
+++ cnames_active.js
@@ -1750,6 +1750,7 @@ var cnames_active = {
   "tori": "boostcode.github.io/torijs",
   "torino": "torinojs.github.io", // noCF? (don´t add this in a new PR)
   "torlondev": "torlondev.github.io",
+  "torpedo": "divysrivastava.github.io/torpedo.js/docs",
   "tp": "jottocraft.github.io/tp",
   "trace": "andreasmadsen.github.io/trace",
   "transposer": "francesco-dipi.github.io/transposer",
