b168c1b7bcf14d567a4547d8d12c0dac22244094
Stefan Keim <indus@users.noreply.github.com>
2019-09-14 20:04:58 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -418,7 +418,7 @@ var cnames_active = {
   "davet": "egecelikci.github.io/davet",
   "davidtaylorjr": "davidtaylorjr.github.io",
   "dawn": "dawnelixir.github.io/dawn.js.org",
-  "dawood": "https://github.com/MohameDawood/Dawood",
+  "dawood": "mohamedawood.github.io/Dawood",
   "day": "iamkun.github.io/dayjs",
   "daysfromnow": "pedrokost.github.io/daysfromnow", // noCF? (don´t add this in a new PR)
   "dbar": "dbarjs.github.io",
