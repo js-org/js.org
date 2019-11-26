f047d83e806b01e82c63b33ecb55b5fc1821b8c6
Kavan Mevada <kavanmevada@gmail.com>
2019-11-26 21:51:52 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1499,6 +1499,7 @@ var cnames_active = {
   "rebem": "rebem.github.io", // noCF? (don´t add this in a new PR)
   "recover": "luisvallejomohl.github.io/Recover.js",
   "recycle": "recyclejs.github.io/recycle",
+  "redfox": "kavanmevada.github.io",
   "redis": "noderedis.github.io/node_redis", // noCF? (don´t add this in a new PR)
   "redline": "abduvokhid.github.io/redline",
   "redom": "redom.github.io/redom-docs",
