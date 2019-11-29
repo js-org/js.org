d6d8d69e3eb4caddf71bc0c0e96387aff67c72e5
Ones <33573384+bailihuiyue@users.noreply.github.com>
2019-11-29 13:08:43 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -88,6 +88,7 @@ var cnames_active = {
   "agrawalnaman": "agrawalnaman.github.io",
   "agrawalrohit": "rohit0803.github.io/agrawal.github.io",
   "ahmad": "aruzikulov.github.io/ahmad",
+  "ai": "xv700.github.io/ai",
   "airtable-plus": "victorhahn.github.io/airtable-plus",
   "ais": "yunyoujun.github.io/ais.js",
   "ajaxable": "artf.github.io/ajaxable",
