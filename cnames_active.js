003fa8d299e78856d1ce6d4f3e4c21b8c1df6f92
namelessjs <57930535+namelessjs@users.noreply.github.com>
2019-12-14 18:38:35 -0600

    Added 'sigma.js.org' to cname_list

--- cnames_active.js
+++ cnames_active.js
@@ -39,6 +39,7 @@
  */
 
 var cnames_active = {
+  "sigma": "https://namelessjs.github.io/sigma.js/",
   "": "js-org.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
