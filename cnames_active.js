f91c5fabe3cf674c63fa3db447795af49d71fecf
Lachlan D-K (TheDarkNova) <lachlandufort-kennett@outlook.com>
2019-12-23 22:14:43 +0000

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1295,6 +1295,7 @@ var cnames_active = {
   "pguth": "pguth.github.io",
   "ph1p": "ph1p.github.io",
   "pharaoh": "pharaoh-js.github.io", // noCF? (don´t add this in a new PR)
+  "phasors-demo": "lachlandk.github.io/phasors-demo",
   "phi": "phi-tools.netlify.com",
   "phobos": "phobosjs.github.io/phobos.js",
   "phoenix35": "phoenix35.github.io/js-help",
