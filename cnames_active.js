2c3f6e8a55ac3568d67db74399fc8b2ee2ae0dd2
Andre Jarrell <mail@andre.gg>
2020-01-03 15:29:14 -0800

    statsbot.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1683,7 +1683,6 @@ var cnames_active = {
   "state-machine-cat": "sverweij.github.io/state-machine-cat",
   "staticinstance": "staticinstance.github.io", // noCF? (don´t add this in a new PR)
   "stats": "js-org.github.io/stats.js.org",
-  "statsbot": "statsbot.netlify.com", // noCF
   "status": "mattipv4.github.io/status-codes",
   "steemsites": "yhozen.github.io/steemsites",
   "stego": "zeruniverse.github.io/CryptoStego", // noCF? (don´t add this in a new PR)
