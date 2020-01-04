4215ee7e984e58537c775ca88c9b28a8a3779ab1
Mikael Karon <mikael@karon.se>
2020-01-04 02:07:01 +0100

    add mimeo.js.org for mimeojs.github.io

--- cnames_active.js
+++ cnames_active.js
@@ -1081,6 +1081,7 @@ var cnames_active = {
   "mikado": "nextapps-de.github.io/mikado",
   "milesgitgud": "milesgitgud.github.io/homepage",
   "mimic": "500tech.github.io/mimic",
+  "mimeo": "mimeojs.github.io"
   "mina": "CenturyUna.github.io/mina",
   "mindyourtime": "michaloslav.github.io/Mind-Your-Time",
   "minecraft-data.prismarine": "prismarinejs.github.io/minecraft-data",
