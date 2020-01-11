8b49baefebfaf39a2134f308003463b0724e9f83
Joshua Sing <25740108+joshuasing@users.noreply.github.com>
2020-01-11 13:13:39 +1100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1004,6 +1004,7 @@ var cnames_active = {
   "lychee": "Artificial-Engineering.github.io/lycheeJS-website",
   "lyra": "amansahil.github.io/lyra.js.org",
   "lzbible": "edwinyosorahardjo.github.io/lzbible",
+  "minijoshua": "joshualovescode.github.io/MiniJoshua-Website/",
   "m01i0ng": "m01i0ng.github.io",
   "m8bot": "mapreiff.github.io/m8-bot-site",
   "ma124": "ma124.netlify.com",
