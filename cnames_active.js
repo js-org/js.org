d2877875d4fe2c3e55c24447faab3e093d94dd4d
eren885 <58482989+eren885@users.noreply.github.com>
2020-01-24 19:35:50 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2036,8 +2036,7 @@ var cnames_active = {
   "zonayed": "zonayedpca.github.io/js.zonayed.me",
   "zplugins": "zlanguage.github.io/plugin-docs",
   "zty": "zetaoyang.github.io",
-  "zyy": "zyyou.github.io/notes",
-  "eren": "eren885.github.io"
+  "zyy": "zyyou.github.io/notes"
   /*
   * please don't add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
