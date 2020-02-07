a25c296c80945c6b8fb1af002edbab46eafa2e64
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2020-02-07 11:50:16 +0000

    Alphabetical (p before s)

--- cnames_active.js
+++ cnames_active.js
@@ -1798,8 +1798,8 @@ var cnames_active = {
   "thehorizon": "kavanmevada.github.io/the-horizon-blog",
   "themoviedb": "chrisenytc.github.io/themoviedb", // noCF? (don´t add this in a new PR)
   "theorem": "arguiot.github.io/TheoremJS",
-  "theshellproject": "theshellproject.github.io",
   "thepeachtimes": "thepeachtimes.github.io",
+  "theshellproject": "theshellproject.github.io",
   "thi": "ngomjnhthj.github.io/thi",
   "thinkdigital": "tomowens08.github.io/thinkdigital",
   "threads": "andywer.github.io/threads.js",
