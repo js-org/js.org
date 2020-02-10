12781cd5bfd27213ff067fb29169207174602181
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2020-02-10 21:37:12 +0000

    Alphabetical (k before n)

--- cnames_active.js
+++ cnames_active.js
@@ -216,8 +216,8 @@ var cnames_active = {
   "becarefulwith": "xMikee1.github.io/becarefulwith",
   "begin": "advanced-webapps-class.github.io/begin", // noCF? (don´t add this in a new PR)
   "benjick": "benjick.github.io",
-  "bernard": "ojengwa.github.io",
   "berk": "berkelmas.github.io",
+  "bernard": "ojengwa.github.io",
   "berzan": "newbie13xd.github.io/berzan",
   "besafe": "ma124.github.io/BeSafe",
   "bestof": "michaelrambeau.github.io/bestofjs",
