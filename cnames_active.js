8725a2ea2fdd4d1b64af578a29098deb05a4a7c8
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2020-02-26 22:13:06 +0000

    Alphabetical, - before r

--- cnames_active.js
+++ cnames_active.js
@@ -1411,8 +1411,8 @@ var cnames_active = {
   "prototyped": "ardalanamini.github.io/prototyped.js",
   "protype": "arguiot.github.io/ProType",
   "ps": "spindlyskit.github.io/ps.js",
-  "ptrdo": "ptrdo.github.io",
   "pt-pt.single-spa": "single-spa.github.io/pt-pt.single-spa.js.org", // noCF
+  "ptrdo": "ptrdo.github.io",
   "pubg": "ickerio.github.io/pubg.js",
   "publish-subscribe": "r37r0m0d3l.github.io/publish_subscribe",
   "published": "fiverr.github.io/published",
