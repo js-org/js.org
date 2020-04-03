8078a82894fb0c13beac7bb7de41dae33041d77a
Dafitrah <61631078+Dafitrah@users.noreply.github.com>
2020-04-03 20:53:05 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -435,6 +435,7 @@ var cnames_active = {
   "danmaku": "weizhenye.github.io/Danmaku",
   "danmol": "dan12mol.github.io", // noCF? (don´t add this in a new PR)
   "dap": "jooher.github.io/dap",
+  "dapit": "dafitrah.github.io",
   "darshit": "dgupta777.github.io",
   "data-dashboard": "dnajs.github.io/data-dashboard",
   "data-migration": "thebenforce.github.io/data-migration",
@@ -2145,7 +2146,6 @@ var cnames_active = {
   "zplugins": "zlanguage.github.io/plugin-docs",
   "zty": "zetaoyang.github.io",
   "zyy": "zyyou.github.io/notes"
-  "dapit": "dafitrah.github.io",
   /*
   * please don't add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
