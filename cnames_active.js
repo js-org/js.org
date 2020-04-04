5355af913e9d007ba079fcfff954e46a08363d84
Syaufy <46257169+Syauqizaidan@users.noreply.github.com>
2020-04-04 18:17:04 +0700

    Add my list on it

--- cnames_active.js
+++ cnames_active.js
@@ -1847,6 +1847,7 @@ var cnames_active = {
   "sydinh": "sydinh.github.io",
   "synth": "lukehorvat.github.io/synth-mood",
   "syr": "dmikey.github.io/syr",
+  "syaauqi": "syauqizaidan.github.io",
   "tab": "gdmcc.github.io/tab",
   "tagster": "goschevski.github.io/tagster", // noCF? (don´t add this in a new PR)
   "taha": "orcxzjeeeee.github.io/taha",
