e4b49a2381d3c0141e7dec89fcf96ca5a09ece42
Giulio De Matteis <giuliod35@gmail.com>
2020-04-14 13:45:12 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -593,6 +593,7 @@ var cnames_active = {
   "emre": "emrekara37.github.io",
   "emulisp": "grahack.github.io/EmuLisp", // noCF? (don´t add this in a new PR)
   "enclave": "eanplatter.github.io/enclave", // noCF? (don´t add this in a new PR)
+  "endersquid": "endersquidjs.github.io",
   "endb": "chroventer.github.io/endb",
   "endpoint-imposter": "lukaszmakuch.github.io/endpoint-imposter",
   "energy": "energychain.github.io/energy",
