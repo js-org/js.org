9b4128638d07f3414e78483f7b33938269f6c82f
Sergey Sova <5620073+sergeysova@users.noreply.github.com>
2020-04-15 10:49:34 +0300

    add hegel.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -802,6 +802,7 @@ var cnames_active = {
   "hay": "hayjs.github.io/hay.js.org",
   "hbase": "adaltas.github.io/node-hbase-docs",
   "heartseekers": "rajington.github.io/heartseekers", // noCF? (don´t add this in a new PR)
+  "hegel": "jsmonk.github.io/hegel",
   "hello": "hello-js-org.github.io", // noCF? (don´t add this in a new PR)
   "helper": "tbnritzdoge.github.io/helperweb",
   "hibiki": "claritymoe.github.io/hibiki",
