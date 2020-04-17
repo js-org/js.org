f8c30c7a764c2f619b7057ebd1bfb9a90580285a
davidm5375 <dovidm5375@gmail.com>
2020-04-17 11:38:43 -0400

    simpleserver.js.org

--- ns_active.js
+++ ns_active.js
@@ -18,5 +18,6 @@
   "engine262": ["ns5.he.net", "ns4.he.net", "ns3.he.net", "ns2.he.net"],
   "ruby": ["ns11.cloudns.net", "ns12.cloudns.net", "ns13.cloudns.net", "ns14.cloudns.net"],
   "samplasion": ["ns31.cloudns.net", "ns32.cloudns.net", "ns33.cloudns.net", "ns34.cloudns.net"],
+  "simpleserver": ["ns31.cloudns.net", "ns32.cloudns.net", "ns33.cloudns.net", "ns34.cloudns.net"],
   "shebang": ["dns1.p01.nsone.net", "dns2.p01.nsone.net", "dns3.p01.nsone.net", "dns4.p01.nsone.net"]
  }
