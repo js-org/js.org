e9336221d5a7f72e0d33e42b77edc3f8ec8b1239
guzhou <guzhou86@gmail.com>
2020-04-22 15:13:25 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -49,7 +49,6 @@ var cnames_active = {
   "2tube": "you2php.github.io/you2php",
   "3d-go": "3d-go.netlify.com",
   "404": "licshee.github.io/404",
-  "588": "588.biz.st",
   "766": "766.github.io",
   "7anshuai": "7anshuai.github.io", // noCF? (don´t add this in a new PR)
   "98": "1j01.github.io/98",
