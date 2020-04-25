733475b38113ad1c4f6e8901237a4784b495c460
Gino <44017640+LolWastedJS@users.noreply.github.com>
2020-04-25 09:35:40 -0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1033,6 +1033,7 @@ var cnames_active = {
   "laubstein": "laubstein.github.io", // noCF? (don´t add this in a new PR)
   "laue": "qingwei-li.github.io/laue",
   "lava": "palladium.github.io/Lava.js",
+  "lavaclient": "lavaclient.github.io/lavaclient",
   "laveesh": "laveesh.github.io",
   "lazycipher": "lazycipher.github.io",
   "lcookie": "lf112.github.io/lcookie",
