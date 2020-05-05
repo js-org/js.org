5734ce81bcb049edebef407dbb055672b030d8b2
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2020-05-05 10:13:09 +0100

    Alphabetical

--- cnames_active.js
+++ cnames_active.js
@@ -1713,10 +1713,10 @@ var cnames_active = {
   "salvacam": "salvacam.github.io",
   "salvattore": "rnmp.github.io/salvattore",
   "sam": "jdubray.github.io/sam", // noCF? (don´t add this in a new PR)
-  "sampoder": "sampoder.github.io",
   "samlify": "tngan.github.io/samlify",
   "samply": "yury-shevchenko.github.io/samply",
   "samplyr": "arze1.github.io/samplyr-site",
+  "sampoder": "sampoder.github.io",
   "sanctuary": "sanctuary-js.github.io/sanctuary-site",
   "sanddance": "microsoft.github.io/SandDance",
   "sandeepkhandewale": "learner1024.github.io",
