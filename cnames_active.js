0ed3cb575f48013b8037e607b2644a6e3a93da6c
guzhou <guzhou86@gmail.com>
2020-05-07 22:22:32 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2026,6 +2026,7 @@ var cnames_active = {
   "turali": "caglarturali.github.io",
   "turbo": "turbo.github.io",
   "tux": "tux.gitbooks.io/docs",
+  "tv": "guzhou.github.io",
   "twcopy": "the94air.github.io/twcopy",
   "tweed": "tweedjs.github.io",
   "tweetnacl": "dchest.github.io/tweetnacl-js",
