bb01cfbaa52a45c50ddb0a9b017afbde91d260a2
이강준 <admin@gangjun.dev>
2020-05-10 22:31:25 +0900

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -503,6 +503,7 @@ var cnames_active = {
   "diffract": "ameyms.github.io/diffract", // noCF? (don´t add this in a new PR)
   "dinesh": "dineshondev.github.io/dinesh", // noCF? (don´t add this in a new PR)
   "dinos": "0xflotus.github.io/dinos",
+  "disclists": "disclists.github.io",
   "discord": "discordjs.github.io/website",
   "discord-anti-spam": "michael-j-scofield.github.io/discord-anti-spam",
   "discord-giveaways": "androz2091.github.io/discord-giveaways",
