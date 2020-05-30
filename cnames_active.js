9580715cc3c405fb195d83a1c0f131e765d0d9ec
Stefan Keim <indus@users.noreply.github.com>
2020-05-30 20:34:51 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1898,7 +1898,7 @@ var cnames_active = {
   "split": "nathancahill.github.io/split",
   "split-ease": "split-ease.netlify.com", // noCF
   "splitting": "shshaw.github.io/Splitting",
-  "spotistats": "https://spotistats-app.netlify.app",
+  "spotistats": "spotistats-app.netlify.app",
   "spotix": "sawyerbx.github.io/spotix",
   "spreadsheet": "chiefofgxbxl.github.io/Spreadsheet.js",
   "spring": "hosting.gitbook.com",
