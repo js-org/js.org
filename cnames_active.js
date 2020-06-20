620ecd0447d66970e966e631781f05666947f8d4
FireBlast <fireblast901@gmail.com>
2020-06-20 17:15:15 -0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -708,6 +708,7 @@ var cnames_active = {
   "fikri": "ikrydev.github.io/fikri.js.org",
   "filer": "filerjs.github.io/filer",
   "finder": "applait.github.io/finderjs", // noCF? (don´t add this in a new PR)
+  "fireblast": "thefireblast.github.io",
   "fire-hydrant": "cchamberlain.github.io/fire-hydrant", // noCF? (don´t add this in a new PR)
   "firenze": "fahad19.github.io/firenze", // noCF? (don´t add this in a new PR)
   "fireorm": "wovalle.github.io/fireorm",
