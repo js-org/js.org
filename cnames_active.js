8479a4532040ffeb1a19e62dbdc37db1f9e402a1
Lee <48642087+lifeiyang79@users.noreply.github.com>
2020-06-24 23:05:26 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1112,6 +1112,7 @@ var cnames_active = {
   "leste": "atwood-cai.github.io/leste.js", // noCF? (don´t add this in a new PR)
   "level": "xv700.github.io/level",
   "lf": "lf112.github.io",
+  "lifeiyoung": "lifeiyang79.github.io",
   "lgs": "lgsturkiye.github.io/lgs",
   "li": "mattsimmons1.github.io/li",
   "lifx": "fncxpro.github.io/lifx.js",
