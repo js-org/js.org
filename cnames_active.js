6c26eeb2138649d9cb0c74a8734993c9880ea2bd
thevagus <codevagus@gmail.com>
2020-07-02 00:59:10 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2177,8 +2177,8 @@ var cnames_active = {
   "v4.webpack": "webpack.github.io/v4.webpack.js.org", // noCF
   "v4f": "v4f.netlify.com",
   "vac": "mlinquan.github.io/vue-awesome-countdown",
-  "valentin": "valentinvieriu.github.io", // noCF? (don´t add this in a new PR)
   "vagus": "thevagus.github.io",
+  "valentin": "valentinvieriu.github.io", // noCF? (don´t add this in a new PR)
   "validarium": "lundegaard.github.io/validarium",
   "validator": "ppoffice.github.io/validator.js", // noCF? (don´t add this in a new PR)
   "valine": "xcss.github.io/valine",
