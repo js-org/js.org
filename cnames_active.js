63552c1254de41d7647c7c4c5d75f0f7f0f4a352
xuanzhi33 <37460139+xuanzhi33@users.noreply.github.com>
2020-07-04 21:03:05 +0800

    paper.js.org
     There is reasonable content on the page (see: No Content)
     I have read and accepted the Terms and Conditions.
    This website has stories, jokes, famous sentences and so on (in Chinese).
    Thank you : )

--- cnames_active.js
+++ cnames_active.js
@@ -1491,6 +1491,7 @@ var cnames_active = {
   "pangkeyd": "pangkeyd.github.io",
   "pantarei": "pantareijs.github.io/pantarei.js.org",
   "panza": "panza-org.github.io/panza-docs", // noCF
+  "paper": "xuanzhi33.github.io/simplepaper/",
   "paperclip": "crcn.github.com/paperclip.js.org", // noCF? (don´t add this in a new PR)
   "paraiba": "paraibajs.github.io", // noCF? (don´t add this in a new PR)
   "parallel": "parallel-js.github.io/parallel.js",
