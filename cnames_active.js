61ab2dbaca36d20977a85d3ff63dfc5027129911
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2020-07-08 13:05:54 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1574,6 +1574,7 @@ var cnames_active = {
   "polygon-clipping": "mfogel.github.io/polygon-clipping",
   "polythene": "arthurclemens.github.io/Polythene", // noCF? (don´t add this in a new PR)
   "pomidorus": "tatyshev.github.io/pomidorus",
+  "pong-zh": "https://xiaozhu2021.github.io/javascript-pong/",
   "popcorn-api": "nirewen.github.io/popcorn-api",
   "popper": "fezvrasta.github.io/popper.js",
   "portafolioacadeller": "acadeller.github.io/portafolioacadeller", 
