d39c7ad107e3f2715f03dfa615bb1d79f0512f6d
Itzik <37775846+PatentLobster@users.noreply.github.com>
2020-07-08 21:21:39 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -974,7 +974,7 @@ var cnames_active = {
   "italia": "italia-js.github.io",
   "itsashis4u": "itsashis4u.github.io", // noCF? (don´t add this in a new PR)
   "itunes-bridge": "angrykiller.github.io/iTunes-bridge",
-  "itzik": "itzik.co",
+  "itzik": "itzik.netlify.app",
   "iutdb": "xchairs.github.io/iutdb.github.io",
   "ivy": "sinistrecyborg.github.io/Ivy",
   "iwanttocountfromone": "realcyguy.github.io/iwanttocountfromone.js",
