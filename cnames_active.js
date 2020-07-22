f3bf1b9298bce9d24cf24c84d5cda42d52fae06e
Tintin <lehuading@qq.com>
2020-07-22 16:21:58 +0800

    add subdomain tin.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2411,7 +2411,8 @@ var cnames_active = {
   "zty": "zetaoyang.github.io",
   "zyh": "zyh-chopper.github.io/zyh"
   "zykj": "zykjofficial.github.io",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "tin": "lhtin.github.io/tin"
   /*
   * please don't add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
