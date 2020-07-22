04a921645cf2e55f69b748288330201311594c59
彭睿扬 <65233781+pengruiyang-cpu@users.noreply.github.com>
2020-07-22 17:37:53 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2411,7 +2411,8 @@ var cnames_active = {
   "zty": "zetaoyang.github.io",
   "zyh": "zyh-chopper.github.io/zyh"
   "zykj": "zykjofficial.github.io",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "pengruiyang-cpu": "pengruiyang-cpu.github.io"
   /*
   * please don't add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
