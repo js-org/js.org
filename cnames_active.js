97ae62d8093cf0b8ec0360f7d43c4ed0361f1e12
Stefan Keim <indus@users.noreply.github.com>
2020-08-13 08:45:30 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1638,8 +1638,8 @@ var cnames_active = {
   "pong-zh": "xiaozhu2021.github.io/javascript-pong",
   "popcorn-api": "nirewen.github.io/popcorn-api",
   "popper": "fezvrasta.github.io/popper.js",
-  "portafolioarnulfo": "arpior.github.io/Portafolio/",
   "portafolioacadeller": "acadeller.github.io/portafolioacadeller",
+  "portafolioarnulfo": "arpior.github.io/Portafolio",
   "portafolioluis": "luisself2020.github.io/portafolioluis",
   "portainer-update": "flweber.github.io/portainer-stack-updater",
   "porter": "ahopkins.github.io/porterjs", // noCF? (don´t add this in a new PR)
