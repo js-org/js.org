6fc62577137fd46e74a12dcf62232541f79bcf15
Arshia <69681529+Mad880@users.noreply.github.com>
2020-08-17 00:34:05 +0430

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -502,6 +502,7 @@ var cnames_active = {
   "d-patterns": "alias.zeit.co", // noCF
   "d4": "joelburget.github.io/d4",
   "daily.tmr": "ttttmr.github.io/daily", // noCF
+  "daystride": "mad880.github.io/DayStride"
   "daksh0225": "daksh0225.github.io",
   "damo": "damodharanj.github.io",
   "dan": "danielh-dev.netlify.com",
