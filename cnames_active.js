3020f399afe1d5881f17741d66d06d3f8619b099
Daniel Saputra <danielwetan.io@gmail.com>
2020-08-18 22:56:16 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -290,6 +290,7 @@ var cnames_active = {
   "bolao": "ziyou3012.github.io",
   "boldom": "marcisbee.github.io/boldom",
   "bombsweeper": "pakastin.github.io/bombsweeper",
+  "bonjour": "danielwetan.github.com/bonjour-web",
   "bonzibuddy": "fardindadev.github.io/bonzibuddy",
   "bookmarklets": "novadevelopment.github.io/bookmarklets.js",
   "bool": "booljs.github.io",
