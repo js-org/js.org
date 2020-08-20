e6ef656ee11e08f4ecfd9149b58efa9ea5b69671
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2020-08-20 11:03:56 +0100

    Alphabetical

--- cnames_active.js
+++ cnames_active.js
@@ -1968,8 +1968,8 @@ var cnames_active = {
   "shortquery": "s--minecraft.gitbooks.io/shortquery-js", // noCF? (don´t add this in a new PR)
   "showroom": "showroom-js.herokuapp.com",
   "shscode": "shs-coding-club-projects.github.io",
-  "sidekik": "inf3cti0n95.github.io/sidekik",
   "siddharth": "chaudhs769.github.io/siddharth.js.org",
+  "sidekik": "inf3cti0n95.github.io/sidekik",
   "sights": "sfxrescue.github.io/sights",
   "silas229": "silas229.github.io",
   "silfr": "silfr.github.io/iterativecolor",
