105db4eca917e580492fc9cc66c50ee6cf491d20
blogimg <60996609+blogimg@users.noreply.github.com>
2020-08-22 09:39:21 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -683,6 +683,7 @@ var cnames_active = {
   "emoji-button": "joeattardi.github.io/emoji-button",
   "emojifier": "badlydrawnletters.github.io/emojifier-docs",
   "emojipanel": "danbovey.github.io/EmojiPanel",
+  "emotion": "blogimg.github.io",
   "empathy": "the94air.github.io/empathy-docs",
   "emre": "emrekara37.github.io",
   "emulisp": "grahack.github.io/EmuLisp", // noCF? (don´t add this in a new PR)
