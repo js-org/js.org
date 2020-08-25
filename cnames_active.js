d51a0e4f7939270b12265a9743c18c1e6402f506
Chen <xiaoqi77614@icloud.com>
2020-08-26 04:03:55 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1712,6 +1712,7 @@ var cnames_active = {
   "pzero": "hanyaku.github.io/ProjectZERO",
   "qbit": "fudan-mse.github.io/qbit",
   "qbsz": "qbsz.github.io",
+  "blog.qbsz": "qbsz.github.io",// noCF? (don´t add this in a new PR)
   "qs": "kirjs.github.io/qs.js", // noCF? (don´t add this in a new PR)
   "quacky": "quacky-bot.github.io",
   "quantlib": "quantlibjs.github.io",
