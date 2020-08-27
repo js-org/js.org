d1b1c832fbe80c8a6f1fc92f74f235b0ca686472
Marcus Ataide <marckaraujo86@gmail.com>
2020-08-27 11:29:01 -0300

    Add CNAME startup

--- cnames_active.js
+++ cnames_active.js
@@ -2095,6 +2095,7 @@ var cnames_active = {
   "starratio": "starratio.github.io/star-ratio", // noCF
   "start": "deepsweet.github.io/start",
   "startjs": "readthedocs.io",
+  "startup": "mataide.github.io/startup",
   "state-machine-cat": "sverweij.github.io/state-machine-cat",
   "staticinstance": "staticinstance.github.io", // noCF? (don´t add this in a new PR)
   "stats": "js-org.github.io/stats.js.org",
