68a74364c254e21c90b8282c66c279405b328bff
fabien huet <fabien.huet@gmail.com>
2020-09-02 13:51:54 +0200

    Update cnames_active.js
    Not a cname addition, but the file is missing comas. Lines 1121 and 1450.
    
    How is it parsed? The JSON errors should break the parsing.

--- cnames_active.js
+++ cnames_active.js
@@ -1117,7 +1117,7 @@ var cnames_active = {
   "karrot": "kemosaf.github.io/karrot",
   "kasfy": "kasfy.github.io/kasfy.js.org",
   "kate": "thecodesuite.github.io/katebot",
-  "katheesh": "katheesh.github.io/katheesh.js"
+  "katheesh": "katheesh.github.io/katheesh.js",
   "kavan": "kavanmevada.gitlab.io/intro",
   "kawaii": "moemoesoft.github.io/kawaii", // noCF
   "kcak11": "kcak11.github.io/js-org-web",
@@ -1446,7 +1446,7 @@ var cnames_active = {
   "mzaini30": "mzaini30.github.io",
   "mzplayer": "prince3661.github.io/Mzplayer",
   "n": "cyb.github.io/n",
-  "n3-rd": "n3-rd.github.io"
+  "n3-rd": "n3-rd.github.io",
   "nabin": "nabin6246.github.io",
   "naja": "jiripudil.github.io/Naja",
   "name-my-colors": "domcoleman.github.io/name-my-colors",
