c608cf099922fd30252a210a5d2bf862f31e8de7
云游君 <me@yunyoujun.cn>
2020-10-03 21:08:41 +0800

    add adv.js.org
    advjs.github.io

--- cnames_active.js
+++ cnames_active.js
@@ -91,6 +91,7 @@ var cnames_active = {
   "aditya": "aditya81070.github.io",
   "adnanbabakan": "adnanbabakan.github.io",
   "adon988": "adon988.github.io",
+  "adv": "advjs.github.io",
   "advancedrpc": "advancedrpc.github.io",
   "aemp": "afexteam.github.io/aemp",
   "aeon": "flazepe.github.io/aeon-web",
