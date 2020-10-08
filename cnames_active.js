a24b270813c4608779b5db2cc83e51c0c3108d56
xhemj <xhemj2680@163.com>
2020-10-08 17:55:48 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -440,9 +440,9 @@ var cnames_active = {
   "cn.mobx": "sangka.github.io/mobx-docs-cn", // noCF
   "cn.redux": "camsong.github.io/redux-in-chinese", // noCF? (don´t add this in a new PR)
   "cn.rx": "rxjs-cn.github.io/RxJS-Docs-CN", // noCF
+  "cn.mingyan": "lehs8n.coding-pages.com", //noCF
   "cnc": "cncjs.github.io/cncjs.org",
   "cndrew": "codemaster233.github.io",
-  "cn.mingyan": "lehs8n.coding-pages.com", //noCF
   "codebooky": "hiteshsubnani0128.github.io/codebooky",
   "codemade": "codemade.github.io", // noCF? (don´t add this in a new PR)
   "codepan": "egoist.github.io/codepan",
