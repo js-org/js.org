ab062cca607dd4a013bbfd88ba4e439a5d31a057
HCLonely <h1606051253@gmail.com>
2020-10-11 22:17:53 +0800

    change CNAME

--- cnames_active.js
+++ cnames_active.js
@@ -210,7 +210,7 @@ var cnames_active = {
   "audio-stream": "guywhogeek.github.io/audio-stream",
   "aui": "alauda.github.io/alauda-ui",
   "autarky": "pranshuchittora.github.io/autarky",
-  "auto-task-doc": "auto-task-doc.vercel.app",
+  "auto-task-doc": "cname.vercel-dns.com",
   "autodocs": "bguiz.github.io/autodocs", // noCF? (don´t add this in a new PR)
   "automic": "automicjs.github.io",
   "avi": "avinassh.github.io/avi", // noCF? (don´t add this in a new PR)
