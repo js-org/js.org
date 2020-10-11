79158c3c36a046985361c950af81280288a229aa
Focus Chen <i@focuschen.com>
2020-10-11 03:07:50 +0100

    Apply NS setup for hx.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -996,7 +996,6 @@ var cnames_active = {
   "hugorocaproyectos": "hugoroca.github.io/coleccion-proyectos",
   "human": "human-js.gitbooks.io", // noCF? (don´t add this in a new PR)
   "humble": "humblejs.github.io",
-  "hx": "hifocus.github.io/www.hxis.me",
   "hybrids": "hybridsjs.github.io/hybrids",
   "hyde": "gheek.github.io/hyde", // noCF? (don´t add this in a new PR)
   "hydro": "hydro-dev.github.io",
