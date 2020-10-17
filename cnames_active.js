3d29de23720a350fdba4bf2c376e5b10191498d3
lyertia <68829927+lyertia@users.noreply.github.com>
2020-10-17 03:41:20 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1317,7 +1317,7 @@ var cnames_active = {
   "lwjerri": "lwjerri.github.io",
   "lx": "blleng.github.io/lx",
   "lychee": "Artificial-Engineering.github.io/lycheeJS-website",
-  "lyertia": "lyertia.github.io",
+  "lyertia": "lyertias.github.io",
   "lyra": "amansahil.github.io/lyra.js.org",
   "lzbible": "edwinyosorahardjo.github.io/lzbible",
   "m01i0ng": "m01i0ng.github.io",
