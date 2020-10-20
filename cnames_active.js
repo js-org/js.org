5b798f1d58664d2f3f0a7134fdcc37fd5bd79701
Eze Gatica <35713330+ezegatica@users.noreply.github.com>
2020-10-20 10:02:25 -0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -787,6 +787,7 @@ var cnames_active = {
   "extenso": "theuves.github.io/extenso.js.org",
   "extraction": "rse.github.io/extraction", // noCF? (don´t add this in a new PR)
   "eye": "arguiot.github.io/EyeJS",
+  "eze": "ezegatica.github.io/eze",
   "faceapp": "negarang.github.io/face-app",
   "facepalm": "santiagogil.github.io/facepalm",
   "facreative": "facreative.github.io",
