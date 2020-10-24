500171a9e00a414d50448b11b6315170128b1a88
Stefan Keim <indus@users.noreply.github.com>
2020-10-24 20:27:14 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2493,7 +2493,7 @@ var cnames_active = {
   "vk-x": "vk-x.github.io",
   "vncz": "xvincentx.github.io/vncz",
   "volantis": "volantis-x.github.io",
-  "volantis-x": "volantis-x.github.io",
+  "volantis-x": "cname.vercel-dns.com", // noCF
   "voloshins": "voloshins.github.io", // noCF? (don´t add this in a new PR)
   "vormulir": "bramaudi.github.io/vormulir",
   "vorpal": "dthree.github.io/vorpal", // noCF? (don´t add this in a new PR)
