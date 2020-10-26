0bc7ab5586c176cd9170573b44403517066545cc
seatwork <mophisoft@gmail.com>
2020-10-26 23:21:37 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1823,7 +1823,6 @@ var cnames_active = {
   "quacky": "quacky-bot.github.io",
   "quantlib": "quantlibjs.github.io",
   "quark": "smarchetti.github.io/quark",
-  "que": "seatwork.github.io/que.js",
   "querybuilder": "mistic100.github.io/jQuery-QueryBuilder", // noCF? (don´t add this in a new PR)
   "question": "teamtofu.github.io/questionify",
   "quick-crud": "kingrayhan.github.io/quick-crud",
