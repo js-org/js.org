ef9ac59570c1d3b48b8a30adb66799f5b52846f9
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2020-10-27 13:25:53 +0100

    kek.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1190,6 +1190,7 @@ var cnames_active = {
   "kea": "kea.netlify.app",
   "keeping-up-with-javascript": "jasperv.github.io/keeping-up-with-javascript",
   "keerthisree": "naveenjujaray.github.io/jksblog",
+  "kek": "skorotkiewicz.github.io/kek",
   "kelvinho": "kelvin2go.github.io", // noCF? (don´t add this in a new PR)
   "kewitz": "kewitz.github.io",
   "keypress": "rumkin.github.io/keypress.js.org",
