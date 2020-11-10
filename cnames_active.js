14e37c8d609e33c113460fa4f8f0fab1d9e52088
一个开发 <62019172+yigekaifa@users.noreply.github.com>
2020-11-10 18:20:40 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2692,7 +2692,8 @@ var cnames_active = {
   "zygds": "zygds.github.io/zygds.js.org",
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "jiba": "yigekaifa.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
