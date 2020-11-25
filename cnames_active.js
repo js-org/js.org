3ca5c5c296db9358cc47ed5d0d4f8be82c189c56
José Carlos Zamora Gómez <66094426+JoseCarlosZamoraGomez@users.noreply.github.com>
2020-11-25 10:54:38 -0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1174,6 +1174,7 @@ var cnames_active = {
   "jose-qs": "jose-qs.github.io/Portafolio",
   "josep": "warlock.github.io",
   "JoseCarlosZamoraGomezPortfolio": "josecarloszamoragomez.github.io/Portfolio",
+  "JoseCarlosZamoraGomezCV": "josecarloszamoragomez.github.io/CV",
   "jparticles": "jparticles.github.io/Documentation",
   "js-fixerr": "anujsinghwd.github.io/js-fixerr",
   "jscord": "thepoptartcrpr.github.io/jscord",
