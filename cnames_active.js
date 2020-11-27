097ab4463ff86ed370c115793e6bcfbd2a3904fc
haoxihuan <75029730+haoxihuan@users.noreply.github.com>
2020-11-27 22:55:38 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -988,6 +988,7 @@ var cnames_active = {
   "gyps": "huijari.github.io/Gyps",
   "gyre": "wridder.github.io/GyreJS", // noCF? (don´t add this in a new PR)
   "gyx": "yourtion.github.io/gyx.js.org",
+  "haoxihuan": "haixihuan.github.io/haoxihuan.js",
   "h": "makenowjust.github.io/h.js",
   "h3": "h3rald.github.io/h3",
   "h7ml": "h7ml.github.io/web",
