a93146f2671cacb16a8319a3aba21bd7dd16fc3f
Adrian <adrianvelazquez528@outlook.com>
2020-12-03 19:53:30 -0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1845,6 +1845,7 @@ var cnames_active = {
   "portafolioarnulfo": "arpior.github.io/Portafolio",
   "portafolioluis": "luisself2020.github.io/portafolioluis",
   "portafoliorufino": "Rufino-Hdez.github.io/portafolio",
+  "Portafolio-web"; "velazquezadrian.github.io/Portafolio-web",
   "portainer-update": "flweber.github.io/portainer-stack-updater",
   "porter": "ahopkins.github.io/porterjs", // noCF? (don´t add this in a new PR)
   "post-type-discovery": "twozeroone.github.io/post-type-discovery",
