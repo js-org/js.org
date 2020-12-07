0c7a386bb6bdf4b4a664d4e7553bd54e09d6ac53
Adrien THIERRY <adrien-thierry@users.noreply.github.com>
2020-12-07 17:15:53 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1628,7 +1628,7 @@ var cnames_active = {
   "ncub8": "ncub8.github.io", // noCF? (don´t add this in a new PR)
   "neaps": "neaps.github.io",
   "nearley": "kach.github.io/nearley",
-  "nectar": "nectar-js-org.vercel.app",
+  "nectar": "cname.vercel-dns.com",
   "nector": "cname.vercel-dns.com",
   "nefter": "nefter.github.io",
   "nemaniarjun": "nemaniarjun.github.io",
