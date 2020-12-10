ba6ef3b002b08d5a79f2a1ac8a1eadac1ca2b9ea
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2020-12-10 09:38:58 +0000

    Lowercase subdomain

--- cnames_active.js
+++ cnames_active.js
@@ -1405,7 +1405,7 @@ var cnames_active = {
   "marcelo": "marcelooggero.github.io/bootstrap",
   "maridreycupcakes": "danyflores.github.io/MariDrey-CupCakes",
   "marie": "marie-js.github.io/MARIE.js",
-  "Maria-Teresa-Zamora-Gomez-Portafolio": "JoseCarlosZamoraGomez.github.io/Maria-Teresa-Zamora-G-mez",
+  "maria-teresa-zamora-gomez-portafolio": "JoseCarlosZamoraGomez.github.io/Maria-Teresa-Zamora-G-mez",
   "mark": "henry-luo.github.io/mark",
   "markbox": "markbox.netlify.com",
   "markdown-notepad": "jz222.github.io/markdown-notepad",
