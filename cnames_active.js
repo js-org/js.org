5c1ebdcd36782914640f1a51b4a42a7074a42a32
Tony <1462668901@qq.com>
2020-12-17 22:36:26 +0800

    Update cnames_active.js
    Co-authored-by: Matt (IPv4) Cowley <me@mattcowley.co.uk>

--- cnames_active.js
+++ cnames_active.js
@@ -2259,7 +2259,7 @@ var cnames_active = {
   "sirkit": "seckwei.github.io/SirKit_CircuitSim", // noCF? (don´t add this in a new PR)
   "sistersbio": "KDiaCodes.github.io/SistersBio",
   "sizle": "christoga.github.io/sizle", // noCF? (don´t add this in a new PR)
-  "sjy": "iamsjy.now.sh",
+  "sjy": "cname.vercel-dns.com",
   "skeptical-coder": "rbpinheiro.github.io/skeptical-coder",
   "sketch": "keiww.github.io/sketch",
   "skim": "muratg.github.io/skim",
