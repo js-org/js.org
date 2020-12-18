de003f7175335aaed8a5eb3164bfbd85ccc7f6e8
Zen <76124818+zennv@users.noreply.github.com>
2020-12-18 15:02:54 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2829,7 +2829,8 @@ var cnames_active = {
   "zygds": "zygds.github.io/zygds.js.org",
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "sixcord": "zennv.github.io/sixcord"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
