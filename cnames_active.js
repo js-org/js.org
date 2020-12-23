57f98034fdc8439abc225fa9f9b3a212c7cb861d
cncdn <69669158+cncdn@users.noreply.github.com>
2020-12-23 21:46:54 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1601,7 +1601,7 @@ var cnames_active = {
   "morse": "the-silver-project.github.io/morse",
   "moviedex": "calm-squash-zqh3z3dg2s83u9gopkkeg10d.herokudns.com",
   "mp3tag": "eidoriantan.github.io/mp3tag.js",
-  "mp": "cncdn.github.io/js.org",
+  "mp": "cncdn.github.io",
   "mpe": "weareroli.github.io/mpejs", // noCF? (don´t add this in a new PR)
   "mrinjamul": "mrinjamul.github.io/mrinjamul.js.org",
   "mrm": "mrmjs.netlify.app",
