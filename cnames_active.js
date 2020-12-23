6f4a0fc3c4eb033ec38df31d84d729c4ab9bebd0
aakhilv <65052071+aakhilv@users.noreply.github.com>
2020-12-23 15:07:55 -0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2507,7 +2507,6 @@ var cnames_active = {
   "tit": "wenjiasen.github.io/tit",
   "title": "sawyerbx.github.io/titlejs",
   "tixol": "vcxiz.github.io/tixol",
-  "tixy": "e366c17a-7fc8-4237-b01f-fe27cd41da7a.repl.co",
   "tiza": "pd4d10.github.io/tiza",
   "tjys": "qikaile.github.io/tjys-notes",
   "tmr": "ttttmr.github.io",
