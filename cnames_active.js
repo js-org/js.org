5be3f3f398da8585072bf1749ba6d925599b7615
MarioRamirez-dev <70715352+MarioRamirez-dev@users.noreply.github.com>
2020-12-27 20:17:01 -0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -994,6 +994,7 @@ var cnames_active = {
   "greylock": "datamart.github.io/Greylock",
   "gridiron": "noderaider.github.io/gridiron",
   "gridsplit": "assetinfo.github.io", // noCF? (don´t add this in a new PR)
+  "grizzlysystems": "marioramirez-dev.github.io/Website_GrizzlySystems",
   "grub": "grubburg.github.io/blog",
   "gruft": "nikola.github.io/gruft", // noCF? (don´t add this in a new PR)
   "grumpy": "aidenybai.github.io/grumpy",
