fdf14b516b223ca50ce9826ec29cbb5076a491ef
kucingedyan <76830798+kucingedyan@users.noreply.github.com>
2021-01-01 12:08:54 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2850,6 +2850,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
   "zyy": "zyyou.github.io/notes"
+  "kucingdesu": "kucingedyan.github.io/kucingdesu"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
