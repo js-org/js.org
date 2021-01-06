b48f01817fce16b82ba9a9d604dc7cf5581e9002
Ringolds Lescinskis <ringolds@lescinskis.com>
2021-01-06 17:46:35 +0200

    Added simple.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2274,6 +2274,7 @@ var cnames_active = {
   "simonhans": "simonhans.github.io",
   "simplecounter": "tomkiernan120.github.io/simplecounter",
   "simplecrypto": "danang-id.github.io/simple-crypto-js",
+  "simple": "lescinskiscom.github.io/simple-js",
   "simples": "micnic.github.io/simples",
   "simpleserver": "MobSpot.github.io/Really-Simple-Server.github.io",
   "simplesnake": "aktindo.github.io/Simple_Snake_Game",
