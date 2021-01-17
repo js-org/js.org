356bcb0f2da86c48f54889970fa1ef9a4a35ef6f
Tyler M-Wise <74430503+TylerMWise@users.noreply.github.com>
2021-01-17 12:34:36 +0000

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2595,6 +2595,8 @@ var cnames_active = {
   "two": "jonobr1.github.io/two.js",
   "twt": "koj-co.github.io/twt",
   "tydel": "fahad19.github.io/tydel", // noCF? (don´t add this in a new PR)
+  "tylermwise": "tylermwise.github.io",
+  "tmblox": "tylermwise.eu.org",
   "typeahead": "corejavascript.github.io/typeahead.js",
   "typed-ui": "pi-cubed.github.io/typed-ui",
   "typeless": "typeless.netlify.com",
