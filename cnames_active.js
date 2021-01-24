0a4fbfe3ab3e18a63308947808ed6a68dd389bac
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2021-01-24 06:16:49 +0330

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -572,6 +572,7 @@ var cnames_active = {
   "d-patterns": "alias.zeit.co", // noCF
   "d4": "joelburget.github.io/d4",
   "daily.tmr": "ttttmr.github.io/daily", // noCF
+  "daisy": "saadeghi.github.io/daisyui-demo",
   "dakokonutboi": "dakokonutboi.github.io/portfolio",
   "daksh0225": "daksh0225.github.io",
   "damo": "damodharanj.github.io",
