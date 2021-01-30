253b2164c0b1bbcd70d705355eee989d4e45a87c
M. Mirmousavi <mirmousavi.m@gmail.com>
2021-01-31 00:52:15 +0330

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -469,6 +469,7 @@ var cnames_active = {
   "cleanblog": "cleanblog.github.io",
   "clearlyelevated": "clearlyelevated.github.io",
   "clevercord": "theanidox.github.io/Clevercord",
+  "clock": "mirmousaviii.github.io/clock",
   "clockwork": "clockworkdev.github.io/ClockworkWebsite",
   "clusterize": "nexts.github.io/Clusterize.js",
   "cmbhackjs2013": "cmbjs.github.io/cmbHackjs2013",
