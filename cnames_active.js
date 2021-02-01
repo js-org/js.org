946951c659c03e1ace7ee53ffd694db0d239fd14
Former <73242251+Fxrmer@users.noreply.github.com>
2021-02-01 15:49:02 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -945,7 +945,6 @@ var cnames_active = {
   "funbot": "jch-notprogram.github.io/funbot-website",
   "fusilli": "anthemaker.github.io/Fusilli.js",
   "futch": "jfbrennan.github.io/futch",
-  "fxrmer": "fxrmer.github.io",
   "fyi": "tobihrbr.github.io/fyi",
   "g": "nodebox.github.io/g.js",
   "g-point": "bdvela.github.io/g-point",
