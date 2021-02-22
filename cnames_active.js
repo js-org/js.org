bad62d5ca505e79c8988aee709ca7de2ddc9ac11
He <zhanghecool@outlook.com>
2021-02-22 18:24:40 +0800

    he.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2748,7 +2748,8 @@ var cnames_active = {
   "zxy": "ZXYFrank.github.io",
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "he": "zhanghecool.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
