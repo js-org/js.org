3041e94d44aa0ea5a7654ae69d56b4ecbf4bdcc5
Azhar Ahmed <49714602+ahmedazhar05@users.noreply.github.com>
2021-03-04 15:08:41 +0530

    adding 8085sim to the list
    The web app is completely based on vanilla js, hence a perfect fit for a js.org subdomain. Thanks

--- cnames_active.js
+++ cnames_active.js
@@ -54,6 +54,7 @@ var cnames_active = {
   "404": "licshee.github.io/404",
   "766": "766.github.io",
   "7anshuai": "7anshuai.github.io", // noCF? (don´t add this in a new PR)
+  "8085sim": "ahmedazhar05.github.io/8085sim",
   "8log": "8log.netlify.app",
   "98": "1j01.github.io/98",
   "9932": "dannyzhan.github.io",
