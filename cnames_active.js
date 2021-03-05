912f75bf17a7edeef799de26519402057635725b
Baker <masterde@users.noreply.github.com>
2021-03-06 00:12:30 +0300

    Update cnames_active.js
    - [x] I have read and accepted the [Terms and Conditions](http://js.org/terms.html)
    - [x] There is reasonable content on the page: [xyz.dev](http://xyz.dev)

--- cnames_active.js
+++ cnames_active.js
@@ -2718,6 +2718,7 @@ var cnames_active = {
   "xstate": "davidkpiano.github.io/xstate",
   "xto6": "mohebifar.github.io/xto6", // noCF? (don´t add this in a new PR)
   "xtype": "lucono.github.io/xtypejs", // noCF? (don´t add this in a new PR)
+  "xyz": "xyz.dev",
   "y86": "quietshu.github.io/y86", // noCF? (don´t add this in a new PR)
   "yadl": "yadljs.github.io",
   "yagolopez": "yagolopez.github.io",
