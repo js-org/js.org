35f785c8320fca38a1bff325918e9d1039a31bb5
Sibin Grasic <sibin.grasic@oblak.studio>
2021-03-06 13:43:04 +0100

    wpwebpack.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2696,6 +2696,7 @@ var cnames_active = {
   "workers": "worker-tools.github.io/workers.js.org",
   "worksnake": "worksnake.github.io",
   "wowufoundme": "wowufoundme.github.io",
+  "wpwebpack": "https://oblakstudio.github.io/wpwebpack",
   "writebar": "alexander-shvets.github.io/writebar",
   "wss": "iguanren.github.io/wss",
   "wstool": "sy-records.github.io/wstool", // noCF
