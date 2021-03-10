9726c1cc786f97293f098b21aff21d68d420bb59
Childe Chén <chenmingmao@126.com>
2021-03-10 22:00:36 +0000

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2781,6 +2781,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
   "zyy": "zyyou.github.io/notes"
+   "chenplus": "chenchilde.github.io",
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
