b18e21bb80a559b9c62487f933df41ed23b14c3d
Max C. Foo <36927158+MaxChang3@users.noreply.github.com>
2021-03-14 13:42:11 +0800

    Update botui-cn.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -334,6 +334,7 @@ var cnames_active = {
   "bottender": "yoctol.github.io/bottender",
   "bottlecap": "rwbeast.github.io/bottlecap",
   "boundless": "enigma-io.github.io/boundless",
+  "botui-cn": "botui-docs-cn.netlify.app",
   "box": "capacitorset.github.io/box-js", // noCF
   "boxed": "lionking27.github.io/boxed",
   "brain": "brainjs.github.io/brain.js.org",
