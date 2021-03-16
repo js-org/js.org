3b56f98aeaaa0f2ee9349370ac13212d87b3d4a7
anas y. bal <59410395+anasybal@users.noreply.github.com>
2021-03-16 21:46:39 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2796,7 +2796,8 @@ var cnames_active = {
   "zxy": "ZXYFrank.github.io",
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "anasybal":"anasybal.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
