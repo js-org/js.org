5d807ef118525ca228deb1c67ed974ac95c414ea
Jesús Linares <34040095+JesusLinares01@users.noreply.github.com>
2021-03-31 22:01:23 -0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1873,7 +1873,7 @@ var cnames_active = {
   "portafolioarnulfo": "arpior.github.io/Portafolio",
   "portafolioluis": "luisself2020.github.io/portafolioluis",
   "portafoliorufino": "Rufino-Hdez.github.io/portafolio",
-  "portafolio-jesuslinares": "jesuslinares01.github.io/Portafolio",
+  "portafolio-linares": "jesuslinares01.github.io/Portafolio",
   "portainer-update": "flweber.github.io/portainer-stack-updater",
   "porter": "ahopkins.github.io/porterjs", // noCF? (don´t add this in a new PR)
   "post-type-discovery": "twozeroone.github.io/post-type-discovery",
