438e763d350b1149256c5561670ea7a58b99a0d9
victorpe1 <70999019+victorpe1@users.noreply.github.com>
2021-04-07 10:29:44 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1872,6 +1872,7 @@ var cnames_active = {
   "pong-zh": "xiaozhu2021.github.io/javascript-pong",
   "popcorn-api": "nirewen.github.io/popcorn-api",
   "popper": "fezvrasta.github.io/popper.js",
+  "portafolio":"victorpe1.github.io/Portafolio"
   "portafolio-web": "velazquezadrian.github.io/Portafolio-web",
   "portafolioacadeller": "acadeller.github.io/portafolioacadeller",
   "portafolioalejandrarobles": "alerobles3.github.io/portafolioalejandrarobles",
