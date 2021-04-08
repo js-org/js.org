98a393376c694ed60c2104a078d4218600e65874
jiayz1997 <58965055+jiayz1997@users.noreply.github.com>
2021-04-08 13:24:29 -0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2830,7 +2830,8 @@ var cnames_active = {
   "zxy": "ZXYFrank.github.io",
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "jiayizhu": "jiayz1997.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
