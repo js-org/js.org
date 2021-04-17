b62bafedfb506b17aa84ef7e1b79440d41f09d24
Stefan Keim <indus@users.noreply.github.com>
2021-04-17 18:25:08 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -628,9 +628,10 @@ var cnames_active = {
   "device": "vpodk.github.io/device",
   "dev-panel": "Involve-Digital.github.io/dev-panel",
   "developerfolio": "saadpasta.github.io/developerFolio",
+  "devfolio": "anilseervi.github.io/DevFolio",
   "devilapi": "devilapi.kazult.repl.co", // noCF
   "devilowl": "website.kazult.repl.co", // noCF
-  "devfolio": "anilseervi.github.io/DevFolio",
+  "devlunch": "superzackx.github.io/DevLunch",
   "devsession": "lukasbach.github.io/devsession",
   "devtr": "zmorcy.github.io/devtr.github.io",
   "dgelong": "alexeyraspopov.github.io/dgelong", // noCF? (don´t add this in a new PR)
@@ -2792,7 +2793,6 @@ var cnames_active = {
   "xhy": "xhy.github.io",
   "xi102": "xi102.github.io",
   "xiangqiz": "xiangqiz.github.io/Hello",
-  "devlunch": "https://superzackx.github.io/DevLunch",
   "xiaolan": "mcxiaolan.github.io",
   "xprmntl": "xprmntl.github.io", // noCF? (don´t add this in a new PR)
   "xss": "tennc.github.io/xss",
