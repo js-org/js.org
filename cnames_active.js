e97c6ec0db43bf21adf1cdc455083b441bc25ea2
Nihat <73744616+KaffinPX@users.noreply.github.com>
2021-04-17 17:05:29 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1938,6 +1938,7 @@ var cnames_active = {
   "pwa-workshop": "sylvainpolletvillard.github.io/pwa-workshop",
   "pwapp": "nikiforov-org.github.io/pwapp",
   "pzero": "hanyaku.github.io/ProjectZERO",
+  "plotyxd": "plotyxd.github.io",
   "qbit": "fudan-mse.github.io/qbit",
   "qbsz": "qbsz.github.io",
   "qs": "kirjs.github.io/qs.js", // noCF? (don´t add this in a new PR)
