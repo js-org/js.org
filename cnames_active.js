af9cf9501eb7169419328e097db4ea82bcc44d53
jiangzeyin <1593503371@qq.com>
2021-04-19 14:28:59 +0800

    jiangzeyin.github.io

--- cnames_active.js
+++ cnames_active.js
@@ -1243,6 +1243,7 @@ var cnames_active = {
   "jwt-autorefresh": "cchamberlain.github.io/jwt-autorefresh", // noCF? (don´t add this in a new PR)
   "jwtonline": "boneyt92.github.io/jwtonline",
   "jz": "jz6.github.io",
+  "jiangzeyin": "jiangzeyin.github.io",
   "kafka": "tulios.github.io/kafkajs",
   "kaguwo": "kaguwos-portfolio.netlify.app",
   "kahoot": "na-west1.surge.sh",
