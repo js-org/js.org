28a0e9bab51651c67ce2433a2e97cfc87163da74
wearehere8 <32786169+wearehere8@users.noreply.github.com>
2021-04-21 09:25:25 +0800

    add
    add

--- cnames_active.js
+++ cnames_active.js
@@ -2857,6 +2857,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
   "zyy": "zyyou.github.io/notes"
+  "willsight": "wearehere8.github.io",
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
