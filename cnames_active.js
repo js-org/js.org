c66fd6f449c0d57246fa849404876e1d2c3e51dc
ikro <58058753+wujiops@users.noreply.github.com>
2021-04-22 17:05:06 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2857,7 +2857,8 @@ var cnames_active = {
   "zxy": "ZXYFrank.github.io",
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "ikro": "wujiops.github.io/ikro.js",
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
