6f71a2fd8b2175ec22875bc14701d7ebbccead86
paisendev <64857329+paisendevmc@users.noreply.github.com>
2021-04-26 13:05:34 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2865,6 +2865,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
   "zyy": "zyyou.github.io/notes"
+  "murbyte": "paisendevmc.github.io
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
