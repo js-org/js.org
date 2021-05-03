82bcd92de2f52c32543e40579e40b3b520d23346
冯思然 <68985689+fengsiran@users.noreply.github.com>
2021-05-03 13:31:23 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2873,6 +2873,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
   "zyy": "zyyou.github.io/notes"
+  "fengsiran": "fengsiran.github.io/"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
