adf7f253a7dbf96702586bd0e0be9144d78ffeb4
Keim, Stefan <stefan.keim@dlr.de>
2021-05-11 16:24:27 +0200

    correct search slice

--- assets/main.ts
+++ assets/main.ts
@@ -7,7 +7,7 @@ const search = window.location.search.slice(1, 20);
 
 (function (el: any) {
     if (search && (el = document.getElementById(el))) {
-        let s = search.slice(1, -3)
+        let s = search.slice(0, -3)
         el.innerHTML = s;
         setTimeout(function () {window.location.href = 'https://js.org/?' + s;}, 5000);
     }
