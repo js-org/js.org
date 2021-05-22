f40b59b7da56b43950eb5f187ef20ce91ca9400a
Okiki <okikio.dev@gmail.com>
2021-05-21 23:56:18 -0400

    add bundle.js.org to cname_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -365,6 +365,7 @@ var cnames_active = {
   "bui": "kjantzer.github.io/bui",
   "bullwinkle": "bullwinkle.github.io",
   "bun": "the94air.github.io/bun",
+  "bundle": "okikio.github.io/bundle",
   "bunjil": "bunjil.netlify.com",
   "bunyan-fork": "cchamberlain.github.io/bunyan-fork", // noCF? (don´t add this in a new PR)
   "bunyan-pmx": "cchamberlain.github.io/bunyan-pmx", // noCF? (don´t add this in a new PR)
