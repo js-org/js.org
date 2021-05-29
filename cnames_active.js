3cf57f4b686ea8dc445b0ad71fd294698a4d7719
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-29 06:49:24 +0530

    add shandler.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -40,6 +40,7 @@
 var cnames_active = {
   "": "js-org.github.io",
   "01mz": "01mz.github.io",
+  "shandler": "https://gmbodhi.github.io/shandler/",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
   "131": "netrvin.github.io",
