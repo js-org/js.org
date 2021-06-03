104f71d148416b0771b5d4dec3656b48760f701a
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2021-06-03 12:07:23 +0100

    Missing comma

--- cnames_active.js
+++ cnames_active.js
@@ -2236,7 +2236,7 @@ var cnames_active = {
   "sanohin": "sanohin.github.io",
   "saso": "huruji.github.io/saso",
   "sass": "medialize.github.io/playground.sass.js", // noCF? (don´t add this in a new PR)
-  "natural": "savior.github.io"
+  "natural": "savior.github.io",
   "satont": "satont.github.io",
   "saulosantiago": "saulosilva.github.io", // noCF? (don´t add this in a new PR)
   "sawyer": "sqwwy.github.io/sawyer.js.org",
