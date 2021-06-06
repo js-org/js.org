88b50bd77b4d53992c984db4571517e24f40ece9
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2021-06-06 13:09:36 +0100

    Rm https://

--- cnames_active.js
+++ cnames_active.js
@@ -2402,7 +2402,7 @@ var cnames_active = {
   "squid": "squidjs.github.io/squid",
   "squirrelly": "squirrellyjs.netlify.com", // noCF
   "sr": "samrobbins85.github.io",
-  "sreeram": "https://sreeram-io.github.io/project-icarus/",
+  "sreeram": "sreeram-io.github.io/project-icarus",
   "sri": "jackub.github.io/subresource-integrity-fallback",
   "ss": "cname.vercel-dns.com", // noCF
   "staart": "staart.netlify.com",
