3d33e89bbcd2ce0b9f5027d600d80790a54bbcf3
Sreeram Padmanabhan <sreeram.com@gmail.com>
2021-06-06 16:02:44 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2402,7 +2402,7 @@ var cnames_active = {
   "squid": "squidjs.github.io/squid",
   "squirrelly": "squirrellyjs.netlify.com", // noCF
   "sr": "samrobbins85.github.io",
-  "sreeram": "sreeram-io.github.io/project-icarus",
+  "sreeram": "sreeramofficial.github.io/sreeram",
   "sri": "jackub.github.io/subresource-integrity-fallback",
   "ss": "cname.vercel-dns.com", // noCF
   "staart": "staart.netlify.com",
