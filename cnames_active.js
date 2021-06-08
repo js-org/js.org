87ebe4c69670f97dcb461b8b092941282334085c
Crispy <61794525+Crispy-Cream@users.noreply.github.com>
2021-06-07 23:00:50 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2713,7 +2713,7 @@ var cnames_active = {
   "veza": "kyranet.github.io/veza",
   "vfm": "meshesha.github.io/VisualFormMaker.github.io",
   "viav": "brandondyer64.github.io/viav",
-  "vibe": "hosting.gitbook.io", // noCF
+  "vibe": "crispy-cream.github.io/Vibe/index.html",
   "vicis": "r37r0m0d3l.github.io/vicis",
   "vico": "bohdantkachenko.github.io/vico", // noCF? (don´t add this in a new PR)
   "victor": "vicdevjs.github.io/vic",
