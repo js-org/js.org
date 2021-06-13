e479ca18c2e02a0b44b101a4f52e2f8a27a9e6c1
Gurkan <82083756+GurkanDev@users.noreply.github.com>
2021-06-13 11:03:59 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1317,7 +1317,7 @@ var cnames_active = {
   "knowyourbundle": "enapupe.github.io/know-your-bundle",
   "ko": "ko25july.github.io/ko.js.org",
   "ko.redux": "deminoth.github.io/redux",// noCF
-  "kruto": "planetgurk.github.io/kruto-docs" // it's the documentation for a project i'm making :D
+  "kruto": "planetgurk.github.io/kruto-docs"
   "koa-rest-api-boilerplate": "posquit0.github.io/koa-rest-api-boilerplate",
   "kobra": "johnsylvain.github.io/kobra",
   "kodadot": "kodadot.github.io",
