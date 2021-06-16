848fbf0803bfb413bd376e1605c12e9cbf2c9966
Sandra Gilazaña <54216038+SanGilazana@users.noreply.github.com>
2021-06-16 18:29:22 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1939,6 +1939,7 @@ var cnames_active = {
   "portafolioarnulfo": "arpior.github.io/Portafolio",
   "portafolioluis": "luisself2020.github.io/portafolioluis",
   "portafoliorufino": "Rufino-Hdez.github.io/portafolio",
+  "portafolioSanGilazana": "sangilazana.github.io/Portfolio",
   "portainer-update": "flweber.github.io/portainer-stack-updater",
   "porter": "ahopkins.github.io/porterjs", // noCF? (don´t add this in a new PR)
   "post-type-discovery": "twozeroone.github.io/post-type-discovery",
