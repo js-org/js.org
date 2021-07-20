0e4aff1f741a58ff373faa32299ffbe65cb86376
BALAJI B <4012718+bala@users.noreply.github.com>
2021-07-20 08:43:06 +0530

    ai.js.org Requested

--- cnames_active.js
+++ cnames_active.js
@@ -111,7 +111,7 @@ var cnames_active = {
   "ahmeterdgn": "ahmeterdgn.github.io",
   "ahnotification": "ahmadhayyan.github.io/ahnotification.js",
   "ahooks": "ahooks.surge.sh",
-  "ai": "bala.github.io/aijs",
+  "ai": "bala.github.io/ai-jscript",
   "aider": "tjz101.github.io/aider-js-pages",
   "airtable-plus": "victorhahn.github.io/airtable-plus",
   "ais": "yunyoujun.github.io/ais.js",
