3c041ef18ae1fb25f99dc151ab607b8290dc64f7
lem0nb <68461894+lem0nb@users.noreply.github.com>
2021-07-20 11:38:08 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2904,6 +2904,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
   "zyy": "zyyou.github.io/notes"
+  "lirui": "lem0nb.github.io/lblog"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
