754e71590964565a58b7d0e924439b83346363b5
guobaogang <67996742+guobaogang@users.noreply.github.com>
2021-07-21 13:30:36 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2957,7 +2957,8 @@ var cnames_active = {
   "zxy": "ZXYFrank.github.io",
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "guobg": "guobg.github.io/peach-design"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
