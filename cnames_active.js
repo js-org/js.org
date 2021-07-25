eb70c1e7c8a4e0d918e8d05befdc768825cbe6b6
Undefined <contact.pritulx@gmail.com>
2021-07-25 12:49:03 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1981,6 +1981,7 @@ var cnames_active = {
   "prettylog": "moosecoop.github.io/PrettyLog",
   "prismarine": "prismarinejs.github.io",
   "pristine": "sha256.github.io/Pristine", // noCF
+  "pritu": "pritudev.github.io",
   "probsims": "o-mono.github.io/theprobsims",
   "producify": "jesobreira.github.io/producify",
   "producthunt-trending": "xiaomingplus.github.io/producthunt-trending",
