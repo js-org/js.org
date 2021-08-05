a7ccc05c9b34ce615cd96c921133d52534df3d91
Sujal Goel <gglebot.helper@gmail.com>
2021-08-05 19:45:43 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1804,6 +1804,7 @@ var cnames_active = {
   "nsptiles": "imthenachoman.github.io/nSPTiles", // noCF? (don´t add this in a new PR)
   "nsw-coronavirus": "maxgherman.github.io/nsw-coronavirus",
   "nuclear": "nukeop.github.io/nuclear",
+  "nuggies": "nuggies.netlify.app"
   "nugu": "vietcode.github.io/nugu",
   "null": "false.netlify.com",
   "num2words": "mcnagynorbi.github.io/num2words",
