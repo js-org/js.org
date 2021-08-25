1b82a8abd6eb2d1975c8657381666e887abd1bf3
Adolfo León <50426680+Adogamm@users.noreply.github.com>
2021-08-24 22:53:50 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -94,6 +94,7 @@ var cnames_active = {
   "adil": "adilzeshan.github.io/adil",
   "aditya": "aditya81070.github.io",
   "adnanbabakan": "adnanbabakan.github.io",
+  "adolfo-leon": "https://adogamm.github.io/Portfolio/",
   "adon988": "adon988.github.io",
   "adv": "advjs.github.io",
   "advancedrpc": "advancedrpc.github.io",
