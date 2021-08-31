8fcf7d9f6721d0f8c6954b58295eb3e078c083af
Aleksandar Trifunovic <aleksandar.trifunovic96@outlook.com>
2021-08-31 09:12:36 +0200

    Added Clean Front End Reference domain

--- cnames_active.js
+++ cnames_active.js
@@ -3012,7 +3012,8 @@ var cnames_active = {
   "zxy": "ZXYFrank.github.io",
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "clean-front-end-reference": "jdevcron.github.io/Clean-Front-End-Reference"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
