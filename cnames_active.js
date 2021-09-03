842f5b845abb439adfe13ea6f400b37e56faa08b
Camilo Andres Garatejo Moreno <79905957+SriRaif300@users.noreply.github.com>
2021-09-03 20:22:07 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2005,6 +2005,7 @@ var cnames_active = {
   "portafolioacadeller": "acadeller.github.io/portafolioacadeller",
   "portafolioalejandrarobles": "alerobles3.github.io/portafolioalejandrarobles",
   "portafolioarnulfo": "arpior.github.io/Portafolio",
+  "portafoliocam.js.org": "sriraif300.github.io/Portafolio",
   "portafolioluis": "luisself2020.github.io/portafolioluis",
   "portafoliorufino": "Rufino-Hdez.github.io/portafolio",
   "portainer-update": "flweber.github.io/portainer-stack-updater",
