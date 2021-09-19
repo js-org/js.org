3d2c2fd3e2a2d13e8eeb5344d12456b6b1180fce
Vitalij Ryndin <sys@crashmax.ru>
2021-09-19 20:40:38 +0800

    Add fireworks.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -931,6 +931,7 @@ var cnames_active = {
   "fireblast": "thefireblast.github.io",
   "firenze": "fahad19.github.io/firenze", // noCF? (don´t add this in a new PR)
   "fireorm": "wovalle.github.io/fireorm",
+  "fireworks": "crashmax-dev.github.io/fireworks-js",
   "fish": "cdaringe.github.io/fish",
   "five": "jackdcrawford.github.io/five",
   "flamecord": "flamexode.github.io/flamecord",
