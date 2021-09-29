3dc30bba740709f9c7a8048a5e06082dcb976069
赤子 <63346301+tobctobc@users.noreply.github.com>
2021-09-29 01:34:40 +0000

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2646,7 +2646,7 @@ var cnames_active = {
   "tayyab": "itayyab.github.io",
   "tead": "teadjs.github.io",
   "techthinker": "tech-thinker.github.io",
-  "techxuexi": "cname.vercel-dns.com", // noCF
+  "techxuexi": "custom.pages.dev", // noCF
   "techy": "hosting.gitbook.com",
   "teddy": "alias.zeit.co", // noCF
   "tejas": "harshsinghdev.github.io/tejas",
