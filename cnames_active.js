f6a4f8c60191c8ecbd100a309b8754a2d3290e58
Jevspr <64289872+Jevspr@users.noreply.github.com>
2021-10-04 10:02:22 -0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1297,6 +1297,7 @@ var cnames_active = {
   "jessm": "jessi-mhernandez.github.io/JessicaM",
   "jetaimetantquetuestoi": "dakokonutboi.github.io/jetaimetantquetuestoi",
   "jets": "nexts.github.io/Jets.js",
+  "jevs": "jevspr.github.io/DigitalPortafolio",
   "jinada": "redsplit.github.io/jinada",
   "jjlc": "k-yak.github.io/JJLC", // noCF? (don´t add this in a new PR)
   "jk": "joname1.github.io",
