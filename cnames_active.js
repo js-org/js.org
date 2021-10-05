2d747d193d837dfda3c857d06c0932cf11b0791c
S4cr4b011 <83314408+S4cr4b011@users.noreply.github.com>
2021-10-05 13:19:16 -0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2034,6 +2034,7 @@ var cnames_active = {
   "pong-zh": "xiaozhu2021.github.io/javascript-pong",
   "popcorn-api": "nirewen.github.io/popcorn-api",
   "popper": "fezvrasta.github.io/popper.js",
+  "portafoliosacrab": "s4cr4b011.github.io/PortafolioSacrab/"
   "portafolio-web": "velazquezadrian.github.io/Portafolio-web",
   "portafolioacadeller": "acadeller.github.io/portafolioacadeller",
   "portafolioalejandrarobles": "alerobles3.github.io/portafolioalejandrarobles",
