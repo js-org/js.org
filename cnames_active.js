9c2cf8a27930306fedac154a4b77803b566f2b11
KENLLY CHACON <86888726+Kenlly-Chacon@users.noreply.github.com>
2021-10-10 19:04:52 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2042,6 +2042,7 @@ var cnames_active = {
   "portafolioarnulfo": "arpior.github.io/Portafolio",
   "portafolioluis": "luisself2020.github.io/portafolioluis",
   "portafoliorufino": "Rufino-Hdez.github.io/portafolio",
+  "portafolio": "kenlly-chacon.github.io/portafolio/",
   "portainer-update": "flweber.github.io/portainer-stack-updater",
   "porter": "ahopkins.github.io/porterjs", // noCF? (don´t add this in a new PR)
   "post-type-discovery": "twozeroone.github.io/post-type-discovery",
