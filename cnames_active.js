331bebf9014f86a3dc80c53cbc796fa99c9edb8b
Lea <75648501+Lea321@users.noreply.github.com>
2021-10-16 17:44:56 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1451,6 +1451,7 @@ var cnames_active = {
   "lcookie": "lf112.github.io/lcookie",
   "ldf": "autplayed.github.io/ldf",
   "le": "letui.github.io",
+  "lea": "Lea321.github.io",
   "leafless": "mofax.github.io/leafless",
   "lean-stack": "lean-stack.github.io", // noCF? (don´t add this in a new PR)
   "leandro": "leandrowd.github.io", // noCF? (don´t add this in a new PR)
