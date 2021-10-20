e49c718e37d820ea3d3fe66d80df79adb839a287
Waren Gonzaga <github@warengonzaga.com>
2021-10-21 01:01:18 +0800

    buymeacoffee.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -389,6 +389,7 @@ var cnames_active = {
   "bustime": "agarzola.github.io/bustime", // noCF? (don´t add this in a new PR)
   "butterfly": "cname.vercel-dns.com", // noCF
   "buttermilk": "buttermilk.netlify.com",
+  "buymeacoffee": "buymeacoffeejs.warengonzaga.com",
   "bwb": "shbwb.github.io/bwb",
   "bwb-docs": "shbwb.github.io/bwb-docs",
   "byakugan": "rockmanvnx6.github.io/byakugan-js",
