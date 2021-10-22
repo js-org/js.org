1427439ef4e49bebdf56a748474e4b1d202c039e
Vins 2106 <69247128+Vins2106@users.noreply.github.com>
2021-10-22 20:19:12 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1115,6 +1115,7 @@ var cnames_active = {
   "gyps": "huijari.github.io/Gyps",
   "gyre": "wridder.github.io/GyreJS", // noCF? (don´t add this in a new PR)
   "gyx": "yourtion.github.io/gyx.js.org",
+  "github-touch": "github-touch.github.io",
   "h": "makenowjust.github.io/h.js",
   "h3": "h3rald.github.io/h3",
   "h7ml": "h7ml.github.io/web",
