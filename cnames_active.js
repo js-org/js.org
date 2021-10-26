42b2ce544d424a3609bad83bcd5337b528424125
Finn <81511282+finncodes@users.noreply.github.com>
2021-10-26 04:56:11 +0000

    Add wellington.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2979,6 +2979,7 @@ var cnames_active = {
   "weekly": "xdimh.github.io/weekly",
   "weky-docs": "7eb46102-c415-4a88-beb9-6d9da4fd6831.repl.co", // noCF
   "welcome": "edapm.github.io/welcomejs",
+  "wellington": "wellington-js.github.io/website",
   "wepl": "obnoxiousnerd.github.io/wepl-website",
   "wfplayer": "zhw2590582.github.io/WFPlayer",
   "wfs": "acewfdy.github.io/static",
