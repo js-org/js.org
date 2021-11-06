42c5ab51ee0d769e956e226d60edb15cd2ee856b
Wonderest <50113417+wonderest@users.noreply.github.com>
2021-11-06 11:32:57 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3021,6 +3021,7 @@ var cnames_active = {
   "wk": "alperr.github.io/wk",
   "wog": "wog-js.github.io",
   "wonderest": "wonderest.github.io",
+  "rizky": "wonderest.github.io/rizky",
   "wonderfulcv": "wonderfulcv.netlify.app",
   "woodsman": "haximilian.github.io/woodsman",
   "wooyun": "jiji262.github.io/wooyun_articles",
