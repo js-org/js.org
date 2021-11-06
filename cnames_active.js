56d085e48ad9dc95a5589677984013d2abda08b2
RedYetiDev <38299977+RedYetiDev@users.noreply.github.com>
2021-11-06 13:35:21 -0400

    web.quizlet.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2974,6 +2974,7 @@ var cnames_active = {
   "wdd": "wangduanduan.github.io",
   "weather": "phanendraguptha.github.io/weather",
   "weaver": "maxkfranz.github.io/weaver", // noCF? (don´t add this in a new PR)
+  "web.quizlet": "redyetidev.github.io/Quizlet.JS-Web",
   "web-sandbox": "web-sandbox-js.github.io/web-sandbox-website",
   "web-widget": "web-widget.github.io",
   "web230": "paylesworth.github.io/web230",
