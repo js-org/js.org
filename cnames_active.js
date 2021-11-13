69bd26bd6da1450c98e546f09e411b82e8ede995
slip-box <296961962@qq.com>
2021-11-13 14:19:39 +0800

    add ide.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -3111,7 +3111,8 @@ var cnames_active = {
   "zxy": "ZXYFrank.github.io",
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "ide": "slip-box.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
