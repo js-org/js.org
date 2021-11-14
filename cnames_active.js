4306a24046fe018426a75390230705355b7b912a
LukaDev <47296785+lukadev-0@users.noreply.github.com>
2021-11-14 14:17:07 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -564,7 +564,7 @@ var cnames_active = {
   "corki": "dvtate.github.io/corki",
   "corona": "soroushchehresa.github.io/awesome-coronavirus",
   "corps": "copay.github.io",
-  "cord": "lukadev-0.github.io/cord",
+  "cord": "lukadev-0.github.io/cord.js",
   "corx": "corx.kazult.repl.co", // noCF
   "cosova": "hardo.github.io/cosova",
   "cote": "dashersw.github.io/cote", // noCF? (don´t add this in a new PR)
