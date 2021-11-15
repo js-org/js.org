9c9369c873bf357485127a68ae3bcb32bb751ab8
simple-alertbox <84765475+simple-alertbox@users.noreply.github.com>
2021-11-15 12:58:37 +0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3117,6 +3117,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes"
+  "alertbox": "simple-alertbox.github.io/alertbox"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
