fa16b24072068504c49b6bd640b51422ac1be592
Andy <andyyxw@163.com>
2021-11-19 10:18:08 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -154,6 +154,7 @@ var cnames_active = {
   "ananta": "anantamishra.github.io/ananta",
   "andresito": "andresitodeguzman.github.io/andresito",
   "android": "twtg93.github.io/Android-js",
+  "andy": "andyyxw.github.io",
   "anenth": "anenth.github.io", // noCF? (don´t add this in a new PR)
   "angular-jsf": "json-schema-faker.github.io/angular-jsf", // noCF? (don´t add this in a new PR)
   "angular-uikit": "whoisjorge.github.io/angular-uikit",
