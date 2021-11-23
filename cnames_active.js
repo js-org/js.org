dd8f37d3f9f7008e8b9a9d5d9e6859e15f35abe0
Ana-Gabriela-Taipe <94630725+Ana-Gabriela-Taipe@users.noreply.github.com>
2021-11-22 19:38:17 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2683,6 +2683,7 @@ var cnames_active = {
   "tagster": "goschevski.github.io/tagster", // noCF? (don´t add this in a new PR)
   "taha": "orcxzjeeeee.github.io/taha",
   "tailwind-baseline": "apkoponen.github.io/tailwind-baseline-site",
+  "taipepagweb": "https://ana-gabriela-taipe.github.io/taipepagweb/",
   "talk": "zonayedpca.github.io/talk.js",
   "talker": "secondstreet.github.io/talker.js", // noCF? (don´t add this in a new PR)
   "tama": "etk.gitlab.io",
