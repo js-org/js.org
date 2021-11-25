03d77889c66731bded033f2a0bc5e068302b268c
evi1hack <evilm4n@qq.com>
2021-11-25 22:12:51 +0800

    add evi1hack.github.io

--- cnames_active.js
+++ cnames_active.js
@@ -922,6 +922,7 @@ var cnames_active = {
   "extenso": "theuves.github.io/extenso.js.org",
   "extraction": "rse.github.io/extraction", // noCF? (don´t add this in a new PR)
   "eye": "arguiot.github.io/EyeJS",
+  "evi1hack": "evi1hack.github.io",
   "faceapp": "negarang.github.io/face-app",
   "facepalm": "santiagogil.github.io/facepalm",
   "facreative": "facreative.github.io",
