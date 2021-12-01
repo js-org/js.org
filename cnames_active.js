0d29d70d6adb28b0ac0f26c6f7133be126d57a61
Jiří Pudil <me@jiripudil.cz>
2021-12-01 11:10:03 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1818,7 +1818,7 @@ var cnames_active = {
   "mzplayer": "prince3661.github.io/Mzplayer",
   "n": "cyb.github.io/n",
   "nabin": "nabin6246.github.io",
-  "naja": "jiripudil.github.io/Naja",
+  "naja": "naja-js.github.io/naja",
   "namaste": "namaste-duniya.github.io/js",
   "name-my-colors": "domcoleman.github.io/name-my-colors",
   "namelessman": "namelessman.github.io",
