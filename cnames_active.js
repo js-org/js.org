d297b72abb442b2a3bf07f4f8b66f7e705f1312a
Fernando Oros <73372277+FernandoOros96@users.noreply.github.com>
2021-12-09 00:25:26 -0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2089,6 +2089,7 @@ var cnames_active = {
   "portafolioarnulfo": "arpior.github.io/Portafolio",
   "portafolioluis": "luisself2020.github.io/portafolioluis",
   "portafoliorufino": "Rufino-Hdez.github.io/portafolio",
+  "portafolio_feroros": "http://portafolio_feroros.js.org/",
   "portainer-update": "flweber.github.io/portainer-stack-updater",
   "porter": "ahopkins.github.io/porterjs", // noCF? (don´t add this in a new PR)
   "post-type-discovery": "twozeroone.github.io/post-type-discovery",
