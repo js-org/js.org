fe360e5b0f204ece82c59e373862a6bcc81e5e2c
Hyro <generalkubo@gmail.com>
2021-12-11 22:50:08 +0100

    garlic team

--- cnames_active.js
+++ cnames_active.js
@@ -1043,6 +1043,7 @@ var cnames_active = {
   "gamedevcontestal": "fromdenisvieira.github.io/gamedevcontestal", // noCF? (don´t add this in a new PR)
   "gametracker": "officialpiyush.github.io/gametracker.js",
   "gamingnet": "gamingmineblox.github.io/GamingNet",
+  "garlic-team": "garlic-team.github.io/website",
   "gathertown": "warengonzaga.github.io/gathertown.js",
   "gatsby-source-pixiv": "poipoii.github.io/gatsby-source-pixiv",
   "gazosekai": "starkblaze01.github.io/Gazo_Sekai",
