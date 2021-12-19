63b79386cfae13868142bc0b13c7a4f2af48bcca
Fernando <fernandoflorez2011@gmail.com>
2021-12-18 21:00:25 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2087,6 +2087,7 @@ var cnames_active = {
   "pong-zh": "xiaozhu2021.github.io/javascript-pong",
   "popcorn-api": "nirewen.github.io/popcorn-api",
   "popper": "fezvrasta.github.io/popper.js",
+  "Portafolio_fernando": "guerreroflorez.github.io/Portafolio",
   "portafolio-web": "velazquezadrian.github.io/Portafolio-web",
   "portafolioacadeller": "acadeller.github.io/portafolioacadeller",
   "portafolioalejandrarobles": "alerobles3.github.io/portafolioalejandrarobles",
