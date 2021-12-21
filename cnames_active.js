22b89a026697ce98369025a16bdb2955d2aea99a
ItsAtelo <88721807+ItsAtelo@users.noreply.github.com>
2021-12-21 11:17:20 -0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -223,6 +223,7 @@ var cnames_active = {
   "async-selector-kit": "humflelump.github.io/async-selector-kit",
   "atavic": "atavic.github.io",
   "atenas": "atenasjs.netlify.app",
+  "atils": "itsatelo.github.io/atils/",
   "athome": "simon300000.github.io/athome",
   "atombundles": "lirantal.github.io/atombundles",
   "atomicreact": "atomicreact.github.io/AtomicReact",
