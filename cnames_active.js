b5c90d33a63115b88f39c80935344a0aacf86ebe
neila-a <78797625+neila-a@users.noreply.github.com>
2021-12-25 20:58:17 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3155,6 +3155,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes"
+  "countdown": "https://neila-a.github.io/countdown.js/index.html"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
