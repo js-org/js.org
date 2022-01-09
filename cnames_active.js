f65d49ff22158736efb348d9c20bda44535719ad
Mr Reza Yazdani <74140299+MrYazdan@users.noreply.github.com>
2022-01-09 17:15:13 +0330

    Update cnames_active.js
    - [x] I have read and accepted the [Terms and Conditions](http://js.org/terms.html)
    - [x] There is reasonable content on the page
    - [x] I have added a CNAME file to my repo: ***https://github.com/mryazdan/gorbeh***

--- cnames_active.js
+++ cnames_active.js
@@ -1119,6 +1119,7 @@ var cnames_active = {
   "goober": "cristianbote.github.io/goober",
   "good": "amirhossein693.github.io/good",
   "goodshare": "koddr.github.io/goodshare.js",
+  "gorbeh": "mryazdan.github.io/gorbeh",
   "gosling": "gosling-lang.github.io/gosling.js",
   "gost-cli": "minecraftpublisher.github.io/gost-cli",
   "gotanda": "gotandajs.github.io",
