ec8bd05b8b8a6045291c93d82a4954542b152f52
Ricardo98l <71473885+Ricardo98l@users.noreply.github.com>
2022-01-16 16:37:46 -0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2109,6 +2109,7 @@ var cnames_active = {
   "pong-zh": "xiaozhu2021.github.io/javascript-pong",
   "popcorn-api": "nirewen.github.io/popcorn-api",
   "popper": "fezvrasta.github.io/popper.js",
+  "portafolio": "ricardo98l.github.io/Portafolio",
   "portafolio-web": "velazquezadrian.github.io/Portafolio-web",
   "portafolioacadeller": "acadeller.github.io/portafolioacadeller",
   "portafolioalejandrarobles": "alerobles3.github.io/portafolioalejandrarobles",
