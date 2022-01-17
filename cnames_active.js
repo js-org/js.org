12d773e9de16451b5a06c8d96ebb63c6ee862c78
Luis Alejandro Velásquez <86325023+LuisVZ485@users.noreply.github.com>
2022-01-17 17:11:22 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2761,6 +2761,7 @@ var cnames_active = {
   "termi": "wearetermi.github.io/termi",
   "terminal": "gottox.github.io/terminal.js", // noCF? (don´t add this in a new PR)
   "tess": "det171.github.io/Tess.js",
+  "testpageforcompany": "luisvz485.github.io/testpageforcompany/",
   "testframe": "testingrequired.github.io/testframe",
   "tetr": "proximitynow19.github.io/TETR.JS",
   "textillate": "jschr.github.io/textillate", // noCF? (don´t add this in a new PR)
