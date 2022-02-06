7836cf2ba856982bb41bafba51d730f0e9d98978
AS BROTHERS <90209858+asbros@users.noreply.github.com>
2022-02-06 12:12:51 +0545

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -207,7 +207,6 @@ var cnames_active = {
   "artery": "arteryjs.github.io/gh-pages", // noCF? (don´t add this in a new PR)
   "arthurmbandeira": "arthurmbandeira.github.io",
   "artitalk": "artitalkjs.github.io/docs",
-  "as": "techimodi.github.io/as.js",
   "asdivyansh": "asdivyansh.github.io",
   "ass": "weizhenye.github.io/ASS",
   "ass-editor": "jeff-tian.github.io/ass",
