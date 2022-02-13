979dd701f4db3a5fc92203786d0c1e83525b6c47
Luis Torres <luis.albertogto@gmail.com>
2022-02-12 18:04:29 -0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1608,6 +1608,7 @@ var cnames_active = {
   "luciascript": "luciascript.netlify.app",
   "lucy": "lucy-bot.github.io",
   "luis": "xyluis.github.io",
+  "luistorres": "luistc83.github.io/luistorres",
   "lukemetoki": "luke2m.github.io",
   "lukks": "lukks.github.io/page",
   "lumi": "furioustsunami.github.io/lumijs",
