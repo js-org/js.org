9d2d25e23c3761bc575f922d4e2869c6722e54b8
Mehmet Eren <95937626+eren78780@users.noreply.github.com>
2022-02-13 16:11:00 +0300

    Update ns_active.js

--- ns_active.js
+++ ns_active.js
@@ -18,6 +18,7 @@ var ns_active = {
   "engine262": ["brad.ns.cloudflare.com", "lia.ns.cloudflare.com"],
   "lolifamily": ["glen.ns.cloudflare.com", "melany.ns.cloudflare.com"],
   "ruby": ["alexis.ns.cloudflare.com", "connie.ns.cloudflare.com"],
+  "www.xlqu": ["ns1.vercel-dns.com", "ns2.vercel-dns.com"],
   "samplasion": ["ns31.cloudns.net", "ns32.cloudns.net", "ns33.cloudns.net", "ns34.cloudns.net"],
   "shebang": ["dns1.p01.nsone.net", "dns2.p01.nsone.net", "dns3.p01.nsone.net", "dns4.p01.nsone.net"]
 }
