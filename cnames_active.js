8ea211ee8e4b158b2541a1c34d3da95717492ac7
Lina <94096360+Linadevv@users.noreply.github.com>
2022-02-18 15:57:54 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1920,7 +1920,7 @@ var cnames_active = {
   "ngspice": "42ua.github.io/ngspice.js",
   "ni": "js-ni.github.io/ni",
   "nic": "nic.github.io",
-  "NikoP100": "NikoP100.github.io"
+  "Linadevv": "Linadevv.github.io"
   "nightcord": "rellylegend.github.io/nightcord",
   "nikita": "adaltas.github.io/node-nikita",
   "nikmartin": "nikmartin.github.io",
