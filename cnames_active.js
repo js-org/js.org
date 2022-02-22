01a2a9e3770fb2479a0bda6faeb551c6ccc83827
Lina <94096360+Linadevv@users.noreply.github.com>
2022-02-22 10:04:44 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2648,6 +2648,7 @@ var cnames_active = {
   "spring": "hosting.gitbook.com",
   "spritesheet": "arcadiogarcia.github.io/Spritesheet.js", // noCF? (don´t add this in a new PR)
   "spritewerk": "bildepunkt.github.io/spritewerk", // noCF? (don´t add this in a new PR)
+  "spud": "nikop100.github.io/spud/",
   "sql": "sql-js.github.io/sql.js",
   "sql2struct": "ymlair.github.io/sql2struct",
   "sqlchart": "sqlchart.github.io/sqlchart",
