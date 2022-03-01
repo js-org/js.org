47302b11b5df368aef6a72f95981a0ef09336bff
NDD-IM（奈应） <68718801+NDD-IM@users.noreply.github.com>
2022-03-01 22:56:21 +0800

    ndd.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1819,6 +1819,7 @@ var cnames_active = {
   "naveenjujaray": "naveenjujaray.github.io/blog",
   "nbabot": "eliotchignell.github.io/NBABot",
   "ncub8": "ncub8.github.io", // noCF? (don´t add this in a new PR)
+  "ndd": "ndd-im.github.io/JS",
   "neaps": "neaps.github.io",
   "nearley": "kach.github.io/nearley",
   "nectar": "cname.vercel-dns.com",
