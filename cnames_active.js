3fd9a72fc9d2a5125fa1adbd6cf8034f45fabc7a
Fernando <90936639+Fer-Bar@users.noreply.github.com>
2022-03-02 12:46:15 -0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -948,6 +948,7 @@ var cnames_active = {
   "festercluck": "festercluck.github.io", // noCF? (don´t add this in a new PR)
   "fetch-json": "center-key.github.io/fetch-json",
   "fetch-the-parrot": "fetch-the-parrot.netlify.app",
+  "fer-bar": "fer-bar.github.io/Portfolio/",
   "fidel": "fidelxyz.github.io",
   "fikri": "ikrydev.github.io/fikri.js.org",
   "filer": "filerjs.github.io/filer",
