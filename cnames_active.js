087e6177224c5094586cc79bf3edca3565be5b79
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-09 15:44:53 +0000

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1413,6 +1413,7 @@ var cnames_active = {
   "kilobyte": "kilobytehq.github.io/open-js",
   "kilvin": "rofrischmann.github.io/kilvin",
   "kim": "khareemnurulla.github.io/kim",
+  "kimera": "ultirequiem.github.io/kimera",
   "kindavishal": "kindavishal.netlify.com",
   "kiranremmarasu": "kiranremmarasu.github.io/kiranremmarasu",
   "kite": "kite-js.github.io/kite",
