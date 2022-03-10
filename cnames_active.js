29a0724285b6bf584bfcebd78fd8bb94e864299f
dersanchez77 <100027206+dersanchez77@users.noreply.github.com>
2022-03-10 11:32:53 -0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2080,6 +2080,7 @@ var cnames_active = {
   "portafolioarnulfo": "arpior.github.io/Portafolio",
   "portafolioluis": "luisself2020.github.io/portafolioluis",
   "portafoliorufino": "Rufino-Hdez.github.io/portafolio",
+  "Portafolio":"https://dersanchez77.github.io/portafolio/",
   "portainer-update": "flweber.github.io/portainer-stack-updater",
   "porter": "ahopkins.github.io/porterjs", // noCF? (don´t add this in a new PR)
   "post-type-discovery": "twozeroone.github.io/post-type-discovery",
