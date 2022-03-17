d258cc2e082ba5c07c11302b4324bad6fa562794
easella <74786054+easella@users.noreply.github.com>
2022-03-17 08:14:45 -0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -47,6 +47,7 @@ var cnames_active = {
   "1c": "oknosoft.github.io/1c",
   "1diaboliko84": "1diaboliko84.github.io",
   "1ib": "1ib.github.io",
+  "phantomail","easella.github.io/phantomail.js.org",
   "21cm": "21cm.github.io",
   "23chemistrycamp": "nchuchemistry.github.io/23chemistrycamp",
   "2tube": "you2php.github.io/you2php",
