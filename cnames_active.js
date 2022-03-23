4e15340622e7f92049f63cb32b87cd4a3cb1fc3c
zimo6918 <101170988+zimo6918@users.noreply.github.com>
2022-03-23 08:40:53 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3136,6 +3136,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes"
+  "zimo6918": "zimo6918.github.io"
+
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
