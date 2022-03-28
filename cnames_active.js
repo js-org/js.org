1d76b8a4b580551a86de10060389ae0ec1556fbd
love2wind <love2wind@gmail.com>
2022-03-28 11:56:06 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1872,6 +1872,7 @@ var cnames_active = {
   "ngspice": "42ua.github.io/ngspice.js",
   "ni": "js-ni.github.io/ni",
   "nic": "nic.github.io",
+  "nie": "love2wind.github.io",
   "nightcord": "rellylegend.github.io/nightcord",
   "nikita": "adaltas.github.io/node-nikita",
   "nikmartin": "nikmartin.github.io",
