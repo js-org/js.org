c64fb7c48f716ab68065b6fc0874e72d95ad7b35
Antoine Lenoir <antoine.alenoir@gmail.com>
2022-04-01 16:01:16 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -123,6 +123,7 @@ var cnames_active = {
   "alasql": "alasql.github.io", // noCF? (don´t add this in a new PR)
   "alastor": "aidenybai.github.io/alastor",
   "ale": "alecefe.github.io",
+  "alenoir": "alenoir.github.io",
   "alertbox": "simple-alertbox.github.io/alertbox",
   "alertism": "assassinaguilar.github.io/Alertism",
   "alex": "alecs297.github.io",
