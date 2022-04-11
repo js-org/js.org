114406c199d5b370000bebe38f7bd3d5f2b67e8f
Swôth <56405862+Swothh@users.noreply.github.com>
2022-04-11 16:05:38 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1554,7 +1554,7 @@ var cnames_active = {
   "ling": "wangziling.github.io",
   "linghucong": "jiji262.github.io",
   "lingui": "cname.vercel-dns.com", // noCF
-  "linkcord": "5bbd9e63-9b02-4651-ac43-3d4b4fb60364.id.repl.co",
+  "linkcord": "5bbd9e63-9b02-4651-ac43-3d4b4fb60364.id.repl.co", // noCF
   "linkify": "hypercontext.github.io/linkifyjs",
   "lips": "jcubic.github.io/lips",
   "lipsum": "llxlr.github.io/lipsum.js",
