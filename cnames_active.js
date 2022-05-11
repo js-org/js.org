14be87c19514274698b5c3165c8468d481622b5e
CallMeChou <63540671+CallMeChou@users.noreply.github.com>
2022-05-11 20:43:25 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3189,6 +3189,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes"
+  "zzz":"callmechou.github.io/blog"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
