9bb91cf79c3729893f374e9cc8e971b442b71cbc
0x11 <wanzhi09@gmail.com>
2022-05-11 09:28:19 +0800

    add "sh" to cnames_active list

--- cnames_active.js
+++ cnames_active.js
@@ -2507,6 +2507,7 @@ var cnames_active = {
   "sesam": "lennertderyck.github.io/sesam",
   "setheum": "setheum-js.github.io",
   "sfml": "xadillax.github.io/node-sfml",
+  "sh": "0x11-me.github.io/sh",
   "shandler": "hosting.gitbook.io", // noCF
   "sharding": "infinitytmbots.github.io/discord-hybrid-sharding",
   "share": "jeff-tian.github.io/weshare",
