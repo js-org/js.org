d64fd6f2247eab3bdedcdac776ae10cae0a2e981
miaoyanginfo <paulmiao@me.com>
2022-05-18 14:05:25 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3193,6 +3193,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes"
+  "miaoyang": "miaoyanginfo.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
