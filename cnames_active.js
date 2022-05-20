1e60dbcb0aa0d1a4705e9ab88b7db744e2b13c60
Pelfox <me@pelfox.dev>
2022-05-20 21:41:04 +0300

    fix

--- cnames_active.js
+++ cnames_active.js
@@ -1212,7 +1212,7 @@ var cnames_active = {
   "helpful": "togatech.github.io/helpful.js",
   "hepsiburada": "berkaltiok.github.io/hepsiburada",
   "heroku": "gary50613.github.io/herokuapi.js",
-  "hesperus": "https://hesperusjs.github.io/hesperus/",
+  "hesperus": "hesperusjs.github.io/hesperus",
   "hexoplusplus": "hexoplusplus.github.io/docs",
   "hfsyun": "hfsyun.github.io",
   "hibiki": "claritymoe.github.io/hibiki",
