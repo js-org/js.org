9c20b451b1bdeb321c4cd85df3e9abf37b369d16
NKDuy <68154054+khanhduy1407@users.noreply.github.com>
2022-05-24 14:20:26 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -744,6 +744,7 @@ var cnames_active = {
   "discords": "botstudios.github.io/discords.js.org",
   "discordutility": "chaosphoe.github.io/discordutility",
   "discuss": "cname.vercel-dns.com", // noCF
+  "discuss-duroom": "https://discuss-duroom.herokuapp.com/",
   "display": "arguiot.github.io/DisplayJS",
   "distillery": "achannarasappa.github.io/distillery", // noCF? (don´t add this in a new PR)
   "distri": "flarp.github.io/Distri.js",
