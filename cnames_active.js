bd861566041f0e972181b5d2b6f929d3f5196208
Belem Zhang <belem.zhang@intel.com>
2022-05-31 16:56:50 +0800

    pwachina.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2192,6 +2192,7 @@ var cnames_active = {
   "pwa-cookbook": "sylvainpolletvillard.github.io/pwa-cookbook",
   "pwa-workshop": "sylvainpolletvillard.github.io/pwa-workshop",
   "pwapp": "nikiforov-org.github.io/pwapp",
+  "pwachina": "https://github.com/ibelem/pwa-developer-day",
   "qbit": "fudan-mse.github.io/qbit",
   "qbsz": "qbsz.github.io",
   "qs": "kirjs.github.io/qs.js", // noCF? (don´t add this in a new PR)
