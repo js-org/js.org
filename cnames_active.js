d85ceb49b8786cc04be72da1e2ee7be0be39c6c1
tf99921 <90237373+tf99921@users.noreply.github.com>
2022-06-05 01:47:07 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -195,6 +195,7 @@ var cnames_active = {
   "argo": "albertosantini.github.io/argo", // noCF? (don´t add this in a new PR)
   "ari": "arbo77.github.io/ari",
   "ariang": "p3terx.github.io/ariang", // noCF
+  "ariangv1.2.4": "tf99921.github.io/AriaNgv1.2.4",
   "arief": "1997arief.github.io",
   "arime": "ninbryan.github.io/arime", // noCF? (don´t add this in a new PR)
   "arithmy": "arithmy.netlify.app",
