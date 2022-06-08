ce354a743fc5a4b3aaabc3c89a24ee666a85d870
Ron B. Yeh <ronyeh@gmail.com>
2022-06-08 01:33:10 -0700

    Add guitar.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1168,6 +1168,7 @@ var cnames_active = {
   "guildeno": "guildeno.github.io/guildeno",
   "guilherme": "guilhermedelemos.github.io/guilherme",
   "guillotine": "matiasgagliano.github.io/guillotine", // noCF? (don´t add this in a new PR)
+  "guitar": "ronyeh.github.io/guitar",
   "gully": "nmabhinandan.github.io/gully", // noCF? (don´t add this in a new PR)
   "gulpkit": "gulpkit.github.io/GulpKit",
   "gun": "gundb.github.io", // noCF? (don´t add this in a new PR)
