b11f1804177f4e81021b94d30cd9c23c05ad3560
moluranli <2285288446@qq.com>
2022-06-15 12:18:41 +0800

    申请moluranliStudy域名

--- cnames_active.js
+++ cnames_active.js
@@ -1813,6 +1813,7 @@ var cnames_active = {
   "mokunshao": "mokunshao.github.io",
   "mol": "eigenmethod.github.io/mol", // noCF
   "molti": "moltijs.github.io/molti",
+  "moluranliStudy": "moluranli.github.io/moluranliStudy"
   "mom": "momjs.github.io/mom",
   "momentum": "wemakeweb.github.io/momentum", // noCF? (don´t add this in a new PR)
   "monaco-tailwindcss": "monaco-tailwindcss.netlify.app",
