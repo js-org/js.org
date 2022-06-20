748d540a9c8c6768395f5338223ab86e0d26d491
Daniel Lu <95860724+DWhirlpool@users.noreply.github.com>
2022-06-20 13:33:54 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -531,6 +531,7 @@ var cnames_active = {
   "colorlab": "signalwerk.github.io/colorlab",
   "colors": "xv700.github.io/colors",
   "colors-web": "yu-tou.github.io/colors-web",
+  "colorshelll": "colorshelll.pages.dev",
   "comixngn": "seun40.github.io/comix-ngn", // noCF? (don´t add this in a new PR)
   "command": "satoqz.github.io/command.ts",
   "commandcord": "commandcord.github.io",
