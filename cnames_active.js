3cdf81f011c814993560b969d029b52cea4517a6
cfwvip <44077129+cfwvip@users.noreply.github.com>
2022-06-28 11:20:59 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -455,6 +455,7 @@ var cnames_active = {
   "ceruttimaicon": "ceruttimaicon.github.io",
   "cesaralvarez": "cesaralvrz.github.io/cesaralvarez",
   "ceyhun": "ceyhunmelek.github.io/ceyhun",
+  "cfw": "cfwvip.github.io",
   "cgoatone": "cgoatone.github.io",
   "chain-able": "fluents.github.io/chain-able-site",
   "chainson": "abcdan.github.io/chainson",
