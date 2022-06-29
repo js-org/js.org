1da1620e084c63f5c8a31be953e1e833a45b46f1
barel31 <barel123133@gmail.com>
2022-06-30 00:49:28 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3226,6 +3226,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes"
+  "barel": "https://barel-portfolio.vercel.app/"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
