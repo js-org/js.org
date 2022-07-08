f7645309490d8ae7eb57a51c99f4337a30ecf04f
FlorZR <108909932+FlorZR@users.noreply.github.com>
2022-07-08 12:50:53 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1018,6 +1018,7 @@ var cnames_active = {
   "flood": "flood.jesec.io", // noCF
   "floppy": "hosting.gitbook.io", // noCF
   "flor": "florplayer.github.io",
+  "florzr": "florzr.github.io/prueba",
   "flowchart": "adrai.github.io/flowchart.js", // noCF? (don´t add this in a new PR)
   "flowcon": "oknosoft.github.io/flowcon",
   "flowjv": "pkishorez.github.io/flowjv",
