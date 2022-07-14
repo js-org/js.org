10c225ea68e20738bd4c937aa8c5dbcec4c88e0d
Edxel Altamirano <73353105+gabaltdev@users.noreply.github.com>
2022-07-13 19:07:08 -0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2160,6 +2160,7 @@ var cnames_active = {
   "pong-zh": "xiaozhu2021.github.io/javascript-pong",
   "popcorn-api": "nirewen.github.io/popcorn-api",
   "popper": "fezvrasta.github.io/popper.js",
+  "dxgabalt": "gabaltdev.github.io/portfolio/",
   "portafolio-web": "velazquezadrian.github.io/Portafolio-web",
   "portafolioalejandrarobles": "alerobles3.github.io/portafolioalejandrarobles",
   "portafolioarnulfo": "arpior.github.io/Portafolio",
