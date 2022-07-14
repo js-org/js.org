bb7cd4ee373e82875f8c82252dd28d4efadec91d
plurryt <70047632+pythoniaweb@users.noreply.github.com>
2022-07-14 16:57:41 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2103,8 +2103,8 @@ var cnames_active = {
   "ph1p": "ph1p.github.io",
   "phaker": "phakerjs.github.io/phaker.js",
   "pharaoh": "pharaoh-js.github.io", // noCF? (don´t add this in a new PR)
-  "phazor": "phazor-js.github.io/Phazor.js",
   "pharo": "pharojs.github.io/pharo-js-org",
+  "phazor": "phazor-js.github.io/Phazor.js",
   "phi": "phi-tools.netlify.com",
   "phisherman": "thewilloftheshadow.github.io/phisherman.js",
   "phobos": "phobosjs.github.io/phobos.js",
