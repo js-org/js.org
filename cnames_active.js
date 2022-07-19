f7a6f94886b80019a244c49651ea2f389fd46df4
İlker Öztürk <108209673+qorncat@users.noreply.github.com>
2022-07-19 22:34:29 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2228,6 +2228,7 @@ var cnames_active = {
   "pwapp": "nikiforov-org.github.io/pwapp",
   "qbit": "fudan-mse.github.io/qbit",
   "qbsz": "qbsz.github.io",
+  "qorn": "qorn.github.io",
   "qs": "kirjs.github.io/qs.js", // noCF? (don´t add this in a new PR)
   "quacky": "quacky-bot.github.io",
   "quantlib": "quantlibjs.github.io",
