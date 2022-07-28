12f42e2e9e65cf2e5afa74b9b6516e056445c57b
HoBoxz <88186876+HoBoxz@users.noreply.github.com>
2022-07-28 22:45:59 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2885,6 +2885,7 @@ var cnames_active = {
   "tiza": "pd4d10.github.io/tiza",
   "tjys": "qikaile.github.io/tjys-notes",
   "tmr": "tmr-blog.netlify.app",
+  "tm-team": "hoboxz.github.io/Three-Musketeers"
   "toad": "wnda.github.io/toad",
   "tom": "cname.vercel-dns.com",
   "tom-select": "orchidjs.github.io/tom-select",
