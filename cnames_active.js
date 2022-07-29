6ce1200de891f1015119482b5e1db5c18d1cf618
Stefan Keim <indus@users.noreply.github.com>
2022-07-29 20:45:23 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2002,12 +2002,12 @@ var cnames_active = {
   "nuxt-fluent-bootstrap": "disjfa.github.io/nuxt-fluent-bootstrap",
   "nuxtdoc": "hopeful-hoover-d87dd1.netlify.com",
   "nva": "ali322.github.io/nva",
+  "nx": "nx-space.github.io/nx-docs", // noCF
   "nxd": "niuxvdong.github.io",
   "nxse": "nxse.github.io",
   "nyax": "springnyan.github.io/nyax",
   "nyc": "nycjsorg.github.io/nyc",
   "nyr": "suriyaakudoisc.github.io/NYR",
-  "nx": "nx-space.github.io/nx-docs", // noCF
   "oak": "donn.github.io/Oak.js",
   "object-gui": "matteobruni.github.io/object-gui",
   "objectid": "the-silver-project.github.io/objectid",
