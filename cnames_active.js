d74a432122dc7b72b73eecba3e2ee0d3fba900fc
XDEandTOM <76867531+XDEandTOM@users.noreply.github.com>
2022-07-30 23:37:47 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3208,6 +3208,7 @@ var cnames_active = {
   "xstate": "davidkpiano.github.io/xstate",
   "xto6": "mohebifar.github.io/xto6", // noCF? (don´t add this in a new PR)
   "xtype": "lucono.github.io/xtypejs", // noCF? (don´t add this in a new PR)
+  "xdeandtom": "xdeandtom.github.io"
   "y2j": "ultirequiem.github.io/y2j",
   "y86": "quietshu.github.io/y86", // noCF? (don´t add this in a new PR)
   "yadl": "yadljs.github.io",
