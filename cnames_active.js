97b8bc1db05af8f11da115903353da7243423e62
Elijah <business@elijahpepe.com>
2022-07-30 17:55:13 -0700

    Add multiversus.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1874,6 +1874,7 @@ var cnames_active = {
   "muiplayer": "muiplayer.github.io",
   "mujin": "mujin669.github.io",
   "multiple": "nexts.github.io/Multiple.js",
+  "multiversus": "elijahpepe.github.io/multiversus.js",
   "munhumutapaalpha": "munhumutapaalpha.github.io/wallet",
   "murajaah": "mzaini30.github.io/murajaah-js",
   "murathalitsever": "murathalitsever.github.io",
