df42bd718489bb8e32b8aaa43589db1464687287
会影 <shaoyin.ssy@alibaba-inc.com>
2022-08-01 09:38:46 +0800

    fix: update cn.redux, remove noCF

--- cnames_active.js
+++ cnames_active.js
@@ -517,7 +517,7 @@ var cnames_active = {
   "cn.history": "doodlewind.github.io/jshistory-cn", // noCF
   "cn.iflow": "unadlib.github.io/iflow-docs-cn",
   "cn.mobx": "sangka.github.io/mobx-docs-cn", // noCF
-  "cn.redux": "nefe.github.io/redux-in-chinese", // noCF? (don´t add this in a new PR)
+  "cn.redux": "nefe.github.io/redux-in-chinese",
   "cn.rx": "rxjs-cn.github.io/RxJS-Docs-CN", // noCF
   "cnc": "cncjs.github.io/cncjs.org",
   "cndrew": "codemaster233.github.io",
