e169813e40060b3220474bbb39bf4e65cf39db22
Anand Chowdhary <github@anandchowdhary.com>
2022-08-13 14:29:59 +0530

    nord.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1980,6 +1980,7 @@ var cnames_active = {
   "nomic": "basicer.github.io/nomic.js-ui",
   "noo": "uyouthe.github.io/noo",
   "noobscroll": "arguiot.github.io/NoobScroll",
+  "nord": "anandchowdhary.github.io/nord.js",
   "notasdeprogramador": "hugoroca.github.io/notasdeprogramador",
   "notebook": "wugenqiang.github.io/NoteBook",
   "noted": "carter-slade.github.io/Noted-", // noCF? (don´t add this in a new PR)
