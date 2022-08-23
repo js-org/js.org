e445b4e588f1722021ef11edf757768c48bab800
mingdev <mingdev@163.com>
2022-08-23 17:40:17 +0800

    update xchart domain

--- cnames_active.js
+++ cnames_active.js
@@ -3091,7 +3091,7 @@ var cnames_active = {
   "wysc": "coffeebank.github.io/wysc-vue",
   "wz": "wangzhen-chn.github.io/wz",
   "x": "nikhiljohn10.github.io/x.js",
-  "xchart": "xchart.vercel.app",
+  "xchart": "cname.vercel-dns.com",
   "xen": "hosting.gitbook.com",
   "xendb": "notmarx.github.io/Xen.db",
   "xhemj": "xhemj.now.sh", // noCF
