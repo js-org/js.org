7dcb088b6755f2842859a26251dcbe500a704885
Stefan Keim <indus@users.noreply.github.com>
2022-08-31 16:01:07 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -37,7 +37,6 @@
  *              BECAUSE OF THE RISK OF ABUSE, I´M NOT ACCEPTING REQUESTS FOR PAGES WITHOUT REASONABLE CONTENT!
  */
 
-
 var cnames_active = {
   "": "js-org.github.io",
   "01mz": "01mz.github.io",
