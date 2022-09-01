7e866ecb1d66e231efaae80a1d110b2cd430f5b8
Stefan Keim <indus@users.noreply.github.com>
2022-09-01 16:07:57 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2837,8 +2837,8 @@ var cnames_active = {
   "tagster": "goschevski.github.io/tagster", // noCF? (don´t add this in a new PR)
   "taha": "orcxzjeeeee.github.io/taha",
   "tailwind-baseline": "apkoponen.github.io/tailwind-baseline-site",
-  "talk": "zonayedpca.github.io/talk.js",
   "takeout": "takeout-bysourfruit.github.io",
+  "talk": "zonayedpca.github.io/talk.js",
   "talker": "secondstreet.github.io/talker.js", // noCF? (don´t add this in a new PR)
   "tama": "etk.gitlab.io",
   "tampered-console": "hosting.gitbook.io", // noCF
