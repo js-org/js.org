499adf6bc7be66bd29e63c4fb60163583a65c528
Lifeless Muffin <72659557+lifeless-muffin@users.noreply.github.com>
2022-09-01 18:59:17 +0530

    Added "muff": "lifeless-muffin.github.io/muff"
    CNAME added on line number 1884. Also really appreciate what you guys are doing.

--- cnames_active.js
+++ cnames_active.js
@@ -1881,6 +1881,7 @@ var cnames_active = {
   "msp430": "mazko.github.io/MSP430.js",
   "mtproto-core": "alik0211.github.io/mtproto-core-website",
   "mubaidr": "mubaidr.github.io",
+  "muff": "lifeless-muffin.github.io/muff",
   "muiplayer": "muiplayer.github.io",
   "mujin": "mujin669.github.io",
   "multiple": "nexts.github.io/Multiple.js",
