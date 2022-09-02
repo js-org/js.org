b65204b9c993f66654cb93e4e77baf0ff0cbf351
ttimochan <teemo@timochan.cn>
2022-09-02 18:50:12 +0800

    add : no cf

--- cnames_active.js
+++ cnames_active.js
@@ -1899,7 +1899,7 @@ var cnames_active = {
   "mvc": "morozovsk.github.io/mvc", // noCF
   "mw": "agauniyal.github.io/mw",
   "mwap": "jacob-ebey.github.io/mwap",
-  "mx-space": "mx-space.github.io/docs",
+  "mx-space": "mx-space.github.io/docs", // noCF
   "my-app": "nirrius.github.io/my-app",
   "my-server": "nirrius.github.io/my-server",
   "mylas": "raouldeheer.github.io/Mylas",
