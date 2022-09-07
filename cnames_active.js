a9b5c7ae3671aa3922994667fbd7e05f3725563b
JesusZacarias <59021713+JesusZacarias@users.noreply.github.com>
2022-09-06 23:13:31 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3258,6 +3258,7 @@ var cnames_active = {
   "yennj12": "yennanliu.github.io",
   "yh": "hksat.github.io",
   "yiff": "codepupper.github.io/yiff",
+  "yisusblog": "jesuszacarias.github.io/yisusblog/",
   "ymlair": "ymlair.github.io/TimestampHumanDemo",
   "yolog": "jitesoft.github.io/yolog",
   "youngjuning": "youngjuning.github.io",
