c9447101c8858b89bffb199ff8b984e3793363e3
TripleG-779 <48449868+TripleG-779@users.noreply.github.com>
2022-09-17 16:43:13 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2203,6 +2203,7 @@ var cnames_active = {
   "pong-zh": "xiaozhu2021.github.io/javascript-pong",
   "popcorn-api": "nirewen.github.io/popcorn-api",
   "popper": "fezvrasta.github.io/popper.js",
+  "portafolio3g": "portafolio3g.js.org",
   "portafolio-web": "velazquezadrian.github.io/Portafolio-web",
   "portafolioalejandrarobles": "alerobles3.github.io/portafolioalejandrarobles",
   "portafolioarnulfo": "arpior.github.io/Portafolio",
