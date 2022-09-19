d025911b5097516781630a194aaec0a2ec742fb4
CRIMX <straybugs@gmail.com>
2022-09-20 00:49:22 +0800

    feat: add value-enhancer.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -3061,6 +3061,7 @@ var cnames_active = {
   "valine": "xcss.github.io/valine",
   "valirator": "massive-angular.github.io/valirator",
   "valobot": "misly16.github.io/ValoBotsite",
+  "value-enhancer": "crimx.github.io/value-enhancer",
   "vanessa": "vanessa219.github.io/vanessa",
   "vanilla": "haeresis.github.io/vanilla-js-dom",
   "vanilla-picker": "sphinxxxx.github.io/vanilla-picker",
