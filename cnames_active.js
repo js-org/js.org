e9a6ab377b5f248b3987989fff35e390b9511595
Giancarlo3G <gian.fayg77@gmail.com>
2022-09-19 12:14:59 -0500

    Fixed

--- cnames_active.js
+++ cnames_active.js
@@ -2203,7 +2203,7 @@ var cnames_active = {
   "pong-zh": "xiaozhu2021.github.io/javascript-pong",
   "popcorn-api": "nirewen.github.io/popcorn-api",
   "popper": "fezvrasta.github.io/popper.js",
-  "portafolio3g": "portafolio3g.js.org",
+  "portafolio3g": "tripleg-779.github.io/Portafolio",
   "portafolio-web": "velazquezadrian.github.io/Portafolio-web",
   "portafolioalejandrarobles": "alerobles3.github.io/portafolioalejandrarobles",
   "portafolioarnulfo": "arpior.github.io/Portafolio",
