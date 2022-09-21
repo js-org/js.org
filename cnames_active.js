43e53f13c35d6182117866ad816d11c870ec2670
Waterworks <100122955+hitptep@users.noreply.github.com>
2022-09-21 21:32:24 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3307,7 +3307,6 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes"
-  "zzy":"hitptep.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
