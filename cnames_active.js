e4f16232b591207becf08be16f96b71efccbeb48
Bebo <109833811+iibeboQ@users.noreply.github.com>
2022-10-02 17:29:42 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -299,6 +299,7 @@ var cnames_active = {
   "beaga": "beagajs.github.io/beaga",
   "beamwind": "kenoxa.github.io/beamwind",
   "beatzoid": "beatzoid.github.io",
+  "beboq": "iibeboq.github.io/beboq",
   "bee": "beejsdev.github.io",
   "begin": "advanced-webapps-class.github.io/begin", // noCF? (don´t add this in a new PR)
   "belfast": "chrislaughlin.github.io/belfastjs",
