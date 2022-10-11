a57ca362e3d2c9593254a9c2506aff714a63abff
Param Siddharth <contact@paramsid.com>
2022-10-12 02:00:37 +0530

    Move Rema to Cloudflare nameserveres
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- ns_active.js
+++ ns_active.js
@@ -17,6 +17,7 @@ var ns_active = {
   "castyte": ["ns31.cloudns.net", "ns32.cloudns.net", "ns33.cloudns.net", "ns34.cloudns.net"],
   "engine262": ["brad.ns.cloudflare.com", "lia.ns.cloudflare.com"],
   "lolifamily": ["glen.ns.cloudflare.com", "melany.ns.cloudflare.com"],
+  "rema": ["crystal.ns.cloudflare.com", "justin.ns.cloudflare.com"],
   "ruby": ["alexis.ns.cloudflare.com", "connie.ns.cloudflare.com"],
   "samplasion": ["ns31.cloudns.net", "ns32.cloudns.net", "ns33.cloudns.net", "ns34.cloudns.net"],
   "shebang": ["dns1.p01.nsone.net", "dns2.p01.nsone.net", "dns3.p01.nsone.net", "dns4.p01.nsone.net"]
