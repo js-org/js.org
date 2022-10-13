d0011ccd9baa23c4df770cac61347ce272dcf7ff
Danielius Vaisnoras <84046635+gamertike@users.noreply.github.com>
2022-10-13 18:59:31 +0100

    feat: add `docs.guildedts` and update `guildedts`

--- cnames_active.js
+++ cnames_active.js
@@ -801,6 +801,7 @@ var cnames_active = {
   "dmitry": "dmitry-zaets.github.io",
   "dna": "dna-engine.github.io/dna-engine",
   "docile": "russellsteadman.github.io/docile",
+  "docs.guildedts": "guildedts.github.io/guilded.ts",
   "docs.light": "hosting.gitbook.com", // noCF
   "docs.modmail": "botstudios.github.io/modmail.js-docs", // noCF
   "docs.mosaic": "hosting.gitbook.io", // noCF
@@ -1204,7 +1205,7 @@ var cnames_active = {
   "guilded": "guildedjs.github.io/guilded.js",
   "guildedapi": "rellylegend.github.io/guilded-api.js",
   "guildedbot": "guilded-bot-npm.github.io/Guilded-Bot-Docs",
-  "guildedts": "guildedts.github.io/guilded.ts",
+  "guildedts": "cname.vercel-dns.com", // noCF
   "guildeno": "guildeno.github.io/guildeno",
   "guilherme": "guilhermedelemos.github.io/guilherme",
   "guillotine": "matiasgagliano.github.io/guillotine", // noCF? (don´t add this in a new PR)
