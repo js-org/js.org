ae274fd6e96d94514a2268633e390cd7c178ea71
Komorebi <aiokr@outlook.com>
2022-10-20 09:42:12 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3325,7 +3325,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes",
-  "type": "photup.github.io"
+  "photup": "photup.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
