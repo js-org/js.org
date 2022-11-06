fef1d503fc900f950138bb950f11e37a8bbe6738
Luke Carr <24438483+lukecarr@users.noreply.github.com>
2022-11-06 21:31:14 +0000

    Added crls

--- cnames_active.js
+++ cnames_active.js
@@ -620,6 +620,7 @@ var cnames_active = {
   "create-project": "chalarangelo.github.io/create-js-project",
   "createrest": "atomixinteractions.github.io/createrest",
   "creaturegen": "ryancromebook.github.io/creaturegen",
+  "crls": "lukecarr.github.io/crls",
   "crossani": "cname.vercel-dns.com", // noCF
   "crq": "shbwb.github.io",
   "crunch": "vukicevic.github.io/crunch", // noCF? (don´t add this in a new PR)
