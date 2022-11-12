61550f3ad5e165edb35426350075a7b8ddf8152e
Itzik <37775846+PatentLobster@users.noreply.github.com>
2022-11-12 16:18:28 +0200

    Update cnames_active.js
    Co-authored-by: Matt Cowley <me@mattcowley.co.uk>

--- cnames_active.js
+++ cnames_active.js
@@ -1408,7 +1408,7 @@ var cnames_active = {
   "itemszop": "michaljaz.github.io/itemszop",
   "itsashis4u": "itsashis4u.github.io", // noCF? (don´t add this in a new PR)
   "itunes-bridge": "angrykiller.github.io/iTunes-bridge",
-  "itzik": "cname.vercel-dns.com",
+  "itzik": "cname.vercel-dns.com", // noCF
   "iwanttocountfromone": "realcyguy.github.io/iwanttocountfromone.js",
   "izmir": "fdemir.github.io/jsizmir.github.io",
   "j3n5en": "j3n5en.github.io",
