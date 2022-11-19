2945f6542dc2edaa2ec2bcdabd4af82a24b13ab3
Wild <118611832+proyectopy@users.noreply.github.com>
2022-11-19 20:38:48 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2273,6 +2273,7 @@ var cnames_active = {
   "protype": "arguiot.github.io/ProType",
   "proxy.meow": "cname.vercel-dns.com", // noCF
   "proyecto-estadistica-2020": "eduardodevop.github.io/proyecto-estadistica-2020",
+  "proyectopy": "proyectopy.github.io/proyectopy.hugo",
   "pruf": "signalwerk.github.io/pruf",
   "pruthvisoni": "pruthvisooni.github.io",
   "ps": "spindlyskit.github.io/ps.js",
