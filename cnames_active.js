19e8cc84b444f1307ce27355275eef05e88b1f24
1Lucas1apk <76886832+1Lucas1apk@users.noreply.github.com>
2022-11-24 13:47:27 -0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1894,7 +1894,7 @@ var cnames_active = {
   "mono": "mono-js.github.io/mono",
   "monopay": "alitnk.github.io/monopay",
   "montage": "montagejs.github.io",
-  "moonlink: "apex-loadbalancer.netlify.com",
+  "moonlink": "apex-loadbalancer.netlify.com",
   "moose": "mustpax.github.io/moose",
   "moro": "omidfi.github.io/moro",
   "morocco": "moroccojs.github.io",
