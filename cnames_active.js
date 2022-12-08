c9763046e172e7dd587846fa66448a0d833ebf94
phanendraguptha <www.phani122@gmail.com>
2022-12-08 17:57:51 +0530

    updated designdevresources.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -715,7 +715,7 @@ var cnames_active = {
   "deportes": "andreapins.github.io/Deportes-Gacetis",
   "deputy": "ry4nolson.github.io/Deputy", // noCF? (don´t add this in a new PR)
   "des-docs": "shadowplay1.github.io/des-docs",
-  "designdevresources": "phanendraguptha.github.io/designdevresources",
+  "designdevresources": "designdevresource.netlify.app",
   "detect-resize": "noderaider.github.io/detect-resize", // noCF? (don´t add this in a new PR)
   "detector": "blackmirror1980.github.io/detector-js",
   "deterministic": "nodeguy.github.io/Deterministic.js",
