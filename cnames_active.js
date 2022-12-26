013114e8f32f45acc5a59cb8d165eccc439599fe
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2022-12-26 17:29:02 -0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3370,7 +3370,8 @@ var cnames_active = {
   "zxy": "zxyfrank.github.io",
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "artificial": "artificial.js.org"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
