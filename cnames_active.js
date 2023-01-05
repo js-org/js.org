705af29396476b6a30a103558227783a51118fad
timigomen <121149519+timigomen@users.noreply.github.com>
2023-01-05 09:30:35 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3378,6 +3378,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes"
+  "gtc": "wtikoofoekingwdy.eu.org"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
