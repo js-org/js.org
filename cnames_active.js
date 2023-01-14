7632cf6068e7006000fb07de56bdbc262ca2182d
星宿君 <huxinfeng1024@qq.com>
2023-01-15 01:12:41 +0800

    Add `polona-doc.js.org`
    Polona-低代码数据可视化平台文档

--- cnames_active.js
+++ cnames_active.js
@@ -2254,6 +2254,7 @@ var cnames_active = {
   "polix": "polix.netlify.com",
   "polkadot": "polkadot-js.github.io",
   "polona": "huxinfeng.github.io/polona",
+  "polona-doc": "huxinfeng.github.io/polona-doc",
   "polygon-clipping": "mfogel.github.io/polygon-clipping",
   "polythene": "arthurclemens.github.io/Polythene", // noCF? (don´t add this in a new PR)
   "pomidorus": "tatyshev.github.io/pomidorus",
