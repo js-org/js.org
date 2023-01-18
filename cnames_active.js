aa7ae266c841c822f7a19523ffb8b709e1f76291
orangezsclite <118232150+orangezsclite@users.noreply.github.com>
2023-01-18 12:49:47 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1516,6 +1516,7 @@ var cnames_active = {
   "jwt-autorefresh": "cchamberlain.github.io/jwt-autorefresh", // noCF? (don´t add this in a new PR)
   "jwtonline": "boneyt92.github.io/jwtonline",
   "jz": "jz6.github.io",
+  "jvav": "orangezsclite.github.io/Jvav",
   "kafka": "tulios.github.io/kafkajs",
   "kaguwo": "kaguwos-portfolio.netlify.app",
   "kahoot": "na-west1.surge.sh",
