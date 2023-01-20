4339ed0aec057c1779da1319a95980e13f7b698e
William Harrison <william@williamdavidharrison.com.au>
2023-01-20 11:41:27 +0800

    Add `filehost.js.org`

--- cnames_active.js
+++ cnames_active.js
@@ -1046,6 +1046,7 @@ var cnames_active = {
   "fidel": "fidelxyz.github.io",
   "fikri": "ikrydev.github.io/fikri.js.org",
   "file-extension": "ultirequiem.github.io/file-extension",
+  "filehost": "williamdavidharrison.github.io/express-file-host",
   "filer": "filerjs.github.io/filer",
   "fill100": "matteobruni.github.io/fill100",
   "filter": "mg98.github.io/filter.js",
