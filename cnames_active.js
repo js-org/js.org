7bf468315c07e4998c3a9c439c43f3845d94f2eb
GavinWells <31149899+geekswg@users.noreply.github.com>
2023-02-14 00:17:37 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1149,6 +1149,7 @@ var cnames_active = {
   "gdal3": "bugra9.github.io/gdal3.js",
   "gdi": "katehra-brc.github.io/gdi.js.org",
   "geekr": "ruanyl.github.io/geekr", // noCF? (don´t add this in a new PR)
+  "geekswg": "geekswg.github.io",
   "gemstone": "pierreavn.github.io/gemstone",
   "generate-license-file": "tobyandtoby.github.io/generate-license-file",
   "geniucker": "geniucker.github.io",
