ad273857899b3a10900125e26158cb2d4b8cdcd0
singingqin <84577131+singingqin@users.noreply.github.com>
2023-02-13 09:15:24 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3320,6 +3320,7 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes"
   "sing": "singingqin.github.io"
+  "haha": "singingqin-github-io.vercel.app"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
