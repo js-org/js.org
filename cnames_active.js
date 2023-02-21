a23e6a0a7b51f0b532540ed5ef7dff8926564cac
wszmk <125188461+wszmk@users.noreply.github.com>
2023-02-21 12:45:15 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3274,7 +3274,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes",
-  "zmk": "wszmk.github.io/book"
+  "zmk": "wszmk.github.io/book",
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
