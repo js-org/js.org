a7492b6db66e7d4380bd56446d821c50d3051111
Stefan Keim <indus@users.noreply.github.com>
2023-03-11 18:42:24 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1605,8 +1605,8 @@ var cnames_active = {
   "ling": "wangziling.github.io",
   "linghucong": "jiji262.github.io",
   "lingui": "cname.vercel-dns.com", // noCF
-  "linkcord": "793d3e52-aa35-4851-aba6-e0905331fe2a.id.repl.co", // noCF
   "link-styler": "sheikhoo.github.io/link-styler",
+  "linkcord": "793d3e52-aa35-4851-aba6-e0905331fe2a.id.repl.co", // noCF
   "linkify": "hypercontext.github.io/linkifyjs",
   "lips": "jcubic.github.io/lips",
   "lipsum": "llxlr.github.io/lipsum.js",
