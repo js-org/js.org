d92b87c9508f43fb6abe918331d7d84adea17401
fanmau <127578292+fanmau@users.noreply.github.com>
2023-03-30 19:38:01 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3297,7 +3297,7 @@ var cnames_active = {
   "zty": "zetaoyang.github.io",
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
   "fanmai": "fanmau.github.io"
   /*
    * please don't add your subdomain records down here!
