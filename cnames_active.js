5fee78fb6f40f92ae197573915ac6793267faeb2
zcns <130817248+zcns@users.noreply.github.com>
2023-05-23 21:04:27 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2040,6 +2040,7 @@ var cnames_active = {
   "nyc": "nycjsorg.github.io/nyc",
   "nyr": "suriyaakudoisc.github.io/NYR",
   "oak": "donn.github.io/Oak.js",
+  "op": "ofap.github.io",
   "object-gui": "matteobruni.github.io/object-gui",
   "objectid": "the-silver-project.github.io/objectid",
   "objectmodel": "sylvainpolletvillard.github.io/ObjectModel", // noCF? (don´t add this in a new PR)
