faec91a4f59616337378cdb095663f1a6fc30c59
Monowar Hussain <95315609+techMonowar@users.noreply.github.com>
2023-05-25 03:58:55 +0530

    Update cnames_active.js
    Co-authored-by: Matt Cowley <me@mattcowley.co.uk>

--- cnames_active.js
+++ cnames_active.js
@@ -1740,7 +1740,7 @@ var cnames_active = {
   "max": "toorusr.github.io/max.js.org",
   "maxnachlinger": "maxnachlinger.github.io", // noCF? (don´t add this in a new PR)
   "maxtracking": "maxtracking.github.io", // noCF? (don´t add this in a new PR)
-  "maxvalid":"techmonowar.github.io/maxvalid",
+  "maxvalid": "techmonowar.github.io/maxvalid",
   "maze95": "maze95.github.io",
   "mc": "magicarbon.github.io/mc", // noCF? (don´t add this in a new PR)
   "mca": "lem0nb.github.io/Minecraft_Auto_Builder",
