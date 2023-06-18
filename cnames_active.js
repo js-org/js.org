01c28d5bf371303e691e918addf4c9b036810fa3
phanendraguptha <www.phani122@gmail.com>
2023-06-18 20:35:26 +0530

    added noCF comment

--- cnames_active.js
+++ cnames_active.js
@@ -1641,7 +1641,7 @@ var cnames_active = {
   "liquid-ajax-cart": "evgeniymukhamedjanov.github.io/liquid-ajax-cart",
   "liquivelte": "liquivelte.github.io/docs",
   "lister": "jpbulman.github.io/lister",
-  "listenin": "cname.vercel-dns.com",
+  "listenin": "cname.vercel-dns.com", // noCF
   "listore": "yieldray.github.io/listore",
   "lite-web-server": "chasyumen.github.io/lite-web-server",
   "litedom": "mardix.github.io/litedom", // noCF
