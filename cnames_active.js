1b5d32b39e69d33e3d951cf80e809efd463376b9
ryzm <contact@ryzm.tech>
2023-07-07 15:03:21 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3358,7 +3358,8 @@ var cnames_active = {
   "zustand-cn": "cname.vercel-dns.com", // noCF
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "dynamotion": "https://dynamotion.vercel.app"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
