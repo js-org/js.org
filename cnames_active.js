bb38ac3dd3bcda7e1f9d87f92adeb6569f9c807a
PythonScratcher <81612672+PythonScratcher@users.noreply.github.com>
2023-07-08 21:13:09 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3099,6 +3099,7 @@ var cnames_active = {
   "ushakov": "ushakov-igor.github.io/ushakov",
   "ushio": "ushio.netlify.com",
   "usv": "usvformat.github.io",
+  "utc": "pythonscratcher.net/utc",
   "utilmonkey": "cyberdev101.github.io/utilmonkey-docs",
   "utils-core": "dev-pengi.github.io/utils-core",
   "utm": "espressofan.github.io/utm",
