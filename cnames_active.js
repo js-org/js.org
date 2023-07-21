e426db29d1820e12ea3038e1375e932dbb05e213
Ray <nn_201312@163.com>
2023-07-21 22:52:48 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -511,6 +511,7 @@ var cnames_active = {
   "clause": "clausejs.github.io/clausejs",
   "cleaners": "swansontec.github.io/cleaners",
   "clearlyelevated": "clearlyelevated.github.io",
+  "clerc": "clerc.netlify.app",
   "clevercord": "theanidox.github.io/Clevercord",
   "clickforhelp": "clickforhelp.netlify.app",
   "clickhouse": "depyronick.github.io/clickhouse-client",
