f733d84352db1b37b1c6be73be3d54ca6e88d5d3
Pedro M. Campos P <95000247+PedritoHDR@users.noreply.github.com>
2023-07-21 22:40:05 -0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2247,6 +2247,7 @@ var cnames_active = {
   "portafoliorufino": "rufino-hdez.github.io/portafolio",
   "portainer-update": "flweber.github.io/portainer-stack-updater",
   "porter": "ahopkins.github.io/porterjs", // noCF? (don´t add this in a new PR)
+  "portfoliopedro": "https://pedritohdr.github.io/portfolio", 
   "poru": "brblacky.github.io/poru",
   "post-type-discovery": "twozeroone.github.io/post-type-discovery",
   "postlit": "postlit.github.io/postlit.js-site",
