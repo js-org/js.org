fd49e43560081d815393efd6e34b0194237eb951
Yoavv <48871059+afetera@users.noreply.github.com>
2023-07-23 11:39:55 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -101,6 +101,7 @@ var cnames_active = {
   "advancedrpc": "advancedrpc.github.io",
   "aeon": "flazepe.github.io/aeon-web",
   "aerogel": "noeldemartin.github.io/aerogel",
+  "afetera": "afetera.github.io",
   "affiliate": "russellsteadman.github.io/affiliate",
   "afilters": "dmytrohoi.github.io/afilters.js",
   "afmsavage": "afmsavage.github.io",
