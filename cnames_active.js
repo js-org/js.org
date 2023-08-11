9530a0a88ba2fce345dbe8edf4ddafba23db36bf
wgumenyuk <wlad@gumenyuk.de>
2023-08-11 12:13:35 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2797,6 +2797,7 @@ var cnames_active = {
   "spritewerk": "bildepunkt.github.io/spritewerk", // noCF? (don´t add this in a new PR)
   "spud": "nikop100.github.io/spud",
   "spx": "spxjs.netlify.app",
+  "spyter": "cname.vercel-dns.com", // noCF
   "sql": "sql-js.github.io/sql.js",
   "sql2struct": "ymlair.github.io/sql2struct",
   "sqlchart": "sqlchart.github.io/sqlchart",
