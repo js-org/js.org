f5923355761fb36054cee295aac97f985daeebbf
Nimesh Official <110774656+nimesh-official@users.noreply.github.com>
2023-08-22 10:11:54 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3390,7 +3390,8 @@ var cnames_active = {
   "zustand-cn": "cname.vercel-dns.com", // noCF
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "nimesh-official": "nimesh-official.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
