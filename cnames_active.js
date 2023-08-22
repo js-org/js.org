d6dbd77a9e8692cc67cd8fab9cba3b7f32253dc3
EMPstone <123616627+EMPstone@users.noreply.github.com>
2023-08-22 10:08:16 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3391,6 +3391,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes"
+  "zyyez": "https://empstone.github.io/"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
