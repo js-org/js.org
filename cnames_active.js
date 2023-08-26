b45a53659b84cc7dd258643a3cd7bdd4ee0c34ef
EveryMao <MaoMaoEvery9527@outlook.com>
2023-08-26 16:36:45 +0800

    go2resume

--- cnames_active.js
+++ cnames_active.js
@@ -3391,7 +3391,8 @@ var cnames_active = {
   "zustand-cn": "cname.vercel-dns.com", // noCF
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "go2resume": "go2resume.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
