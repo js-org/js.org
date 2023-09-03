a1533ef969068e750ecc97c79ee0f32c67dbe198
Yevhenii Hyzyla <hyzyla@gmail.com>
2023-09-03 18:03:01 +0200

    pdfium.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2172,6 +2172,7 @@ var cnames_active = {
   "pcl": "cname.vercel-dns.com", // noCF
   "pdf": "iamcristye.github.io/PDF",
   "pdf-lib": "hopding.github.io/pdf-lib", // noCF
+  "pdfium": "cname.vercel-dns.com", // noCF
   "pdfutils": "gottox.github.io/node-pdfutils", // noCF? (don´t add this in a new PR)
   "pdp-elements": "go4cas.github.io/pdp_elements", // noCF? (don´t add this in a new PR)
   "pecis": "techpot-studio.github.io/Pecis",
