7e36a148618ba7bc1aa184e93cc5de312f7a3bf4
Ron Dekker <RedKenrok@users.noreply.github.com>
2023-09-16 16:00:40 +0200

    Fixed missing comma

--- cnames_active.js
+++ cnames_active.js
@@ -1302,7 +1302,7 @@ var cnames_active = {
   "hltvbot": "nescabir.github.io/HLTVBot",
   "hmsscioly": "scioly.netlify.app",
   "hoa": "thehoa.github.io",
-  "hoast": "hoast.github.io"
+  "hoast": "hoast.github.io",
   "hois": "hoisw.github.io",
   "holidaycss": "evgenyorekhov.github.io/holiday.css",
   "hooked-on-redux": "harryhope.github.io/hooked-on-redux",
