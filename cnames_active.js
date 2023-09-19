e5e7d01e19f84adee2fdc3bfbea2cf64dbfe1982
Sitoi <sitoi@sjtu.edu.cn>
2023-09-19 09:02:41 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2739,6 +2739,7 @@ var cnames_active = {
   "sirkit": "seckwei.github.io/SirKit_CircuitSim", // noCF? (don´t add this in a new PR)
   "sistersbio": "kdiacodes.github.io/SistersBio",
   "sizle": "christoga.github.io/sizle", // noCF? (don´t add this in a new PR)
+  "sitoi": "sitoi.github.io", 
   "sjy": "cname.vercel-dns.com", // noCF
   "skeld": "skeldjs.github.io/SkeldJS",
   "skeptical-coder": "rbpinheiro.github.io/skeptical-coder",
