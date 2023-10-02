3d998560c4422222483703b70e96769f4ba8eb19
Stefan Keim <indus@users.noreply.github.com>
2023-10-02 19:27:25 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1196,8 +1196,7 @@ var cnames_active = {
   "gloria": "gloriajs.github.io",
   "glottologist": "arguiot.github.io/Glottologist",
   "glyph": "zyrouge.github.io/glyph.css",
-  "gnatale": "giosooul.github.io/gnatale.github.io",
-  "go": "cname.vercel-dns.com", // noCF
+  "gnatale": "giosooul.github.io/gnatale.github.io"
   "god": "godow.github.io",
   "goji": "airbnb.github.io/goji-js",
   "gol": "goljs.github.io/GoL",
@@ -1793,6 +1792,7 @@ var cnames_active = {
   "mc": "magicarbon.github.io/mc", // noCF? (don´t add this in a new PR)
   "mca": "lem0nb.github.io/Minecraft_Auto_Builder",
   "mcd": "n9gc.github.io/mcdjs",
+  "mctrl": "cname.vercel-dns.com", // noCF,
   "mcul": "russellsteadman.github.io/mcul",
   "md": "cname-china.vercel-dns.com", // noCF
   "md5": "cname-china.vercel-dns.com", // noCF
