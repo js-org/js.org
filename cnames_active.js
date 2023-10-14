7262d1fc7111e92ef01f2046c8233c958e576855
Alderson Chiu <s1311132009@nutc.edu.tw>
2023-10-15 00:10:42 +0800

    Change to img.js.me

--- cnames_active.js
+++ cnames_active.js
@@ -1370,7 +1370,7 @@ var cnames_active = {
   "iiimix": "iiimix.github.io/iiimixjsorg",
   "iio": "iioinc.github.io/iio.js", // noCF? (don´t add this in a new PR)
   "illustrator": "illustratorjs.netlify.app",
-  "image": "spacemiqote.github.io/image",
+  "img": "spacemiqote.github.io/img",
   "imagecapture": "googlechrome.github.io/imagecapture-polyfill",
   "imagine": "karimayachi.github.io/imagine",
   "imap-mailbox": "vsdparibas.github.io/imap-mailbox-doc",
