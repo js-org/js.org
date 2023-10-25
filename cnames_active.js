f885fcbd5a2cdae6013a27a3547a5897a146a2fb
Saurabh Kirve <saurabhkirve@gmail.com>
2023-10-25 17:21:40 +0530

    Update cnames_active.js
     I have read and accepted the Terms and Conditions
     There is reasonable content on the page: [https://portfolio-pro2.vercel.app/]

--- cnames_active.js
+++ cnames_active.js
@@ -2636,6 +2636,7 @@ var cnames_active = {
   "saso": "huruji.github.io/saso",
   "sass": "medialize.github.io/playground.sass.js", // noCF? (don´t add this in a new PR)
   "satori": "satorijs.github.io",
+  "saurabh": "portfolio-pro2.vercel.app"
   "saulosantiago": "saulosilva.github.io", // noCF? (don´t add this in a new PR)
   "sawyer": "sqwwy.github.io/sawyer.js.org",
   "sazerac": "sazeracjs.github.io/sazerac-site",
