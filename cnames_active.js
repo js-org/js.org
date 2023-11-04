aca465d7e0c744cb413b881b6023d586610e6a27
Cyril Wong <25571631+for05@users.noreply.github.com>
2023-11-04 20:44:04 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3446,6 +3446,7 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "cyril": "for05.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
