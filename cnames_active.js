fcf1fc62be0e3c3c6562097f7df6964fb25e43cd
Marcus S. Abildskov <8391194+marcus-sa@users.noreply.github.com>
2023-11-11 23:25:16 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -723,6 +723,7 @@ var cnames_active = {
   "deep-mandelbrot": "munsocket.github.io/deep-mandelbrot",
   "deepcodes": "deepduggal.github.io/deepcodes",
   "deepintr": "deepintr.github.io",
+  "deepkit-graphql": "deepkit-graphql.github.io/docs",
   "deepu": "deepu105.github.io",
   "defi": "finom.github.io/defi.js.org",
   "delegacias-fortaleza": "juliosampaio.github.io/delegacias-fortaleza", // noCF? (don´t add this in a new PR)
