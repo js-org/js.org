cbde47f684db87190c0af28779a9b1fd0e801c23
William Harrison <william@williamdavidharrison.com.au>
2023-11-18 10:17:57 +0800

    feat(cnames_active): add `william.js.org`

--- cnames_active.js
+++ cnames_active.js
@@ -3356,6 +3356,7 @@ var cnames_active = {
   "wiki": "wiki-js-org.netlify.app", // noCF
   "wikidata-elements": "lisongx.github.io/wikidata-elements",
   "wildfire": "cheng-kang.github.io/wildfire",
+  "william": "wdhdev.github.io/william.js", // noCF
   "willy": "willyarisky.github.io/willy",
   "windows": "munchkinhalfling.github.io/wasm-windows",
   "wiot": "iotcat.github.io/wIoT",
