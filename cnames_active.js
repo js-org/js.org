ab219c9d1459c5dfc5e892cd52a146595d999192
Nouman Qamar <84765475+noumanqamar450@users.noreply.github.com>
2023-11-25 16:01:25 +0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1270,6 +1270,7 @@ var cnames_active = {
   "gyps": "huijari.github.io/Gyps",
   "gyre": "wridder.github.io/GyreJS", // noCF? (don´t add this in a new PR)
   "gyx": "yourtion.github.io/gyx.js.org",
+  "glass-alert-box": "noumanqamar450.github.io/glass-alert",
   "h": "makenowjust.github.io/h.js",
   "h3": "h3rald.github.io/h3",
   "h7ml": "h7ml.github.io/web",
