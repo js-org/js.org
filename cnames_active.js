0b1c8cdd92b27f58a69459e8868717ee3572be18
Nouman Qamar <84765475+noumanqamar450@users.noreply.github.com>
2023-11-25 15:55:46 +0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3463,7 +3463,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  'glass-alert-box': "noumanqamar450.github.io/glass-alert"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
