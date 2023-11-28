6bfb5683c8269c71b67c8d3cdc8356b3c7c21590
Rohit Chouhan <34239087+rohit-chouhan@users.noreply.github.com>
2023-11-28 19:53:37 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3468,7 +3468,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "sacgencode": "sacgencode.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
