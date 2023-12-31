f100b3d536779b2d2797e9e846aee61b318fdde7
shrstudio <149881542+shrstudio@users.noreply.github.com>
2023-12-31 09:07:28 +0800

    cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2998,6 +2998,7 @@ var cnames_active = {
   "synergies": "lukasbach.github.io/synergies",
   "synth": "lukehorvat.github.io/synth-mood",
   "syr": "dmikey.github.io/syr",
+  "shrstudio": "shrstudio.github.io",
   "t": "l-js.mydevapp.tfuture.eu.org", // noCF
   "tab": "gdmcc.github.io/tab",
   "tagscript": "cname.vercel-dns.com", // noCF
