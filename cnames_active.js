2504d07071d2581fe18add4539c863ac9928c743
Rafael Bardini <rafael@rbardini.com>
2024-01-10 23:35:25 -0300

    storybook-addon-playroom.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2937,7 +2937,6 @@ var cnames_active = {
   "storage": "uzitech.github.io/storage.js",
   "storelocator": "storelocatorjs.github.io",
   "storybook": "storybooks.netlify.app", // noCF
-  "storybook-addon-playroom": "storybook-addon-playroom.netlify.app",
   "storybooks": "storybooks.github.io",
   "str": "manelet.github.io/str",
   "stratic": "straticjs.github.io",
