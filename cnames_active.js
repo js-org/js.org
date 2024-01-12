d8903e705c7537582a545f8a4ec32a5bb7363438
M1 <140819896+m1-dev@users.noreply.github.com>
2024-01-12 14:53:31 +0200

    Add noCF

--- cnames_active.js
+++ cnames_active.js
@@ -3262,7 +3262,7 @@ var cnames_active = {
   "velt": "veltjs.github.io",
   "vendywira": "vendywira.github.io",
   "vento": "oscarotero.github.io/vento",
-  "vercel": "cname.vercel-dns.com",
+  "vercel": "cname.vercel-dns.com", // noCF
   "verifyr": "arze1.github.io/verifyr-site",
   "verse": "druidic.github.io/verse",
   "vesselize": "vesselize.github.io/docs",
