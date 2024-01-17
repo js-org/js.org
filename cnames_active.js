ca3e6a662e723d95aa9bde23cb3ac565e829dadf
Kadir <k4dircelikk@gmail.com>
2024-01-17 19:03:42 +0300

    ncx.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2041,7 +2041,7 @@ var cnames_active = {
   "naveenjujaray": "naveenjujaray.github.io/blog",
   "nbabot": "eliotchignell.github.io/NBABot",
   "ncub8": "ncub8.github.io", // noCF? (don´t add this in a new PR)
-  "ncx": "kad1rr.github.io/ncx",
+  "ncx": "cname.vercel-dns.com", // noCF
   "neaps": "neaps.github.io",
   "nearley": "kach.github.io/nearley",
   "nectar": "cname.vercel-dns.com", // noCF
