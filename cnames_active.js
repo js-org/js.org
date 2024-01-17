8abcaf4b5121f6277122372495d8d8f96f0f1088
Daniil Sitdikov <hello.boriskas@gmail.com>
2024-01-17 11:27:09 +0100

    add node-file-router

--- cnames_active.js
+++ cnames_active.js
@@ -2098,6 +2098,7 @@ var cnames_active = {
   "nod": "diegohaz.github.io/nod",
   "node": "nodejs.github.io/node.js.org",
   "node-atlas": "haeresis.github.io/NodeAtlas",
+  "node-file-router": "danilqa.github.io/node-file-router",
   "node-slate": "center-key.github.io/node-slate",
   "node-vimeo": "sidisliveyt.github.io/node-vimeo",
   "nodebook": "hosting.gitbook.io",
