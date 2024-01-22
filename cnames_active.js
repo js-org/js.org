8a9265d995c947986b347a50a4a3deca0c87f278
CHILL <99878812+CHILLBRO28@users.noreply.github.com>
2024-01-22 17:56:15 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3503,7 +3503,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "chillcode": "https://chillbro28.github.io/chillcode/",
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
