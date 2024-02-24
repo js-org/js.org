fe8d980476226f2833714c19cad7995313b428b4
xXMomazosNiz_69Xx <76523353+galleniz@users.noreply.github.com>
2024-02-23 21:09:33 -0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1853,7 +1853,7 @@ var cnames_active = {
   "md": "cname-china.vercel-dns.com", // noCF
   "md5": "cname-china.vercel-dns.com", // noCF
   "mdbf-css": "more-beautiful-div-framework.github.io",
-  "mdcdev": "galleniz.github.io/mdcdev.js/",
+  "mdcdev": "galleniz.github.io/mdcdev.js",
   "mde": "lukehorvat.github.io/mde-soundboard",
   "mechan": "dusterthefirst.github.io/mechan.js",
   "medan": "medan-js.github.io", // noCF
