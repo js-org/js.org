91921927bf53faa2f7ddb6a6376c5f7d5e7eab86
Julian Cataldo <contact@juliancataldo.com>
2024-03-17 06:39:26 +0100

    Add `gracile.netlify.app`

--- cnames_active.js
+++ cnames_active.js
@@ -1248,6 +1248,7 @@ var cnames_active = {
   "gotanda": "gotandajs.github.io",
   "govind": "emgk.github.io",
   "gr": "cname.vercel-dns.com", // noCF
+  "gracile": "gracile.netlify.app",
   "gram": "gram-js.github.io/docs",
   "gramps": "gramps-graphql.github.io/gramps",
   "graph": "tomas-wrobel.github.io/graph",
