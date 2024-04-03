c0530ab166e030c8b920845a411c438a5861a981
ouatu-ro <ouatubogdanioan@gmail.com>
2024-04-03 05:16:57 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2356,6 +2356,7 @@ var cnames_active = {
   "polygon-clipping": "mfogel.github.io/polygon-clipping",
   "polythene": "arthurclemens.github.io/Polythene", // noCF? (don´t add this in a new PR)
   "pomidorus": "tatyshev.github.io/pomidorus",
+  "pong": "ouatu-ro.github.io/pong",
   "pong-zh": "xiaozhu2021.github.io/javascript-pong",
   "popcorn-api": "nirewen.github.io/popcorn-api",
   "popper": "fezvrasta.github.io/popper.js",
