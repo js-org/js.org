7c8215b6cb1e48bb22ce0bf52fedb0c7db257b5b
futur <me@futuristick.ca>
2024-04-04 17:06:51 -0400

    Add skyware

--- cnames_active.js
+++ cnames_active.js
@@ -2852,6 +2852,7 @@ var cnames_active = {
   "sksamim": "esksamim.github.io/sksamim",
   "skyle": "skylejs.github.io/skyle",
   "skyscript": "cname.vercel-dns.com", // noCF
+  "skyware": "skyware-js.github.io/web",
   "slang": "ramadis.github.io/slang", // noCF? (don´t add this in a new PR)
   "slash-create": "snazzah.github.io/slash-create-website",
   "slate": "iamshouvikmitra.github.io/slate",
