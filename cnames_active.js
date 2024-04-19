c15e4968ea8fe8623b7f198f17f8130a19409905
e1399579 <e1399579@gmail.com>
2024-04-19 15:51:28 +0800

    Update cnames_active.js
    according to https://e1399579.github.io/chat/

--- cnames_active.js
+++ cnames_active.js
@@ -3554,7 +3554,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "chat": "e1399579.github.io/chat/"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
