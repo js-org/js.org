8743a586316727f32d47605c34493c4157d193dc
Dmitrii Vasilev <dimkasv1@gmail.com>
2024-04-24 18:45:48 +0300

    newton.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2086,6 +2086,7 @@ var cnames_active = {
   "newbyte": "newbytee.github.io/newbyte",
   "newcar": "newcar-docs.netlify.app",
   "newspool": "68b0dfd0-2b8d-4335-9da2-5653f09d2d34.id.repl.co", // noCF
+  "newton": "caracal7.github.io/newton.js", 
   "newyear": "dndrbots.github.io/new_year_counter",
   "next": "zeit.github.io/next-site",
   "next-admin": "cname.vercel-dns.com", // noCF
