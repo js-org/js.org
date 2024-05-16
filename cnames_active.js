4c757c375f06717571925cd6ecc4605d7a62340d
only9464 <115201218+only9464@users.noreply.github.com>
2024-05-16 21:40:03 +0800

    Update cnames_active.js
    ADD   "sky": "only9464.github.io/front"

--- cnames_active.js
+++ cnames_active.js
@@ -3573,6 +3573,7 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "sky": "only9464.github.io/front"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
