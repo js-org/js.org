3209dd52e8575330c8098f1fe16f10de3b306cdd
selvaklnc <nkselvakumaran@gmail.com>
2024-05-16 23:08:55 +0530

    Adding a new subdomain

--- cnames_active.js
+++ cnames_active.js
@@ -2442,6 +2442,7 @@ var cnames_active = {
   "pwa-cookbook": "sylvainpolletvillard.github.io/pwa-cookbook",
   "pwa-workshop": "sylvainpolletvillard.github.io/pwa-workshop",
   "py-like": "ruben-arushanyan.github.io/py-like",
+  "projects": "wikimint.github.io/jsprojects",
   "qbit": "fudan-mse.github.io/qbit",
   "qbsz": "qbsz.github.io",
   "qs": "kirjs.github.io/qs.js", // noCF? (don´t add this in a new PR)
