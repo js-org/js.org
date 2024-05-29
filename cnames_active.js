68dc58eee4433703e01b754129726ae07a5543f0
做个懒人 <admin@mcpal.cn>
2024-05-29 15:36:39 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3576,7 +3576,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "xiaochen": "blog.xiao2024.cn"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
