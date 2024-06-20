f87c28f36c34bf8d4bc9b1e776abbe597ebb3f1a
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-06-20 13:25:36 -0300

    add order ai

--- cnames_active.js
+++ cnames_active.js
@@ -39,6 +39,7 @@
 
 var cnames_active = {
   "": "js-org.github.io",
+  "order-ai": "gen1x.is-a.dev/OrderAI",
   "01mz": "01mz.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
