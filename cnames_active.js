f45901a0ae66c71d4a0df95ef96afc3afdc68bde
kxyzt <stxiz_1@126.com>
2024-06-26 10:10:39 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1666,6 +1666,7 @@ var cnames_active = {
   "kumeru": "ultirequiem.github.io/kumeru",
   "kunal": "kunalghosh02.github.io",
   "kunkun": "smallkunkun.github.io",
+  "kxyzt": "kxyzt.github.io"
   "kyoto": "kyotojs.github.io",
   "l2dwidget": "xiazeyu.github.io/live2d-widget.js-doc",
   "lab": "labjs.netlify.app",
