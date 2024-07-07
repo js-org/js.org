0dfec8b12f1213420451da44531aae4588e910ba
lisuid2 <145828868+lisuid2@users.noreply.github.com>
2024-07-07 14:58:11 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3590,7 +3590,7 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
-  "lisuid": "lisuid2.github.io",
+  "lisui": "lisuid2.github.io",
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
