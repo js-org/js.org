4f26a3a9429941b0f00203428c0cfb174462d096
jun3 <337268759@qq.com>
2024-07-20 19:11:40 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3592,7 +3592,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "goinit": "go-initialization.github.io/wensite"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
