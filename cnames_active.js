febe8d0678930b508d559880513bd8b77dac7709
slatev1 <slatev1@outlook.com>
2024-08-05 20:00:37 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3092,6 +3092,7 @@ var cnames_active = {
   "synergies": "lukasbach.github.io/synergies",
   "synth": "lukehorvat.github.io/synth-mood",
   "syr": "dmikey.github.io/syr",
+  "slatev1": "slatev1.github.io
   "t": "l-js.mydevapp.tfuture.eu.org", // noCF
   "tab": "gdmcc.github.io/tab",
   "tagscript": "cname.vercel-dns.com", // noCF
