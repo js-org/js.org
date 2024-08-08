7c113762b6eb9decaa58006844670b4641bee57f
Free to Maid <153776417+freetomaid@users.noreply.github.com>
2024-08-08 13:18:10 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1671,6 +1671,7 @@ var cnames_active = {
   "kumeru": "ultirequiem.github.io/kumeru",
   "kunal": "kunalghosh02.github.io",
   "kunkun": "smallkunkun.github.io",
+  "kuyshare": "freetomaid.github.io",
   "kyoto": "kyotojs.github.io",
   "l2dwidget": "xiazeyu.github.io/live2d-widget.js-doc",
   "lab": "labjs.netlify.app",
