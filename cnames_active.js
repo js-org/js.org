b5ff3fe02efe8c3f1c34a75827f3a3e75742063a
Mohammed Kareem <alrasamking123@gmail.com>
2024-09-04 21:35:12 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1363,6 +1363,7 @@ var cnames_active = {
   "heroku": "gary50613.github.io/herokuapi.js",
   "hexoplusplus": "hexoplusplus.github.io/docs",
   "hfsyun": "hfsyun.github.io",
+  "hfyc": "mohammedkmo.github.io",
   "hibiki": "claritymoe.github.io/hibiki",
   "hiddenfrombots": "cname.vercel-dns.com", // noCF
   "highfive": "hosting.gitbook.io", // noCF
