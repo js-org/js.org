154906ca7a614fc910c5777514771c8577bab9e4
Mrbzbz <bzbz233nb@163.com>
2024-09-15 22:14:32 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -442,6 +442,7 @@ var cnames_active = {
   "bynote": "bynotes.github.io",
   "byteasier": "kagurain.github.io/ByteasierIndex",
   "bytemd": "bytemd.netlify.app",
+  "bzbz":"bzbzblog.pages.dev",
   "c": "cocobear.github.io",
   "c-3po": "ttag-org.github.io/c-3po",
   "c-installer": "llenax.github.io/c-installer",
