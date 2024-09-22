d351919b25dc6c48fb276f7820a2f98d319875d1
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2024-09-22 13:00:17 +0200

    add shrimple.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2860,6 +2860,7 @@ var cnames_active = {
   "shortener": "jsmiith.github.io/shortener",
   "shortio": "ichiidev.github.io/short.io-docs",
   "shuwan9": "shuwan9.github.io",
+  "shrimple": "shrimplejs.github.io",
   "siddharth": "chaudhs769.github.io/siddharth.js.org",
   "sidekick": "cname.vercel-dns.com", // noCF
   "sidekik": "inf3cti0n95.github.io/sidekik",
