a3d56594102219c7f3c943fde8697fb08a18318c
李臻烁 <admin@nnbwl.com>
2024-09-25 00:07:41 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3633,6 +3633,7 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "nnbw": "nnbwchenn.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
