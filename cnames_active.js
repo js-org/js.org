3ea277694f90b7ad924d51b8f1e6d751e18e0e1f
Abhishek Gupta <112748178+imabhishek476@users.noreply.github.com>
2024-10-01 15:39:19 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -74,6 +74,7 @@ var cnames_active = {
   "abelacostaportafolio": "asaa19971a.github.io/AbelAcostaAcostaPortafolio",
   "abhishek": "rpidanny.github.io/personal-collage",
   "abhishekmittal": "abhishek-mittal.github.io/abhishekmittal",
+  "abhishekgupta": "imabhishek476.github.io/abhishekgupta"
   "abi": "abi-js.github.io",
   "abialbon": "abialbon.github.io",
   "aby": "icoldplayer.github.io/aby",
