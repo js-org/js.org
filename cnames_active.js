a4b5939c8ac07b55da540ffbe12da822fc9232ed
JhonyRamos <119146160+JhonyRamos@users.noreply.github.com>
2024-10-03 13:14:50 -0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2412,7 +2412,7 @@ var cnames_active = {
   "popcorn-api": "nirewen.github.io/popcorn-api",
   "popper": "fezvrasta.github.io/popper.js",
   "popup": "simondmc.github.io/popup-site",
-  "portafolio-ramos": "jhonyramos.github.io/portafolio-ramos"
+  "portafolio-ramos": "jhonyramos.github.io/portafolio-ramos",
   "portafolio-web": "velazquezadrian.github.io/Portafolio-web",
   "portafolioalejandrarobles": "alerobles3.github.io/portafolioalejandrarobles",
   "portafolioarnulfo": "arpior.github.io/Portafolio",
