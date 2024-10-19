eef91aba45404423be17d438e279177d478aaffc
TheNamelessTree <73066748+thenamelesstree@users.noreply.github.com>
2024-10-19 16:48:04 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -109,6 +109,7 @@ var cnames_active = {
   "affiliate": "russellsteadman.github.io/affiliate",
   "afilters": "dmytrohoi.github.io/afilters.js",
   "afmsavage": "afmsavage.github.io",
+  "africanwealth": "thenamelesstree.github.io/africanwealth",
   "afterwind": "eedrxs.github.io/afterwind",
   "agentnpm": "meetping.github.io/agentnpm",
   "aghasemi": "aghasemi.github.io",
@@ -2068,7 +2069,6 @@ var cnames_active = {
   "mujin": "mujin669.github.io",
   "multiple": "nexts.github.io/Multiple.js",
   "multiversus": "cname.vercel-dns.com", // noCF
-  "munhumutapaalpha": "munhumutapaalpha.github.io/wallet",
   "murajaah": "mzaini30.github.io/murajaah-js",
   "murathalitsever": "murathalitsever.github.io",
   "murmansk": "interfluve.github.io/Murmansk",
