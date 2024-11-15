5686461b52df1ddc07e59636b5febbcc44ab7ac5
LazyTong <lazytong-tech@ik.me>
2024-11-16 02:30:24 +0800

    fix

--- cnames_active.js
+++ cnames_active.js
@@ -201,8 +201,8 @@ var cnames_active = {
   "aoitr": "dexaroffical.github.io/aoi.js-tr",
   "aom": "scarych.github.io/aom",
   "aomex": "aomex.github.io",
-  "apicluster": "ramsunvtech.github.io/apicluster", // noCF? (don´t add this in a new PR)
   "api.lt": "cname.vercel-dns.com", // noCF
+  "apicluster": "ramsunvtech.github.io/apicluster", // noCF? (don´t add this in a new PR)
   "apilyertia": "apilyertia.github.io",
   "aping": "johnnythetank.github.io/apiNG", // noCF? (don´t add this in a new PR)
   "aplayer": "diygod.github.io/APlayer",
