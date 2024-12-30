7f702b3796c4436c55c3c005786f7b370209dc2b
eyallior <eyallior@eyallior.com>
2024-12-30 13:18:36 +0200

    Update cnames_active.js
    Co-authored-by: Matt Cowley <me@mattcowley.co.uk>

--- cnames_active.js
+++ cnames_active.js
@@ -3418,7 +3418,7 @@ var cnames_active = {
   "vanilla": "haeresis.github.io/vanilla-js-dom",
   "vanilla-picker": "sphinxxxx.github.io/vanilla-picker",
   "vanilla-swr": "droyson.github.io/vanilla-swr",
-  "vanillaboot": "github.com/eyallior/vanillaboot",
+  "vanillaboot": "eyallior.github.io/vanillaboot",
   "vanillarouter": "vanillarouter.github.io",
   "vansul": "vansul.github.io",
   "vant-react-native": "youngjuning.github.io/vant-react-native",
