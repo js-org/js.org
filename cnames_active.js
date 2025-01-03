9a861e8761eda84ca93684204900693cdae57ab5
Ayesha Mehnaaz <39873704+Abbaskhurram255@users.noreply.github.com>
2025-01-03 14:51:01 +0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -38,7 +38,7 @@
  */
 
 var cnames_active = {
-  "": "js-org.github.io",
+  "photography-site.js.org": "abbaskhurram255.github.io/site-2.github.io",
   "01mz": "01mz.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
