44b26fa2226e8f39f2410fadde6bca23ffd2f5f6
xiaolin930 <3775610318@qq.com>
2025-01-19 10:00:00 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3696,7 +3696,7 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
-  "xiaolin": "xiaorin.com"
+  "xiaolin": "xiaolin930.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
