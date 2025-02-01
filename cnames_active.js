89a3b6c944571850877d6972ab939ef628d5b27d
planet-free <anton798@inbox.lv>
2025-02-01 23:57:01 +0400

    Add files via upload

--- cnames_active.js
+++ cnames_active.js
@@ -3702,7 +3702,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "planet-free": "planet-free.github.io/planet-free"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
