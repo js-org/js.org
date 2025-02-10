dbe2eb88401dab0a25957a27808a82e2043ddf69
InI Versity <iniverzity@gmail.com>
2025-02-10 16:58:55 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1495,6 +1495,7 @@ var cnames_active = {
   "infoooze": "devxprite.github.io/infoooze",
   "inig": "inig.github.io",
   "initial-wu": "initial-wu.github.io",
+  "iniversity": "iniversity.js.org",
   "injectify": "injectify.github.io/docs",
   "ink": "colossalchicken.github.io/ink.js",
   "inklog": "lucaslah.github.io/inklog.js",
