e604d2c75797c124668c70e31f9e75e6a3ffd058
冰梦 <68908911+icemyst@users.noreply.github.com>
2025-02-13 15:31:29 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3710,6 +3710,7 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "icemyst": "icemyst.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
