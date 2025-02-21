c3e018fc1fe4cd3f6bbe55561b54152dd1d1b2b0
Sera Programmer <73139993+seraprogrammer@users.noreply.github.com>
2025-02-21 19:33:36 +0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -586,12 +586,12 @@ var cnames_active = {
   "cocy": "krmax44.github.io/cocy",
   "code-tour": "code-tour.netlify.app",
   "codebooky": "hiteshsubnani0128.github.io/codebooky",
+  "codekori" "name.vercel-dns.com",
   "codemade": "codemade.github.io", // noCF? (don´t add this in a new PR)
   "codemirror-languageservice": "codemirror-languageservice.netlify.app",
   "codepan": "egoist.github.io/codepan",
   "codeshare": "cname.vercel-dns.com", // noCF
   "codewing": "nicesapien.github.io/codewing",
-  "codekori" "name.vercel-dns.com",
   "codeium" : "cname.vercel-dns.com", // noCF
   "codinsky": "izhaki.github.io/codinsky",
   "coffea": "caffeinery.github.io/coffea",
