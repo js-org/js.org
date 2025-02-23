9bccc791cb5b957f366b4659ceada35f67876613
OKSHIJIAN <71979147+OKSHIJIAN@users.noreply.github.com>
2025-02-23 20:12:52 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3719,6 +3719,7 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "sj": "okshijian.github.io/sj"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
