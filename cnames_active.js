ad96b800168806ea58dcf74b5fc552485f15c398
Matt Cowley <me@mattcowley.co.uk>
2025-03-01 15:26:55 +0000

    Alphabetical

--- cnames_active.js
+++ cnames_active.js
@@ -3338,9 +3338,9 @@ var cnames_active = {
   "tui-nuxt": "hosting.gitbook.com", // noCF
   "tulsa": "thatguysam.github.io/tulsa.js",
   "turali": "caglarturali.github.io",
-  "tusk": "cname.vercel-dns.com", // noCF
   "turbo": "turbo.github.io",
   "turboprisma": "cname.vercel-dns.com", // noCF
+  "tusk": "cname.vercel-dns.com", // noCF
   "tw-themes": "kevinast.github.io/tw-themes",
   "twallpaper": "crashmax-dev.github.io/twallpaper",
   "twcopy": "the94air.github.io/twcopy",
