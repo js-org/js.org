a0db50a4717830c7669296a4a3f2670ce06ba149
ZS <zsorg34@gmail.com>
2025-03-17 12:52:35 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3713,6 +3713,7 @@ var cnames_active = {
   "zen": "tomelam.github.io/zen",
   "zenfuse": "zenfuse.github.io/zenfuse.js",
   "zephyr": "zephyr-dh.github.io/zephyr",
+  "zayn": "zimaamzaynweb.github.io",
   "zero-dependency": "zero-dependency.github.io/zero-dependency",
   "zeroclick": "studiomotio.github.io/zeroclick",
   "zeroframe": "filips123.github.io/ZeroFrameJS",
