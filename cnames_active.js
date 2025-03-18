894e546a3c3ab1f4d7ce97a5b44db7159e03ba56
Mcenahle <admin@mcenahle.sh.cn>
2025-03-18 13:47:34 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3733,7 +3733,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "tdli": "mcenahle.github.io/tdli---to-do-list"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
