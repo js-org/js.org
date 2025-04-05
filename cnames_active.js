33cc457698f9b1cface15d2c6edb52844ca0a992
xam <awesomematylda@gmail.com>
2025-04-05 14:52:48 -0400

    Update cnames_active.js
    Co-authored-by: Matt Cowley <me@mattcowley.co.uk>

--- cnames_active.js
+++ cnames_active.js
@@ -3259,7 +3259,9 @@ var cnames_active = {
   "tgsnake": "cname.vercel-dns.com", // noCF
   "tgx-core": "tgx-core.github.io/website",
   "thea": "idokutela.github.io/thea",
+  "thegrid": "antoniandre.github.io/grid",
   "thema": "themakit.github.io",
+  "theme-next": "hexo-theme-next.netlify.app",
   "themoviedb": "chrisenytc.github.io/themoviedb", // noCF? (don´t add this in a new PR)
   "thenandnow": "devdavmen.github.io/then-and-now",
   "theorem": "arguiot.github.io/TheoremJS",
