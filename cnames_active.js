87c1c0306994feb2c29aae64639be00e2757d3fe
YrUqx <18072262+thianda91@users.noreply.github.com>
2025-04-09 14:33:47 +0800

    add thianda to  cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3268,6 +3268,7 @@ var cnames_active = {
   "theorem": "arguiot.github.io/TheoremJS",
   "thepeachtimes": "thepeachtimes.github.io",
   "thi": "ngomjnhthj.github.io/thi",
+  "thianda": "thianda91.github.io",
   "thinkdigital": "tomowens08.github.io/thinkdigital",
   "threadly": "spideyzac.github.io/Threadly",
   "threads": "andywer.github.io/threads.js",
