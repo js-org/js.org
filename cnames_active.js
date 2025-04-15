1bc1f84ef971138983d18e111ed88795f438862a
izum00 <karisute14@gmail.com>
2025-04-16 06:31:05 +0900

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -972,6 +972,7 @@ var cnames_active = {
   "dynamotion": "cname.vercel-dns.com", // noCF`
   "dyo": "dyo.github.io/dyo",
   "dyte": "dytejs.github.io",
+  "e4f": "izum00.github.io/e4f",
   "e-query": "mjkhonline.github.io/e-query",
   "eagleeye": "webkrafters.github.io/eagleeye",
   "eahmed234": "eahmed234.github.io",
