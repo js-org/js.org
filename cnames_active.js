48c85b94183cafc1c10041a865567af0c9219a83
worlarusham <120298722+worlarusham@users.noreply.github.com>
2025-04-16 02:36:38 +0530

    Update cnames_active.js
    add worlar.js.org for worlarusham portfolio

--- cnames_active.js
+++ cnames_active.js
@@ -3645,6 +3645,7 @@ var cnames_active = {
   "wordywordy": "sverweij.github.io/wordywordy",
   "workers": "worker-tools.github.io/workers.js.org",
   "worksnake": "worksnake.github.io",
+  "worlar": "worlarusham.netlify.app",
   "wowufoundme": "wowufoundme.github.io",
   "wpwebpack": "oblakstudio.github.io/wpwebpack",
   "wq": "wqjs.alwaysdata.net",
