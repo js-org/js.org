7092c3d3a6f4dd0fac583ddd6d811086bb840a7e
Howe Hsiang <hosiang1026@sina.com>
2025-04-16 13:49:02 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3739,7 +3739,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "haoxiang": "hosiang1026.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
