f28e2769656d9d1bc56d302953c37d35bc09c26d
Hamza-Benfatmi <165222293+Hamza-Benfatmi@users.noreply.github.com>
2025-04-17 22:04:49 +0200

    Create web.js

---
+++ web.js
@@ -0,0 +1,9 @@
+// hamzabenfatmi.js.org
+module.exports = {
+  "hamzabenfatmi": {
+    "owner": "Hamza-Benfatmi",
+    "repo": "mon-site", // Remplace "ton-repo" par le nom de ton dépôt
+    "branch": "main",   // Si tu utilises une autre branche, change ici
+    "https": true
+  }
+};
