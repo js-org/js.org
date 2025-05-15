41a5289757157e625fb91e2d7a7ad6472c6ad2fe
闲耘™ <hotoo@users.noreply.github.com>
2025-05-15 19:19:19 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -705,8 +705,8 @@ var cnames_active = {
   "cryptons-api": "agenthackeryt.github.io/cryptons-api",
   "crystaldb": "erikmcm.github.io/CrystalDB",
   "crystalpay-sdk": "deadshumz.github.io/crystalpay-sdk",
-  "css3": "hotoo.github.io/css-handbook",
   "css-variable": "jantimon.github.io/css-variable",
+  "css3": "hotoo.github.io/css-handbook",
   "cssfun": "8tentaculos.github.io/cssfun",
   "cssx": "salteadorneo.github.io/cssx",
   "csur": "cname.vercel-dns.com", // noCF
