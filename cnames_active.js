e25a5c12f37117c3892c67746cdb5c984271c7fd
闲耘™ <hotoo@users.noreply.github.com>
2025-05-15 19:07:55 +0800

    Update cnames_active.js
    add css3.js.org to hotoo.github.io/css-handbook

--- cnames_active.js
+++ cnames_active.js
@@ -704,7 +704,7 @@ var cnames_active = {
   "cryptons-api": "agenthackeryt.github.io/cryptons-api",
   "crystaldb": "erikmcm.github.io/CrystalDB",
   "crystalpay-sdk": "deadshumz.github.io/crystalpay-sdk",
-  "css": "hotoo.github.io/css",
+  "css3": "hotoo.github.io/css-handbook",
   "css-variable": "jantimon.github.io/css-variable",
   "cssfun": "8tentaculos.github.io/cssfun",
   "cssx": "salteadorneo.github.io/cssx",
