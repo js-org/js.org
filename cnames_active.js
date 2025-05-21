ef89a0726c5fb62329ac935af7b666cd72a2749e
NanoTurtle1145 <guixiabaifo2022@outlook.com>
2025-05-22 06:32:42 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1445,7 +1445,7 @@ var cnames_active = {
   "hooked-on-redux": "harryhope.github.io/hooked-on-redux",
   "hookstate": "hookstate.netlify.app", // noCF
   "hooloo": "hooloo.github.io", // noCF? (don´t add this in a new PR)
-  "hopestudio": "hopestudio.github.io",
+  "hopestudio": "nanoturtle1145.github.io",
   "hoshii": "reinhello.github.io/hoshii-docs",
   "houp": "houpjs.github.io/houp-docs",
   "hours": "cname.vercel-dns.com", // noCF
