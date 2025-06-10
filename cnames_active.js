821da15970665e8ad13efa3509062030fbe02ceb
Kang Cahya <cahyadyazin@yahoo.com>
2025-06-10 09:08:36 +0700

    Update cnames_active.js: req subdomain kang-cahya.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -3771,6 +3771,7 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "kang-cahya": "dyazincahya.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
