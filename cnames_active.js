d6510710eba5f9b9b5fc67142b01003dd4fd80cd
Talha Wazolski <108323276+talhawt@users.noreply.github.com>
2025-06-20 01:02:30 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3249,6 +3249,7 @@ var cnames_active = {
   "taha": "orcxzjeeeee.github.io/taha",
   "tailwind-baseline": "apkoponen.github.io/tailwind-baseline-site",
   "takeout": "takeout-bysourfruit.github.io",
+  "talha": "talhawt.github.io",
   "talk": "zonayedpca.github.io/talk.js",
   "talker": "secondstreet.github.io/talker.js", // noCF? (don´t add this in a new PR)
   "tama": "etk.gitlab.io",
