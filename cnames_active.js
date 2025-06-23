4ac80086ce035c11d6d29a0ae7bf424bb3cf91dd
JounQin <admin@1stg.me>
2025-06-23 21:15:35 +0800

    unuse.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -3459,6 +3459,7 @@ var cnames_active = {
   "unoconv": "blog.ryanwu.me",
   "unreal": "sprayxe.github.io/unreal.js.org",
   "unsafe": "unsafely.github.io/unsafe.js",
+  "unuse": "un-ts.github.io/unuse",
   "up": "codefeathers.github.io/up",
   "updates.flightpkg": "cname.vercel-dns.com", // noCF
   "updevs-db": "up-devs.github.io/db-docs",
