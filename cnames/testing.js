f3723aaa001f8ec4017b2df5281abf987b2f5349
free12345678901 <free12345543210987@gmail.com>
2025-07-14 07:57:35 +0530

    Create testing.js

---
+++ cnames/testing.js
@@ -0,0 +1,10 @@
+{
+  "owner": {
+    "repo": "https://github.com/free12345678901/testing",
+    "email": "free12345543210987@gmail.com"
+  },
+  "record": {
+    "CNAME": "free12345678901.github.io"
+  },
+  "proxied": false
+}
