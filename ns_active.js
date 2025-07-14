e3606b36df7295fd9da6eeaa2fe742a4e439b7d8
Namhyeon, Go <gnh1201@gmail.com>
2025-07-14 09:52:41 +0900

    Update ns_active.js
    Add welson.js.org

--- ns_active.js
+++ ns_active.js
@@ -18,7 +18,8 @@ var ns_active = {
   "lolifamily": ["glen.ns.cloudflare.com", "melany.ns.cloudflare.com"],
   "mermaid": ["dns1.p04.nsone.net", "dns2.p04.nsone.net", "dns3.p04.nsone.net", "dns4.p04.nsone.net"],
   "ruby": ["alexis.ns.cloudflare.com", "connie.ns.cloudflare.com"],
-  "samplasion": ["ns31.cloudns.net", "ns32.cloudns.net", "ns33.cloudns.net", "ns34.cloudns.net"]
+  "samplasion": ["ns31.cloudns.net", "ns32.cloudns.net", "ns33.cloudns.net", "ns34.cloudns.net"],
+  "welson": ["graham.ns.cloudflare.com", "nadia.ns.cloudflare.com"]
 }
 
 /*
