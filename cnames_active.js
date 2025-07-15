ccf90e942f4256bb85e6a8a973b168606642d76a
UVEXZ <public@yjk.im>
2025-07-15 16:40:40 +0800

    zer0.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -3548,6 +3548,7 @@ var cnames_active = {
   "zed": "paul-browne.github.io/Zed-framework",
   "zen": "tomelam.github.io/zen",
   "zephyr": "zephyr-dh.github.io/zephyr",
+  "zer0": "cname.vercel-dns.com", // noCF
   "zero-dependency": "zero-dependency.github.io/zero-dependency",
   "zeroclick": "studiomotio.github.io/zeroclick",
   "zeroframe": "filips123.github.io/ZeroFrameJS",
