2fa29b2716b35e19638b06aec2719b23089089d2
Wh1teT1ger55 <125185383+Wh1teT1ger55@users.noreply.github.com>
2025-07-20 15:55:08 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1435,6 +1435,7 @@ var cnames_active = {
   "immense": "immense.github.io", // noCF? (don´t add this in a new PR)
   "immybox": "immense.github.io/immybox", // noCF? (don´t add this in a new PR)
   "impactminiblox": "progmem-cc.github.io/miniblox.impact.client.updatedv2",
+  "impostergame": "wh1tet1ger55.github.io/impostergame",
   "impress": "impress.github.io/impress.js",
   "imrc-datetime-picker": "smrsan76.github.io/imrc-datetime-picker",
   "imvu": "imvujs.pages.dev", // noCF
