172156a6353dbf7791c7585252923b297cb7360c
Azure Horizon Charters <j.gm13@hotmail.com>
2025-07-24 09:19:35 -0400

    Add azurehorizoncharters.js.org subdomain

--- cnames_active.js
+++ cnames_active.js
@@ -289,6 +289,7 @@ var cnames_active = {
   "aziz": "iaziz786.netlify.app",
   "azlyrics-ext": "zyrouge.github.io/azlyrics-ext",
   "azurapi": "azurapi.github.io/azurapi-js",
+  "azurehorizoncharters": "gonzaj418.github.io/Azure_Horizon_Charters",
   "bachors": "bachors.github.io",
   "backlog": "backlog-js.github.io/backlog.js.org", // noCF? (don´t add this in a new PR)
   "backstage": "backstagejs.github.io/backstage",
