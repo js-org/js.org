f8215779d8e73b04afaf891e7d73794ae42c03f8
千寻啊千寻 <960339491@qq.com>
2025-07-31 19:03:12 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3589,7 +3589,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "caicai": "qianxunclub.github.io/CaiCai"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
