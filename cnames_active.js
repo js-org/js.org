9cc9a44185c288f01941aba356a6322735c6fa7b
Edsel Ayala <50189767+edsel77@users.noreply.github.com>
2025-07-31 16:05:02 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -285,6 +285,7 @@ var cnames_active = {
   "axios-cache-interceptor": "arthurfiorette.github.io/axios-cache-interceptor",
   "axios-cache-lite": "nom-nom-hub.github.io/axios-cache-lite",
   "axon": "caracal7.github.io/axon.js",
+  "ayala": "edsel-ayala-portfolio.netlify.app",
   "azbuka-vkusa": "bullwinkle.github.io/azbuka-vkusa",
   "aziz": "iaziz786.netlify.app",
   "azlyrics-ext": "zyrouge.github.io/azlyrics-ext",
