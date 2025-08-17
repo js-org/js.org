4704ff96e91941dbcb77bf9d1c55d37da2bc89c8
haitham-512 <79488702+haitham-512@users.noreply.github.com>
2025-08-17 04:19:06 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3125,6 +3125,7 @@ var cnames_active = {
   "termi": "wearetermi.github.io/termi",
   "terminal": "gottox.github.io/terminal.js", // noCF? (don´t add this in a new PR)
   "tess": "det171.github.io/Tess.js",
+  "test123": "https://haitham-512.github.io/",
   "testframe": "testingrequired.github.io/testframe",
   "testio": "yavrumian.github.io/test.io",
   "tetr": "proximitynow19.github.io/TETR.JS",
