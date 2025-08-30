d7814819156e5cba5a382fdb736343de8c0d556c
Shivamkanse2104 <shivamkanse2104@gmail.com>
2025-08-30 14:36:50 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3612,7 +3612,9 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "shivamkanse": "shivamkanse.github.io/portfolio"
+
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
