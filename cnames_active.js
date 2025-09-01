89f66401f94bc9cc9422b514d856ac3b2e3475f4
ARJUN <105428932+arjun2014@users.noreply.github.com>
2025-09-01 15:17:47 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2483,7 +2483,7 @@ var cnames_active = {
   "quantum": "militia21.github.io/quantum",
   "que": "seatwork.github.io/que.js",
   "querybuilder": "mistic100.github.io/jQuery-QueryBuilder",
-  "quickinstaller": "arjun2014.github.io/quickinstaller" // noCF? (don´t add this in a new PR)
+  "quickinstaller": "arjun2014.github.io/quickinstaller", // noCF? (don´t add this in a new PR)
   "quick": "d7b7ae5e1e-hosting.gitbook.io", // noCF
   "quick-crud": "kingrayhan.github.io/quick-crud",
   "quickdb": "plexidev.github.io/quick.db-docs",
