139431feb7003300973ff692d8e2e560476da550
daily pixel <dailypixelsite@gmail.com>
2025-09-27 12:42:44 +0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -161,6 +161,7 @@ var cnames_active = {
   "alpha-coders": "biologyscience.github.io/alpha-coders",
   "alphabetify": "davidpomerenke.github.io/alphabetify",
   "alpine-ajax": "imacrayon.github.io/alpine-ajax",
+    "dailypixel": "dailypixelsite.github.io/daily-pixel",
   "alt": "goatslacker.github.io/alt", // noCF? (don´t add this in a new PR)
   "alveron": "cname.vercel-dns.com", // noCF
   "alyreza": "alyreza.github.io",
