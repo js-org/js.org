53a5e2075b2d8f3010cd251299860334bd86fd7d
Stefan Keim <indus@users.noreply.github.com>
2025-10-11 19:07:13 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2455,7 +2455,7 @@ var cnames_active = {
   "pretty-print-json": "center-key.github.io/pretty-print-json",
   "prettylog": "moosecoop.github.io/PrettyLog",
   "printx": "x-ext.netlify.app",
-  "pristine": "sha256.github.io/Pristine", // noCF  
+  "pristine": "sha256.github.io/Pristine", // noCF
   "producify": "jesobreira.github.io/producify",
   "producthunt-trending": "xiaomingplus.github.io/producthunt-trending",
   "profanity-finder": "gautamkrishnar.github.io/profanity-finder.js",
