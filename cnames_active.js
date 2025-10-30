e1fff8cca64a16b998b0cf6c50b4ee4c8b87ba0a
madvidd <69314407+madvidd@users.noreply.github.com>
2025-10-30 11:34:30 +0000

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1847,6 +1847,7 @@ var cnames_active = {
   "madankumar": "jmadankumar.github.io",
   "madebyzen": "mzaini30.github.io/madebyzen",
   "madewithdativejs": "madewithdativejs.github.io",
+  "madvid": "madvid.js.org",
   "magickwand": "mmomtchev.github.io/magickwand.js",
   "magidoc": "magidoc-org.github.io/magidoc",
   "magnet": "magnetjs.github.io/Magnet", // noCF? (don´t add this in a new PR)
