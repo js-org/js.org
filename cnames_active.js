2cb2d6885ee2cae944243ca6eb1fe190d2a6ca72
Jonathan Baquero <78324860+JonathanBaquero01@users.noreply.github.com>
2025-10-31 19:20:26 -0500

    Add new CNAME entries for nuestro-bot-unad

--- cnames_active.js
+++ cnames_active.js
@@ -3656,7 +3656,9 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "nuestro-bot-unad": "jonathanbaquero01.github.io/Nuestro-bot-UNAD",
+
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
