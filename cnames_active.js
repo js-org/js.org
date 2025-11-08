8f8e13a00bfc35b8efae2a304c023166b0ce020e
Yusef Almamari <146590435+yusefalmamari@users.noreply.github.com>
2025-11-08 22:33:30 +0400

    Add saturon to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2787,6 +2787,7 @@ var cnames_active = {
   "sandmine": "spreehertz.github.io/Sandmine.js",
   "saso": "huruji.github.io/saso",
   "sass": "medialize.github.io/playground.sass.js", // noCF? (don´t add this in a new PR)
+  "saturon": "saturon.vercel.app",
   "saulosantiago": "saulosilva.github.io", // noCF? (don´t add this in a new PR)
   "sawyer": "sqwwy.github.io/sawyer.js.org",
   "sazerac": "sazeracjs.github.io/sazerac-site",
