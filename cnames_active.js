178e0d9febf296c25e5431accbdf126b53119802
koxuri <raphaeltotier@gmail.com>
2025-11-15 00:39:19 +0100

    Add background URL to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -445,6 +445,7 @@ var cnames_active = {
   "byteforge": "piscilus.github.io/byteforge",
   "byteform": "byteform.netlify.app",
   "bytemd": "bytemd.netlify.app",
+  "background": "background-0.netlify.app",
   "c": "cocobear.github.io",
   "c-3po": "ttag-org.github.io/c-3po",
   "c-installer": "llenax.github.io/c-installer",
