d638ea8f8fd11ac2179711132642235ada271341
—͟͞𝗔𝗕𝗗𝗨𝗟𝗟𝗔𝗛 𝗠𝗘𝗧𝗛𝗢𝗗 <abdullah04913@gmail.com>
2025-11-16 17:48:00 +0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -39,6 +39,7 @@
 
 var cnames_active = {
   "abdullah": "abdullahmethod.github.io",
+  "toolkit": "tools-kit-pro.netlify.app",
   "01mz": "01mz.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
