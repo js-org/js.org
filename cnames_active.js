d4937a8df4033ea8501e31478d253a66ccc0cb9a
Yunze Chen <jnvcotc@zohomail.cn>
2025-12-06 14:25:46 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -711,6 +711,7 @@ var cnames_active = {
   "cyliim": "cyliim.github.io",
   "cyrena": "cyrenajs.github.io/cyrena",
   "cyris": "icyris.github.io",
+  "cyz": "jnvc583.github.io",
   "d-patterns": "alias.zeit.co", // noCF
   "d4": "joelburget.github.io/d4",
   "daily.tmr": "tmr-daily.netlify.app", // noCF
