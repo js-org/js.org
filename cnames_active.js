b52af02ca2ce36813456155ac5aef16f79ea290a
Pankaj Vaghela <vpankaj1998official@gmail.com>
2025-12-11 01:03:51 +0100

    Add `//noCF` to `sequelize-guard` CNAME entry
    Added comment `//noCF` to sequelize-guard entry.

--- cnames_active.js
+++ cnames_active.js
@@ -2851,7 +2851,7 @@ var cnames_active = {
   "senil": "mrfarhad.github.io/senilwebsite",
   "seoul": "seouljs.github.io/seoul.js.org",
   "sequel": "sequeljs.github.io",
-  "sequelize-guard": "cname.vercel-dns.com",
+  "sequelize-guard": "cname.vercel-dns.com", // noCF
   "sequential": "lorenzoongithub.github.io/sequential",
   "serginator": "serginator.github.io", // noCF? (don´t add this in a new PR)
   "serializer": "haircvt.github.io/serializerjs", // noCF? (don´t add this in a new PR)
