787841f0c640c196035af68fcfb5cbd90801d4e8
JOEElmenshawy <159122991+JOEElmenshawy@users.noreply.github.com>
2025-12-16 00:55:11 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -39,6 +39,7 @@
 
 var cnames_active = {
   "": "js-org.github.io",
+  "tomasulo" :"2258f7c7f01c8ce4.vercel-dns-017.com.",
   "01mz": "01mz.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
