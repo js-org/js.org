45d38a8a9618f82c9d0b9bebbf1acd0f5d6009c2
mindus0 <parisienres@gmail.com>
2025-12-19 00:35:37 -0500

    Add gitforge.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1346,6 +1346,7 @@ var cnames_active = {
   "gyps": "huijari.github.io/Gyps",
   "gyre": "wridder.github.io/GyreJS", // noCF? (don´t add this in a new PR)
   "gyx": "yourtion.github.io/gyx.js.org",
+  "gitforge": "gitforge.onrender.com/mindus/nexus/index",
   "h": "makenowjust.github.io/h.js",
   "h3": "h3rald.github.io/h3",
   "h7ml": "h7ml.github.io/web",
