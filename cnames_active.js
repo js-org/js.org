7a7fbd4e8631358542b60c2b44717c6e54469052
Fluxora <fluxora156@hotmail.com>
2025-12-20 17:43:01 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3686,7 +3686,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "fluxora": "fluxora-tools.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
