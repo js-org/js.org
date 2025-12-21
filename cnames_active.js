79538538174918e65daffb271e6acd50647d2f91
Dileep100 <121390926+Dileep100Code@users.noreply.github.com>
2025-12-21 21:04:19 +0530

    Restore pappify subdomain entry in cnames_active

--- cnames_active.js
+++ cnames_active.js
@@ -1,6 +1,6 @@
 
 
-"pappify": "dileep100code.github.io/pappify",
+
 
 /*
  ***** ACTIVE SUBDOMAINS
@@ -43,6 +43,7 @@
 
 var cnames_active = {
   "": "js-org.github.io",
+  "pappify": "dileep100code.github.io/pappify",
   "01mz": "01mz.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
