0605f4c604205d1b1a11937eb7ae7529e213de63
vtempest <1274452+vtempest@users.noreply.github.com>
2025-12-29 11:42:48 -0800

    Change 'grab' URL to cname.vercel-dns.com

--- cnames_active.js
+++ cnames_active.js
@@ -1298,7 +1298,7 @@ var cnames_active = {
   "gotanda": "gotandajs.github.io",
   "govind": "emgk.github.io",
   "gr": "cname.vercel-dns.com", // noCF
-  "grab": "grab-url.vercel.app",
+  "grab": "cname.vercel-dns.com", // noCF
   "gracile": "gracile.netlify.app",
   "graffle": "jasonkuhrt.github.io/graphql-request",
   "gram": "gram-js.github.io/docs",
