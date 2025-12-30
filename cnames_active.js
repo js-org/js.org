9026f1cf1b29a6e78dd0de74a94bb086afb99466
ahazoorbhinder <ahazoorbhinder@gmail.com>
2025-12-30 14:59:02 +0500

    Add ahbhinder.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -122,6 +122,7 @@ var cnames_active = {
   "agros": "agrosjs.github.io",
   "agrume": "cname.vercel-dns.com", // noCF
   "ahmad": "aruzikulov.github.io/ahmad",
+  "ahbhinder": "ahazoorbhinder.github.io",
   "ahnotification": "ahmadhayyan.github.io/ahnotification.js",
   "ahooks": "ahooks.surge.sh",
   "ahooks-v2": "ahooks-v2.surge.sh",
