732107604e3eabca1a6593c7eeae7c3728af36fe
298z7wvpqj-collab <298z7wvpqj@privaterelay.appleid.com>
2026-01-02 10:53:25 +0530

    Add ecomcrew.js

---
+++ ecomcrew.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "EcomCrew",
+  description: "E-commerce growth services for Amazon, Flipkart & Meesho sellers",
+  website: "https://ecomcrew.pages.dev",
+  owner: {
+    name: "EcomCrew",
+    email: "contact@ecomcrew.in"
+  }
+};
