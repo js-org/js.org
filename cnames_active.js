f9db8d8ae95166a9e63b36059fdeb8c85858e973
Luvion <samcheng072@gmail.com>
2026-01-19 00:35:18 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1086,7 +1086,7 @@ var cnames_active = {
   "extraction": "rse.github.io/extraction", // noCF? (don´t add this in a new PR)
   "eye": "arguiot.github.io/EyeJS",
   "ezoradom": "4614s.github.io/ezoradom-the-functions",
-  'fax-lang': 'luvion1.github.io',
+  "fax-lang": "luvion1.github.io/Fax-lang",
   "f1": "marinofranz.github.io/f1.ts",
   "facepalm": "santiagogil.github.io/facepalm",
   "facreative": "facreative.github.io",
