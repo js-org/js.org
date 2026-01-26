edbc76dbda461f4b03a4acda179aad26035033f5
Tajudin A <92188273+Tajudin143@users.noreply.github.com>
2026-01-26 03:32:54 +0300

    Add tajudin.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -3184,7 +3184,7 @@ var cnames_active = {
   "taha": "orcxzjeeeee.github.io/taha",
   "tailwind-baseline": "apkoponen.github.io/tailwind-baseline-site",
   "tailwindtocss": "tailwindtocss.netlify.app",
-  "tajudin": "Tajudin143.github.io",
+  "tajudin": "tajudin143.github.io",`
   "takeout": "takeout-bysourfruit.github.io",
   "talk": "zonayedpca.github.io/talk.js",
   "talker": "secondstreet.github.io/talker.js", // noCF? (don´t add this in a new PR)
