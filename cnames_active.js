72546f0bb25cd6cfc8511fb739b15c8e32f6906a
Aleksei Ivanov <contact@aleksei.dev>
2026-02-05 08:06:17 +0300

    Add 'lavoro' entry to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1745,6 +1745,7 @@ var cnames_active = {
   "lavacord": "lavacord.github.io/Lavacord",
   "lavashark": "hmes98318.github.io/LavaShark",
   "laveesh": "laveesh.github.io",
+  "lavoro": "apex-loadbalancer.netlify.com", // noCF
   "lazy": "gopirathod.github.io/lazy",
   "lazycipher": "lazycipher.github.io",
   "lazycoder": "prasantdev.github.io/lazycoder",
