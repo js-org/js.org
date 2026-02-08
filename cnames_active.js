aff1498f2ff2ece980cc5df94b05a6d6e937e589
ngoctuk36 <bngoctu629@gmail.com>
2026-02-08 19:53:16 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -298,6 +298,7 @@ var cnames_active = {
   "azura": "hosting.discloud.app", // noCF
   "azurajs-scalar": "d3veduardo.github.io/azurajs-scalar",
   "azurapi": "azurapi.github.io/azurapi-js",
+  "buitu": "buitu.vercel.app",
   "bachors": "bachors.github.io",
   "backlog": "backlog-js.github.io/backlog.js.org", // noCF? (don´t add this in a new PR)
   "backstage": "backstagejs.github.io/backstage",
