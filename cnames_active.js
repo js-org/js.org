438cdee1bcae2ffbcb2b6a363690b8acb4f9ba33
dangreen <danon0404@gmail.com>
2026-02-15 20:05:11 +0400

    nano_kit alias

--- cnames_active.js
+++ cnames_active.js
@@ -2151,6 +2151,7 @@ var cnames_active = {
   "namelessman": "namelessman.github.io",
   "nanimation": "imthenachoman.github.io/nAnimation", // noCF? (don´t add this in a new PR)
   "nano": "planet-free.github.io/nano",
+  "nano-kit": "trigensoftware.github.io/nano_kit",
   "nano-queries": "vitonsky.github.io/nano-queries",
   "nano_kit": "trigensoftware.github.io/nano_kit",
   "nanoexpress": "hosting.gitbook.com",
