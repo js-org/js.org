cd8d0382c2fc8ac1ce4da75e4991ea83bfda3983
1key <rolandsbates@gmail.com>
2026-02-16 09:28:22 -0500

    Add 1key.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -47,6 +47,7 @@ var cnames_active = {
   "1c": "oknosoft.github.io/1c",
   "1diaboliko84": "1diaboliko84.github.io",
   "1ib": "1ib.github.io",
+  "1key": "1key.pages.dev",
   "21cm": "21cm.github.io",
   "23chemistrycamp": "nchuchemistry.github.io/23chemistrycamp",
   "2tube": "you2php.github.io/you2php",
