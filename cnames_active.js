75447b4a4b20f71889e6741637a1d56dfeca77fa
VenomSnake_Twitch <Venomsnaketwitch@gmail.com>
2026-02-17 15:14:25 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3740,6 +3740,8 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "venomsnakeofficalwebseite": "VenomSnakeTwitch/VenomSnake_Twitch-Webseite",
+
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
