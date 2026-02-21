74e22b68d5bd6953e4ce24cab93e0cb12b844ceb
˹  ᴘʀᴏᴠɪᴅᴇʀʙᴏᴛᴢ  ˼ <animeokids@gmail.com>
2026-02-21 20:10:07 +0530

    Add providerbotz to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3743,7 +3743,8 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "providerbotz": "providerbotz.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
