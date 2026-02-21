06f1fc8b964a7a9e66c24f483ecbf47b3701b2c4
Sharek Abdullah | GlitchForge <glitchforge97@gmail.com>
2026-02-21 17:28:03 +0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -39,6 +39,7 @@
 
 var cnames_active = {
   "": "js-org.github.io",
+  "brainbeebd": "commanderforge.github.io",
   "01mz": "01mz.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
