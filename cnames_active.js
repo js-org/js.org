5c182f7abc98a62d970e7ddb85460e678e52ccf7
Andrew <notspammail@icloud.com>
2026-02-25 13:53:02 +0200

    Add `guilds.js.org`

--- cnames_active.js
+++ cnames_active.js
@@ -1349,6 +1349,7 @@ var cnames_active = {
   "guildedbot": "guilded-bot-npm.github.io/Guilded-Bot-Docs",
   "guildedts": "cname.vercel-dns.com", // noCF
   "guildeno": "guildeno.github.io/guildeno",
+  "guilds": "andrewdku.github.io/guilds.js",
   "guilherme": "guilhermedelemos.github.io/guilherme",
   "guillotine": "matiasgagliano.github.io/guillotine", // noCF? (don´t add this in a new PR)
   "guitar": "ronyeh.github.io/guitar",
