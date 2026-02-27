c91e0072ccb28963877e932ad1078f9aec158348
Peter <127602778+peteroin@users.noreply.github.com>
2026-02-27 23:53:17 +0530

    peter-3d-hub

--- cnames_active.js
+++ cnames_active.js
@@ -2425,7 +2425,7 @@ var cnames_active = {
   "personalkanban": "nishantpainter.github.io/personal-kanban",
   "perspective": "leopoldthecoder.github.io/Perspective",
   "peter": "peterccpang.github.io",
-  "peterkumar": "peteroin.github.io/peterkumar",
+  "peter-3d-hub": "peteroin.github.io/peterkumar",
   "pguth": "pguth.github.io",
   "ph1p": "ph1p.github.io",
   "phaker": "phakerjs.github.io/phaker.js",
