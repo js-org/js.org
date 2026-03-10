79347a58e04f23cb4697b4716424f4a3ee96b075
subumanktm <subu777k@gmail.com>
2026-03-10 12:22:24 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3769,6 +3769,7 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "testingpro": "pitampuri.github.io/myproject"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
