c2a54c12d1f206292675b5b87e1d2c93fadf9d16
uikoo9 <uikoo9@qq.com>
2026-03-18 07:26:58 +0000

    vino.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -3541,6 +3541,7 @@ var cnames_active = {
   "vigour": "mokunshao.github.io/vigour",
   "villain": "btzr-io.github.io/Villain",
   "vinimdocarmo": "vinimdocarmo.github.io", // noCF? (don´t add this in a new PR)
+  "vino": "uikoo9.github.io/vino.js",
   "vinton": "vinton.github.io/website",
   "viperhtml": "viperhtml.github.io",
   "virtual-clock": "dvdgiessen.github.io/virtual-clock",
