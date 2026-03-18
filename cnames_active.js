92a75d450ef89b39709f6ce0a1b3a84fcb00053c
elit3snip3r <88350368+Elitesniper06@users.noreply.github.com>
2026-03-18 12:36:33 +0100

    Added storeflow

--- cnames_active.js
+++ cnames_active.js
@@ -3163,6 +3163,7 @@ var cnames_active = {
   "stoneyallen": "stoneyallen.github.io",
   "stonk": "anujsharma141.github.io/stonk",
   "storage": "uzitech.github.io/storage.js",
+  "storeflow": "store-flow-phi.vercel.app",
   "storelocator": "storelocatorjs.github.io",
   "storybook": "apex-loadbalancer.netlify.app", // noCF
   "storybooker": "guptasiddhant.github.io/storybooker",
