223cbeb987ebe694c758f4493b4942660aee21af
Indunil Chathuranga <154481796+indunilchathuranga@users.noreply.github.com>
2026-03-20 13:01:28 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1519,6 +1519,7 @@ var cnames_active = {
   "imrc-datetime-picker": "smrsan76.github.io/imrc-datetime-picker",
   "imvu": "imvujs.pages.dev", // noCF
   "indie": "indie-org.github.io/indie",
+  "indunilchathuranga": "indunilchathuranga.netlify.app",
   "inference": "cname.vercel-dns.com", // noCF
   "infinite-tree": "cheton.github.io/infinite-tree",
   "infoooze": "devxprite.github.io/infoooze",
