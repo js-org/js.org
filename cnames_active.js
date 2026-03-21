c5e7f79224bd947bf7a838ff2e37d5584186b9ee
Rukkit-official <rukkitofficial@gmail.com>
2026-03-21 21:39:19 +0900

    Add css-animator.js.org for CSS-Animatior

--- cnames_active.js
+++ cnames_active.js
@@ -701,7 +701,7 @@ var cnames_active = {
   "cryptons-api": "agenthackeryt.github.io/cryptons-api",
   "crystaldb": "erikmcm.github.io/CrystalDB",
   "crystalpay-sdk": "deadshumz.github.io/crystalpay-sdk",
-  "css-animator": "rukkit-official.github.io/CSS-animator/", 
+  "css-animator": "rukkit-official.github.io/CSS-animator",
   "css-variable": "jantimon.github.io/css-variable",
   "cssfun": "8tentaculos.github.io/cssfun",
   "cssx": "salteadorneo.github.io/cssx",
