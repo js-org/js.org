bdc2bb36a3efe426b0a3cbf8ebbde42e8cfc77e1
Firsty <fc.firsty@gmail.com>
2026-03-23 08:32:34 +0700

    Replace screensaver with repeat-js in cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3764,7 +3764,7 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes",
-  "screensaver": "hifirsty999.github.io"
+  "repeat-js": "hifirsty999.github.io"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
