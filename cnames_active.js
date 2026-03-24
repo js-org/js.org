12ccf2844cfbdfd27046a3fa0a90f13f3f2b5f20
Stefan Keim <indus@users.noreply.github.com>
2026-03-24 16:45:58 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3290,7 +3290,7 @@ var cnames_active = {
   "termi": "wearetermi.github.io/termi",
   "terminal": "gottox.github.io/terminal.js", // noCF? (don´t add this in a new PR)
   "tess": "det171.github.io/Tess.js",
-  "test":"test",
+  "test":"test.github.io",
   "testframe": "testingrequired.github.io/testframe",
   "testio": "yavrumian.github.io/test.io",
   "tetr": "proximitynow19.github.io/TETR.JS",
