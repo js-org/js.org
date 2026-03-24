8f992b6ccd2098e9f77340afbb3b68f430c24317
discify <discify@hotmail.com>
2026-03-24 15:48:14 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3752,11 +3752,11 @@ var cnames_active = {
   "yt-quota-manager": "arnav-kr.github.io/yt-quota-manager",
   "yu": "yuxizhe.github.io/yu",
   "yuko": "notmarx.github.io/Yuko-Website",
+  "yunite": "discify.github.io/yunite.js",
   "yup-phone": "abhisekp.github.io/yup-phone",
   "yurba": "cname.vercel-dns.com", // noCF
   "yurd": "thebigbot0000.github.io/yurd.js.org",
   "yuuka": "yuukajs.github.io/docs",
-  "yunite": "discify.github.io/yunite.js",
   "yuye": "cyea.github.io",
   "yvm": "tophat.github.io/yvm",
   "zaimu": "zaimu.netlify.app", // noCF
