a333230c16d139e5552077a6807b396bfd5a8588
shankar282YT <shankarshaw815@gmail.com>
2026-03-26 13:51:27 +0530

    Update cnames_active.js
    add axiom.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -296,6 +296,7 @@ var cnames_active = {
   "axios-cache-interceptor": "arthurfiorette.github.io/axios-cache-interceptor",
   "axios-cache-lite": "nom-nom-hub.github.io/axios-cache-lite",
   "axon": "caracal7.github.io/axon.js",
+  "axiom": "shankar282YT.github.io/axiom-backend",
   "azbuka-vkusa": "bullwinkle.github.io/azbuka-vkusa",
   "aziz": "iaziz786.netlify.app",
   "azlyrics-ext": "zyrouge.github.io/azlyrics-ext",
