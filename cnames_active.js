96974340c984a303fc67c6a8d8c6b8e212ce8614
shepu9462-art <shepu9462@gmail.com>
2026-03-27 12:42:24 +0600

    Add Shepu subdomain to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3794,6 +3794,7 @@ var cnames_active = {
   "zylog": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "Shepu": "shepu9462-art.github.io/My-website/"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
