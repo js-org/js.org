ea2e04839ef43b261368aeb97d258da4aa07d591
shepu9462-art <shepu9462@gmail.com>
2026-03-27 18:13:58 +0600

    Add shepu entry to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2961,6 +2961,7 @@ var cnames_active = {
   "sheet": "sheetjs.github.io/sheet.js.org",
   "shekhar": "shekharreddy4.github.io/Shekhar",
   "shell": "adaltas.github.io/node-shell",
+  "shepu": "https://shepu9462-art.github.io/"
   "sheweny": "sheweny.github.io/framework",
   "shex": "shexjs.github.io/shex.js",
   "shift": "shift-wasm.pages.dev",
