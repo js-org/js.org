dc4c6e4198059ab4154044d4b7d7b94f1c0d416b
Ma'moon Al-Akash <phpirate@gmail.com>
2026-03-29 04:50:45 +0300

    akash.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -140,6 +140,7 @@ var cnames_active = {
   "akala": "npenin.github.io/akala",
   "akar": "mahabubx7.github.io/akar",
   "akase": "cedmax.github.io/akase", // noCF? (don´t add this in a new PR)
+  "akash": "phpirate.github.io/akashjs",
   "akhyar": "akhyar.pages.dev",
   "aksara": "sajenid.github.io/aksara.js",
   "aksharamukha": "paramsiddharth.github.io/aksharamukha.js",
