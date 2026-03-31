777ba43ae1d2569d94bfe5b33e81208ca8cb6ce7
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2026-03-31 13:57:10 +0600

    add tarikh subdomain

--- cnames_active.js
+++ cnames_active.js
@@ -3262,6 +3262,7 @@ var cnames_active = {
   "tao": "zzyzxlab.github.io/tao.js",
   "tapi": "sinisimattia.github.io/tapi",
   "tara": "shakyatara.github.io",
+  "tarikh": "cname.vercel-dns.com", // noCF
   "tarant": "tarantx.github.io",
   "taro": "nervjs.github.io/taro-website",
   "tart": "tart.github.io/tartJS", // noCF? (don´t add this in a new PR)
