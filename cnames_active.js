9b7cca6dec71b08704d3e2651f6d9aa9b2f83abe
abongslot-dev <rsh74772@gmail.com>
2026-04-01 07:02:21 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -78,7 +78,7 @@ var cnames_active = {
   "abhishekmittal": "abhishek-mittal.github.io/abhishekmittal",
   "abi": "abi-js.github.io",
   "abialbon": "abialbon.github.io",
-  "abongmantap": "abongslot-web.vercel.app",
+  "abongslot": "abongslot-web.vercel.app",
   "abra": "abracadabra-demo.pages.dev",
   "abracadabra": "doc-cgg.pages.dev",
   "aby": "icoldplayer.github.io/aby",
