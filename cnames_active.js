fe88344f0994df2c0f2969340e40247c58e7581d
mmahdih <191803465+mmahdih@users.noreply.github.com>
2026-04-02 09:54:47 +0200

    Add new CNAME entry for mainz

--- cnames_active.js
+++ cnames_active.js
@@ -3804,7 +3804,8 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zylog": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "mainz": "mmahdih.github.io/calc"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
