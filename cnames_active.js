884377078e7c2ddfc82b24bcbf31d01d85cc0560
Vineet Pratap Singh <vtps@duck.com>
2026-04-05 09:39:57 +0530

    Add vinimdocarmo entry to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3564,8 +3564,8 @@ var cnames_active = {
   "video-react": "video-react.github.io",
   "vigour": "mokunshao.github.io/vigour",
   "villain": "btzr-io.github.io/Villain",
-  "vinimdocarmo": "vinimdocarmo.github.io", // noCF? (don´t add this in a new PR)
   "vineet": "vptsh.github.io/nexfolio",
+  "vinimdocarmo": "vinimdocarmo.github.io", // noCF? (don´t add this in a new PR)
   "vino": "uikoo9.github.io/vino.js",
   "vinton": "vinton.github.io/website",
   "viperhtml": "viperhtml.github.io",
