9dd7cd5a16d90b5792af0204faced0df241f3e86
A01 Dev <126718579+a01adem@users.noreply.github.com>
2026-04-06 00:25:43 +0200

    Add distats URL to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -40,6 +40,7 @@
 var cnames_active = {
   "": "js-org.github.io",
   "01mz": "01mz.github.io",
+  "distats": "https://distats.vercel.app",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
   "131": "netrvin.github.io",
