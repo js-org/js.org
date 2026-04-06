fe72c8ea8e0fddb80e1e11ec9096bf24d6d19bf7
PIPSH <piperalt4@gmail.com>
2026-04-06 13:31:50 +0200

    Add studworks to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3195,6 +3195,7 @@ var cnames_active = {
   "strygwyr": "sinkaroid.github.io/strygwyr",
   "stuck": "linuxenko.github.io/unstuck-webpack",
   "studios": "botstudios.github.io/Studios",
+  "studworks": "studworks.onrender.com",
   "style": "dhilipsiva.github.io/style.js", // noCF? (don´t add this in a new PR)
   "styled-css-grid": "styled-css-grid.netlify.app",
   "stylis": "thysultan.github.io/stylis.js",
