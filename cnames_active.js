0f4b8a01bcc8ed30f4b26fc0351f3e9f0aeab503
akibkawshara7 <na9714152@gmail.com>
2026-04-08 19:51:12 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1682,6 +1682,7 @@ var cnames_active = {
   "jwjawa": "cmdjwj.github.io",
   "jwt-autorefresh": "cchamberlain.github.io/jwt-autorefresh", // noCF? (don´t add this in a new PR)
   "jwtonline": "boneyt92.github.io/jwtonline",
+  "kyotv": "akibkawshar7.github.io/kyotv"
   "kafka": "tulios.github.io/kafkajs",
   "kaguwo": "kaguwos-portfolio.netlify.app",
   "kahoot": "na-west1.surge.sh",
