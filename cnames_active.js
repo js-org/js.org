5a6db6b112b59db485c7d3e789386b4979528d25
kredrin1shere-blip <kredrin1shere@gmail.com>
2026-04-10 19:49:38 +0300

    Add new cname for 'the-past-project'

--- cnames_active.js
+++ cnames_active.js
@@ -38,6 +38,7 @@
  */
 
 var cnames_active = {
+  "the-past-project": "kredrin1shere-blip.github.io/The-Past-project",
   "": "js-org.github.io",
   "01mz": "01mz.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
