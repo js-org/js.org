5c95b89d20ba32635843ecab39954b10089bf5d4
Jyotishankar Patra <patrajyotishankar@gmail.com>
2026-04-15 18:28:21 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2342,7 +2342,7 @@ var cnames_active = {
   "observable-hooks": "crimx.github.io/observable-hooks",
   "octofetch": "maartenvn.github.io/OctoFetch",
   "odararmy": "odararmy.github.io",
-  "odialang": "cname.vercel-dns.com",
+  "odialang": "cname.vercel-dns.com", // noCF`
   "odin": "foxifyjs.github.io/odin",
   "oec": "crellison.github.io/oec",
   "oem": "linttrapmedia.github.io/oem",
