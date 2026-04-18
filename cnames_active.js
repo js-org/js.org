b65c1330340346197b6ef3076f728b956c7c46d9
losenchufaos <17eli.nunezs@gmail.com>
2026-04-17 21:26:05 -0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1763,6 +1763,7 @@ var cnames_active = {
   "kunal": "kunalghosh02.github.io",
   "kunkun": "smallkunkun.github.io",
   "kyoto": "kyotojs.github.io",
+  "losenchufaos": "losenchufaos.github.io",
   "l2dwidget": "xiazeyu.github.io/live2d-widget.js-doc",
   "lab": "labjs.netlify.app",
   "labelauty": "fntneves.github.io/jquery-labelauty", // noCF? (don´t add this in a new PR)
