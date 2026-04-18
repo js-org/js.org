d0d9e85d9f7555c55e369e363cbdd5751cdf1427
Neil Paolo Cabrera <neilpaolocabrera@gmail.com>
2026-04-18 22:58:52 +0800

    Add staycationweb and update stein CNAME

--- cnames_active.js
+++ cnames_active.js
@@ -3179,6 +3179,7 @@ var cnames_active = {
   "status": "mattipv4.github.io/status-codes",
   "status-hx": "status-hx-js-org.dns.huangxin.org", // noCF
   "stax": "stax.serv00.net",
+  "staycationweb": "cname.vercel-dns.com", 
   "steemsites": "yhozen.github.io/steemsites",
   "stego": "zeruniverse.github.io/CryptoStego", // noCF? (don´t add this in a new PR)
   "stein": "cname.vercel-dns.com", // noCF
