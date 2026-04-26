c64f10d7b039c8b961bdc09a2cda48c69ff3f346
Darking053 <darking053@protonmail.com>
2026-04-26 15:14:14 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1392,6 +1392,7 @@ var cnames_active = {
   "gyps": "huijari.github.io/Gyps",
   "gyre": "wridder.github.io/GyreJS", // noCF? (don´t add this in a new PR)
   "gyx": "yourtion.github.io/gyx.js.org",
+  "gettic": "darking053official.github.io",
   "h": "makenowjust.github.io/h.js",
   "h3": "h3rald.github.io/h3",
   "h7ml": "h7ml.github.io/web",
