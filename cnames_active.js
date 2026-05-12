3ff7e57f63a9d36825f8c7acdbcdc83c1092cef9
Gh995 <mahdi.aladdab@gmail.com>
2026-05-12 21:02:28 +0300

    Add gh995 subdomain to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3846,6 +3846,8 @@ var cnames_active = {
   "zylog": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "gh995": "gh995.github.io",
+
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
