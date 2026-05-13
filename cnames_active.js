cb5e0102e4e1456c1103b867e60c867b32f95024
YamanWeb1 <YamanWebsite@outlook.com>
2026-05-13 18:59:09 +0400

    Add dairy.js.org → yamanweb1.github.io/bethany-hamilton

--- cnames_active.js
+++ cnames_active.js
@@ -741,6 +741,7 @@ var cnames_active = {
   "d-patterns": "alias.zeit.co", // noCF
   "d4": "joelburget.github.io/d4",
   "daily.tmr": "tmr-daily.netlify.app", // noCF
+  "dairy": "yamanweb1.github.io/bethany-hamilton",
   "daisy": "warengonzaga.github.io/daisy.js",
   "daksh0225": "daksh0225.github.io",
   "damo": "damodharanj.github.io",
