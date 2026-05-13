e3909e2499dc36293e59ca58e24f5520455f2812
Noel Muthomi <144278778+noelmuthomi@users.noreply.github.com>
2026-05-13 13:40:10 +0300

    Update cnames_active.js
    This is a portal for a Church School Ministry in Nairobi, Kenya, helping teachers manage student registers and resources using a JavaScript-based (Apps Script) backend.

--- cnames_active.js
+++ cnames_active.js
@@ -1773,6 +1773,7 @@ var cnames_active = {
   "kun": "lmk97.github.io/kun-docs",
   "kunal": "kunalghosh02.github.io",
   "kunkun": "smallkunkun.github.io",
+  "kwpchurchschool": "noelmuthomi.github.io",
   "kyoto": "kyotojs.github.io",
   "l2dwidget": "xiazeyu.github.io/live2d-widget.js-doc",
   "lab": "labjs.netlify.app",
@@ -3852,3 +3853,4 @@ var cnames_active = {
    * <3
    */
 }
+Add kwpchurchschool.js.org
