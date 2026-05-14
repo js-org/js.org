07e6c9d70dcdc4b7798b7cbb27c2409950a0c63f
heiimzy <35106873+heiimzy@users.noreply.github.com>
2026-05-14 12:49:43 +0800

    Add ai-money-maker.js.org subdomain

--- cnames_active.js
+++ cnames_active.js
@@ -306,6 +306,7 @@ var cnames_active = {
   "azura": "hosting.discloud.app", // noCF
   "azurajs-scalar": "d3veduardo.github.io/azurajs-scalar",
   "azurapi": "azurapi.github.io/azurapi-js",
+  "ai-money-maker": "heiimzy.github.io/ai-money-maker",
   "bachors": "bachors.github.io",
   "backlog": "backlog-js.github.io/backlog.js.org", // noCF? (don´t add this in a new PR)
   "backolon": "r47onfire.github.io/backolon",
