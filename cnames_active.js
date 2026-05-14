8396eae7056d080412b1df5143b11007057ee42d
StoreLunchFlow <jusspound@gmail.com>
2026-05-14 12:26:48 +0200

    Remove duplicate entries for js2lua, jscord, and jsdec

--- cnames_active.js
+++ cnames_active.js
@@ -1655,11 +1655,8 @@ var cnames_active = {
   "js-labs": "cname.vercel-dns.com", // noCF
   "js-utils": "teneplaysofficial.github.io/js-utils-kit",
   "js2lua": "xiangnanscu.github.io/js2lua", // noCF
-  "js2lua": "xiangnanscu.github.io/js2lua", // noCF
   "jscord": "thepoptartcrpr.github.io/jscord",
   "js-leadmachine": "NileGazer00.github.io/NileGazer00-js-leadmachine",
-  "jsdec": "liulihaocai.github.io/JSDec",
-  "jscord": "thepoptartcrpr.github.io/jscord",
   "jsdec": "liulihaocai.github.io/JSDec",
   "jsfe": "jsfe.netlify.app",
   "jslabs": "cname.vercel-dns.com", // noCF
