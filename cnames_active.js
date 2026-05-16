14f2466a0704fb8e87c9cce18a51bd51bd647e16
cagriatav8-sudo <cagriatav8@gmail.com>
2026-05-16 10:49:34 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2456,7 +2456,7 @@ var cnames_active = {
   "pastate": "birdleescut.github.io/pastate",
   "paste": "cname.vercel-dns.com", // noCF
   "pastebin-alert": "ramadhanamizudin.github.io/pastebin-alert.js", // noCF? (don´t add this in a new PR)
- "pathfinder": "kvnq-poza.github.io/pathfinder",
+  "pathfinder": "kvnq-poza.github.io/pathfinder",
   "pathtrace": "kovacsv.github.io/WebGLPathTrace", // noCF? (don´t add this in a new PR)
   "pear": "pear-site.vercel.app",
   "pax": "nathan.github.io/pax",
