9613a8cd02dfc801dcbed9d6b1ec7ffcbfaa40d1
Luis Miguel Hernandez Mejia <hm20059@ues.edu.sv>
2026-05-17 12:08:40 -0600

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3847,6 +3847,7 @@ var cnames_active = {
   "zylog": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "luishdzmejia": "https://coruscating-lolly-2c6344.netlify.app/"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
