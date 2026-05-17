d14d9bd76c3ac02850c725e8740f2eb6578c3f5a
Sholehuddin Khairy <musickhairy@gmail.com>
2026-05-17 13:15:36 +0700

    Added `japkgen.js.org`

--- cnames_active.js
+++ cnames_active.js
@@ -1693,6 +1693,7 @@ var cnames_active = {
   "jwjawa": "cmdjwj.github.io",
   "jwt-autorefresh": "cchamberlain.github.io/jwt-autorefresh", // noCF? (don´t add this in a new PR)
   "jwtonline": "boneyt92.github.io/jwtonline",
+  "japkgen": "japkgen.opendnf.cloud",
   "kafka": "tulios.github.io/kafkajs",
   "kaguwo": "kaguwos-portfolio.netlify.app",
   "kahoot": "na-west1.surge.sh",
