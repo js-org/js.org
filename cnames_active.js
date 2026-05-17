5b0b363a0cf7f3675f80765103b7c39fd47298a2
4XW1N <aswinkumarank@gmail.com>
2026-05-17 18:34:51 +0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2984,10 +2984,10 @@ var cnames_active = {
   "serializer": "haircvt.github.io/serializerjs", // noCF? (don´t add this in a new PR)
   "serlina": "djyde.github.io/serlina",
   "service-mocker": "service-mocker.github.io/service-mocker",
-  'shooter': '4xw1n.github.io',
   "serviceberry": "bob-gray.github.io/serviceberry",
   "servify-express": "aarondoran.github.io/servify-express",
   "sesam": "lennertderyck.github.io/sesam",
+  "shooter": "4xw1n.github.io",
   "setheum": "setheum-js.github.io",
   "sfml": "xadillax.github.io/node-sfml",
   "shadng": "alimjanablikim.github.io/sng-ui",
