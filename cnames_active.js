73714a7877e05f0a957f807677d60b3ee845931d
ozaycank <32206089+ozaycank@users.noreply.github.com>
2026-05-20 23:25:53 +0300

    Add 'fanx' CNAME entry
    Add fanx.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1137,6 +1137,7 @@ var cnames_active = {
   "fancy-pants": "fancy-pants.tritarget.org",
   "fandom": "fandomjs.netlify.app", // noCF
   "fantas": "fantastiser.github.io",
+  "fanx": "ns1.vercel-dns.com",
   "farfetch": "websitebeaver.github.io/far-fetch",
   "farfetchd": "achannarasappa.github.io/farfetchd", // noCF? (don´t add this in a new PR)
   "farzad": "wikiweb.github.io/farzad",
