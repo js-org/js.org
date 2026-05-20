73c8316e2a7b925f665d938642abf9d4aff16096
Nouman Qamar <84765475+noumanqamar450@users.noreply.github.com>
2026-05-20 17:40:01 +0500

    Fix URL for react-alert-box entry

--- cnames_active.js
+++ cnames_active.js
@@ -2681,7 +2681,7 @@ var cnames_active = {
   "rclone": "sntran.github.io/rclone.js",
   "rdf": "rdfjs.github.io", // noCF
   "rdpmovy": "rdp-studio.github.io/rdpmovy.js",
-  "react-alert-box": "https://noumanqamar450.github.io/react-alert-box/",
+  "react-alert-box": "noumanqamar450.github.io/react-alert-box",
   "react-atomic-ui": "react-atomic.github.io/react-atomic-ui",
   "react-autosuggest": "moroshko.github.io/react-autosuggest", // noCF? (don´t add this in a new PR)
   "react-autowhatever": "moroshko.github.io/react-autowhatever", // noCF? (don´t add this in a new PR)
