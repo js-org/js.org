74f053e4e07508fe1f0c4758aee76a0da2e3c4cf
Neon <neonlinegm@gmail.com>
2026-05-22 14:38:23 -0300

    Add neon-axg.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -3853,7 +3853,8 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zylog": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "neon-axg": "neon-axg.github.io",
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
