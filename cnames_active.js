d267b7428a0ddd49281f744913789a0d7c8b4fcb
AGS <51550110+SAKURAFLY0910@users.noreply.github.com>
2026-05-26 21:33:38 +0800

    Add cname for 'love' to Vercel DNS

--- cnames_active.js
+++ cnames_active.js
@@ -1908,6 +1908,7 @@ var cnames_active = {
   "loog": "israelroldan.github.io/loog",
   "lostyle": "rtsao.github.io/lostyle",
   "lottiefy": "pd4d10.github.io/lottiefy",
+  "love": "cname.vercel-dns.com", // noCF
   "loxt": "loxt.netlify.app",
   "lps": "mauris.github.io/lps.js.org",
   "lrclib": "igorwastaken.github.io/lrclib-api",
