cca9408767207d1f2232405ab6b11c5615e0ddca
vtxgamesofficial <robertrhys379@gmail.com>
2026-05-30 18:42:39 +0100

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1833,7 +1833,7 @@ var cnames_active = {
   "leipzig": "leipzigjs.github.io", // noCF? (don´t add this in a new PR)
   "lemn": "lemnjs.github.io/lemn",
   "leo": "leo66123.github.io",
-  "leotable": "cname.vercel-dns.com",
+  "leotable": "cname.vercel-dns.com", // noCF
   "leoj": "leoaj.github.io", // noCF? (don´t add this in a new PR)
   "leoric": "cyjake.github.io/leoric",
   "lerna": "lerna.github.io/website",
