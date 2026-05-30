247d872baf8698f59588d618a2dace50954614a7
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2026-05-30 21:22:37 +0600

    Restore coreify-env.js.org record

--- cnames_active.js
+++ cnames_active.js
@@ -660,6 +660,7 @@ var cnames_active = {
   "core-ln": "runcitadel.github.io/core-ln.ts",
   "coreact": "coreactjs.github.io/coreact",
   "coredi": "empla.github.io/coredi",
+  "coreify-env": "cname.vercel-dns.com", // noCF
   "coreutils": "ultirequiem.github.io/coreutils",
   "cork": "davej.github.io/CorkJS",
   "corki": "dvtate.github.io/corki",
