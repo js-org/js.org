81b7b2eeeb5ace0af7039826bacdbf6ca31ee0fe
vtxgamesofficial <robertrhys379@gmail.com>
2026-05-30 18:37:48 +0100

    Add leotable CNAME entry

--- cnames_active.js
+++ cnames_active.js
@@ -1834,6 +1834,7 @@ var cnames_active = {
   "leipzig": "leipzigjs.github.io", // noCF? (don´t add this in a new PR)
   "lemn": "lemnjs.github.io/lemn",
   "leo": "leo66123.github.io",
+  "leotable": "cname.vercel-dns.com",
   "leoj": "leoaj.github.io", // noCF? (don´t add this in a new PR)
   "leoric": "cyjake.github.io/leoric",
   "lerna": "lerna.github.io/website",
