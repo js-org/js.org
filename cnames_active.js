551d4d048c24a1572f37c0a18f5e237058377afe
MRAKS05 <mraks05@users.noreply.github.com>
2026-06-01 18:01:22 +0600

    Add browser-limit.js.org subdomain

--- cnames_active.js
+++ cnames_active.js
@@ -438,6 +438,7 @@ var cnames_active = {
   "brizusan": "blackmyab.github.io/brizusan",
   "brotat": "mariobob.github.io/brotat-website",
   "browse4extact": "browse4extract.github.io/b4e-website",
+  "browser-limit": "browser-limit.js.org",
   "browser-ocr": "oshekharo.github.io/Browser-OCR",
   "browserless": "microlinkhq.github.io/browserless",
   "brum": "romantic-noyce-67d6ee.netlify.app",
