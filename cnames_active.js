b54903ba6433cd6570768601e75457822e52a8a7
lui-josh <23102689@usc.edu.ph>
2026-06-01 21:33:44 +0800

    Add whitebored

--- cnames_active.js
+++ cnames_active.js
@@ -3739,6 +3739,7 @@ var cnames_active = {
   "which-node": "tobysmith568.github.io/which-node-js",
   "whistle": "whistle-lang.github.io/website",
   "whiteboard": "yhozen.github.io/whiteboard",
+  "whitebored": "whiteboard-app-v77u.onrender.com",
   "whoasked": "mathdebate09.github.io/who-asked",
   "wi": "rstee.serv00.net",
   "wice": "yulioaj290.github.io/wice.js",
