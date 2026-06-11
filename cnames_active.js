082247dd0b2ebc13fb7b0aa1bbe58222e1760b25
ZhangYuQiao326 <110710091+ZhangYuQiao326@users.noreply.github.com>
2026-06-12 00:32:47 +0800

    Add 'evil' entry to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1102,6 +1102,7 @@ var cnames_active = {
   "event-storage": "albe.github.io/node-event-storage",
   "eventstore": "adrai.github.io/node-eventstore", // noCF? (don´t add this in a new PR)
   "everything": "ajays97.github.io/Everything.js",
+  "evil": "maker-kit-saas-template.vercel.app",
   "evolutionland": "evolutionlandorg.github.io/js-sdk",
   "evolutions": "anshumanv.github.io/evolutions-webapp",
   "ewa": "lyfeyaj.github.io/ewa",
