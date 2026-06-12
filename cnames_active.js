94732db852cea633420a105a84a1dc700c06b3fd
charmpo2 <charmpo2@users.noreply.github.com>
2026-06-12 18:33:54 +0800

    Add sageshadowstream subdomain

--- cnames_active.js
+++ cnames_active.js
@@ -2913,6 +2913,7 @@ var cnames_active = {
   "sacgencode": "sacgencode.github.io",
   "saeed": "mrsaeeddev.github.io",
   "safe": "hiren2001.github.io/SafeJS",
+  "sageshadowstream": "shadowstream-bgo0.onrender.com",
   "sagui": "saguijs.github.io", // noCF? (don´t add this in a new PR)
   "sahilister": "sahilister.github.io/portfolio",
   "sai": "hackers4peace.github.io/sai-js",
