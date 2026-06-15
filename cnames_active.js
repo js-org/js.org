4a63d8650b85478a59d009ef1a16a77eb88c5660
ivandkriddd <ivandkriddd@gmail.com>
2026-06-15 11:06:17 +0300

    add robx
    yes

--- cnames_active.js
+++ cnames_active.js
@@ -2690,6 +2690,7 @@ var cnames_active = {
   "rax0-6": "raxjs.github.io/website-rax-v0.6",
   "raylen": "ausername-1.github.io/raylen.js",
   "rayo": "getrayo.github.io/rayo.js",
+  "rbx": "l3plihcj.up.railway.app", // noCF
   "rclone": "sntran.github.io/rclone.js",
   "rdf": "rdfjs.github.io", // noCF
   "rdpmovy": "rdp-studio.github.io/rdpmovy.js",
