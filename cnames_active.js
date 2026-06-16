0703660a58a5ef4b4040386b7c5f72dbafa32e71
JeremySharpless <harshaware11@gmail.com>
2026-06-16 17:47:07 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1669,7 +1669,7 @@ var cnames_active = {
   "jscord": "thepoptartcrpr.github.io/jscord",
   "jsdec": "liulihaocai.github.io/JSDec",
   "jsfe": "jsfe.netlify.app",
- "jaremysharpless": "JeremySharpless.github.io", 
+  "jeremysharpless": "JeremySharpless.github.io", 
   "jslabs": "cname.vercel-dns.com", // noCF
   "jsnippet": "jesseeisen.github.io/snippets", // noCF
   "jsoboro": "jsoboro.github.io",
