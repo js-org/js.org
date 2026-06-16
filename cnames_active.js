2edae79e349ad8d007afb1734d0426a4ec9807b3
DarkBotWeb <gamellab@eclipso.de>
2026-06-16 13:34:42 +0200

    Add darkbot CNAME entry

--- cnames_active.js
+++ cnames_active.js
@@ -3872,6 +3872,7 @@ var cnames_active = {
   "zylog": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "darkbot": "DarkBotWeb.github.io",
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
