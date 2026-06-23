fae5a1572755d05f2562e2418510940c712b5345
Stefan Keim <indus@users.noreply.github.com>
2026-06-23 15:56:08 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -39,6 +39,7 @@
 
 var cnames_active = {
   "": "js-org.github.io",
+  "test": "justatest.github.io",
   "01mz": "01mz.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
