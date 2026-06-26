6a4b39a37d4c4585f08d3a8480cc75c982b40b67
hberdon <hberdn@gmail.com>
2026-06-26 10:28:07 +0200

    Add notes.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2329,6 +2329,7 @@ var cnames_active = {
   "notebook": "wugenqiang.github.io/NoteBook",
   "noted": "carter-slade.github.io/Noted-", // noCF? (don´t add this in a new PR)
   "notepad": "amitmerchant1990.github.io/notepad",
+  "notes": "hberdon.github.io/notesjs",
   "nothing": "cbtak.github.io/nothing",
   "notibar": "duyetdev.github.io/notibar.js",
   "notion-cms": "cname.vercel-dns.com", // noCF
