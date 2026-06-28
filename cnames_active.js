ce7327acd6f43af7b478c5ddd3263dbcda4928cb
soltygeams <133987930+soltygeams@users.noreply.github.com>
2026-06-28 22:25:01 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3878,14 +3878,15 @@ var cnames_active = {
   "zizhujy": "jeff-tian.github.io/zizhujy",
   "zodiac": "indus.github.io/Zodiac", // noCF? (don´t add this in a new PR)
   "zodiacsigns": "dipanshkhandelwal.github.io/my-zodiac-sign",
-  "zombie": "assaf.github.io/zombie", // noCF? (don´t add this in a new PR)
+"zombie": "assaf.github.io/zombie", // noCF? (don´t add this in a new PR)
   "zoo": "ivankra.github.io/javascript-zoo",
   "zova": "cabloy.github.io/zova",
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zylog": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "novapanel": "nova-panel-web.solty433.workers.dev"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
