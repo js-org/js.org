6c01a6e3137a54d20bbe9f268f7119d83753c465
Adam Eehan <blindrusheroffical@gmail.com>
2026-06-28 11:48:56 +0530

    Add 'aira' entry to cnames_active.js
    Added a new entry for 'aira' in the cnames_active.js file.

--- cnames_active.js
+++ cnames_active.js
@@ -105,6 +105,7 @@ var cnames_active = {
   "aditya": "aditya81070.github.io",
   "adnanbabakan": "adnanbabakan.github.io",
   "adon988": "adon988.github.io",
+  "aira": "blindrusheroffical-wq.github.io",
   "adv": "advjs.github.io",
   "advancedrpc": "advancedrpc.github.io",
   "aerogel": "noeldemartin.github.io/aerogel",
