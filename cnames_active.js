70d9e700da324719c175ec842765b27d81cc967a
6p6t <dev.6p6t@proton.me>
2026-06-29 04:55:12 +0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -40,6 +40,7 @@
 var cnames_active = {
   "": "js-org.github.io",
   "01mz": "01mz.github.io",
+  "6p6t": "i6p6t.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
   "131": "netrvin.github.io",
