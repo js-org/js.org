b4592db977b0974938211c91ae39f6e0ab4da2e8
arakanartarea <arakanartarea@gmail.com>
2026-06-29 22:11:38 +0700

    Add arakanartarea to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -227,6 +227,7 @@ var cnames_active = {
   "apr": "ramitos.github.io/apr",
   "aprende-react": "ptcc.github.io/aprende-react",
   "aptrinh": "aptrinh.github.io",
+  "arakanartarea": "arakanartarea.github.io/ArakanArtArea",
   "arc": "diegohaz.github.io/arc",
   "archipelago": "thephar.github.io/archipelago.js",
   "arcn": "alexakasanjeev.github.io/arcn",
