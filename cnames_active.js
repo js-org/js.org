30835a5780cdc6762c2e4962ae7bb54cef1759db
Ritwan Ristanto <ritwan.ristanto@gmail.com>
2026-06-29 18:53:42 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -606,7 +606,7 @@ var cnames_active = {
   "codetype": "vedantkawade3.github.io/codeType",
   "codewing": "nicesapien.github.io/codewing",
   "codinsky": "izhaki.github.io/codinsky",
-  "coding-terminal": "ritwanristanto.github.io/Coding-Terminal",
+  "coding-terminal": "ritwanristanto.github.io",
   "coffea": "caffeinery.github.io/coffea",
   "coir": "channg.github.io/coir",
   "coldemo": "coldemo.github.io",
