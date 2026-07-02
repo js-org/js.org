5fe91cb73da075596d581f8b76b5c17a40520e9a
12null123 <148341273+12null123@users.noreply.github.com>
2026-07-02 15:27:53 +0400

    Restore osystudio entry in cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1283,7 +1283,6 @@ var cnames_active = {
   "gdal3": "bugra9.github.io/gdal3.js",
   "gdi": "katehra-brc.github.io/gdi.js.org",
   "geekr": "ruanyl.github.io/geekr", // noCF? (don´t add this in a new PR)
-  "osystudio": "cname.vercel-dns.com", // noCF
   "gemstone": "pierreavn.github.io/gemstone",
   "genanki": "krmanik.github.io/genanki-js",
   "generate-license-file": "tobyandtoby.github.io/generate-license-file",
@@ -2425,6 +2424,7 @@ var cnames_active = {
   "oscillator": "lukehorvat.github.io/super-oscillator",
   "oslo": "pilcrowonpaper.github.io/oslo",
   "osom": "kikobeats.github.io/osom",
+  "osystudio": "cname.vercel-dns.com", // noCF
   "ot": "otjs-docs.github.io",
   "otimizacao": "jmmccota.github.io/Otimizacao",
   "otp": "60-min-code.github.io/gen-totp",
