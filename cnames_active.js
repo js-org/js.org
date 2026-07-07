5f6bdc8a92b5beea5f2a50e51c7af42b007f2eed
lucaszatkowski3-eng <lucaszatkowski3@gmail.com>
2026-07-07 08:11:38 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1442,6 +1442,7 @@ var cnames_active = {
   "heartseekers": "rajington.github.io/heartseekers", // noCF? (don´t add this in a new PR)
   "heax": "coderosh.github.io/heaxjs", // noCF
   "hegel": "jsmonk.github.io/hegel",
+  "heidkoppel-marketplace": "lucaszatkowski3-eng.github.io",
   "helia": "ipfs.github.io/helia",
   "hell": "hell-js.github.io",
   "hello": "hello-js-org.github.io", // noCF? (don´t add this in a new PR)
