755e85d8ff6cfb38c7861c445ed57a41b9ce3c39
tombills <38777145+tombills@users.noreply.github.com>
2026-07-11 15:02:25 +0800

    Add new CNAME entry for 'aicn'

--- cnames_active.js
+++ cnames_active.js
@@ -129,6 +129,7 @@ var cnames_active = {
   "ahnotification": "ahmadhayyan.github.io/ahnotification.js",
   "ahooks": "ahooks.surge.sh",
   "ahooks-v2": "ahooks-v2.surge.sh",
+  "aicn": "tombills.github.io",
   "aider": "tjz101.github.io/aider-js-pages",
   "aiga": "cname.vercel-dns.com", // noCF
   "ailcr": "cname.vercel-dns.com", // noCF
