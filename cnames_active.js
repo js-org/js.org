1566cbee1ded4f4963f1754e867249bb441bcb43
Raphael Sparda <raphasparda@gmail.com>
2026-07-11 11:20:50 -0300

    Domínio adicionado

--- cnames_active.js
+++ cnames_active.js
@@ -215,6 +215,7 @@ var cnames_active = {
   "aoi": "aoijs.github.io/website",
   "aom": "scarych.github.io/aom",
   "aomex": "aomex.github.io",
+  "aowiki": "raphasparda.github.io/AOG",
   "api.just": "js-just.github.io/raw-api", // noCF
   "apicluster": "ramsunvtech.github.io/apicluster", // noCF? (don´t add this in a new PR)
   "apilyertia": "apilyertia.github.io",
