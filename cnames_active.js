ab8ef3cefab99f62bd0030c7349eb0208ec5ae4b
ThienNah <nhan110615@gmail.com>
2026-07-15 02:19:50 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1,4 +1,3 @@
-// Bấm sửa file cnames_active.js rồi dán đoạn này vào khu vực chữ D nha ông bạn
   "diaspora": "diaspora.github.io",
   "dice": "idreams.github.io/dice",
   "diencosongtoan": "diencosongtoan.netlify.app",
