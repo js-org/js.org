a5b71a02cb01aea35064552c5e262b33d795bb95
vakandi <wael.bousfira@protonmail.com>
2026-07-17 23:15:21 +0100

    Add teleorbit.js.org for TeleOrBit Telegram analytics platform
    - teleorbit.js.org → teleorbit-homepage.vercel.app
    
    Next.js-powered Telegram channel analytics and similar-channel discovery tool. Domain added to Vercel for DNS.

--- cnames_active.js
+++ cnames_active.js
@@ -3378,6 +3378,7 @@ var cnames_active = {
   "telaviv": "dustin-h.github.io/telaviv", // noCF? (don´t add this in a new PR)
   "telegraf": "telegraf.github.io/telegraf", // noCF? (don´t add this in a new PR)
   "telegram": "yuvapoojary.github.io/telegram.js-website",
+  "teleorbit": "teleorbit-homepage.vercel.app", // noCF
   "teletype": "keyvank.github.io/teletype.js",
   "temme": "shinima.github.io/temme",
   "template-pro": "template-pro.github.io",
