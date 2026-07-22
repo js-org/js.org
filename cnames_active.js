55d4d1cd4b813254364ce52025663601b576a256
topwatereeee-pixel <topwatereeee@gmail.com>
2026-07-22 14:45:48 -0500

    Fix 'codeforge' URL in cnames_active.js
    Updated the URL for 'codeforge' entry in cnames_active.

--- cnames_active.js
+++ cnames_active.js
@@ -39,7 +39,7 @@
 
 var cnames_active = {
   "": "js-org.github.io",
-  "codeforge": "https://code-forge-coding.netlify.app/#/editor",
+  "codeforge": "code-forge-coding.netlify.app",
   "01mz": "01mz.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
   "101": "7anshuai.github.io/js101", // noCF? (don´t add this in a new PR)
