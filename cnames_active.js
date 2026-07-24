b25726dd161d4f8d07e559870b8ab1869b011dba
Pedro Henrique <pedrohcsantos.pessoal@gmail.com>
2026-07-24 20:25:08 -0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3916,7 +3916,8 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zylog": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "lykos": "peagasss.github.io/lykostorneios"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
