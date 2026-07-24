b001834cc95e3a8feec48c596ff92d5c74feb2ce
Krunalkumar Shah <einstein1410@gmail.com>
2026-07-24 23:11:40 +0530

    added krunalkumar site

--- cnames_active.js
+++ cnames_active.js
@@ -1761,7 +1761,6 @@ var cnames_active = {
   "kloner": "fivefifteen.github.io/kloner",
   "kmeans": "jeff-tian.github.io/k-means",
   "knc": "chaituknag.github.io",
-  "knf": "officialkrunalkumar.github.io/Karuna-Nidhi-Foundation",
   "ko": "ko25july.github.io/ko.js.org",
   "ko.mobx": "cname.vercel-dns.com", // noCF
   "ko.redux": "deminoth.github.io/redux", // noCF
