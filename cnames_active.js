82fffee050f2c5e6418ceb529ef50c4cf1c956b0
Average Rumia <158121572+IblisseAbstractArt@users.noreply.github.com>
2026-07-24 12:56:59 -0500

    Fix casing in mesopota URL

--- cnames_active.js
+++ cnames_active.js
@@ -2076,7 +2076,7 @@ var cnames_active = {
   "mern": "iammohaiminul.github.io/MERNjs",
   "mert": "mert574.github.io",
   "mesh": "crcn.github.io/mesh.js.org", // noCF? (don´t add this in a new PR)
-  "mesopota": "lblisseAbstractArt.github.io/Meiso-potage-flash-collection",
+  "mesopota": "lblisseabstractart.github.io/Meiso-potage-flash-collection",
   "meshesha": "meshesha.github.io",
   "metadata": "oknosoft.github.io/metadata.js",
   "metapulse": "noyjoyluckclub.github.io/metapulse",
