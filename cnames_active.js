e0b6de42df6422dab6d9ad5b7d8b492fd06e9acc
WeiU <sunrise@weiu.top>
2026-07-24 16:53:23 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3914,7 +3914,8 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zylog": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "weiu": "js.weiu.workers.dev"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
