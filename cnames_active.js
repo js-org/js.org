3ef5c5b25a7d23f53aeaab4a82daef927361e278
robertkodes <kodesprobert@gmail.com>
2026-07-24 21:18:19 +0300

    Add aelnetwork.js.org -> ael-network.onrender.com

--- cnames_active.js
+++ cnames_active.js
@@ -108,6 +108,7 @@ var cnames_active = {
   "adon988": "adon988.github.io",
   "adv": "advjs.github.io",
   "advancedrpc": "advancedrpc.github.io",
+  "aelnetwork": "ael-network.onrender.com", // noCF
   "aerogel": "noeldemartin.github.io/aerogel",
   "aesthetic": "esthetic-docs.netlify.app",
   "affiliate": "russellsteadman.github.io/affiliate",
