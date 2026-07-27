94365083f7742ed58628deb4d1b2e631cb24c0be
DEVsine <devteam@dobybot.com>
2026-07-27 19:14:14 +0700

    Add vocabrunner.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -3695,6 +3695,7 @@ var cnames_active = {
   "vncz": "xvincentx.github.io/vncz",
   "vnpay": "cname.vercel-dns.com", // noCF
   "vocab": "battleofplassey.github.io/ministry-of-vocabulary",
+  "vocabrunner": "devsine.github.io/vocab-runner",
   "volantis": "volantis-x.github.io/community",
   "volantis-x": "volantis-x.github.io",
   "voloshins": "voloshins.github.io", // noCF? (don´t add this in a new PR)
