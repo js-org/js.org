fce49350f6139d00d4140dec83d27150c5f85ece
Onur Celep <8829545+onurcelep@users.noreply.github.com>
2026-07-28 15:17:16 +0200

    Add etude.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1106,6 +1106,7 @@ var cnames_active = {
   "eta": "alias.zeit.co", // noCF
   "eth": "eth-js.github.io/eth-dev-tools",
   "ethavatar": "filips123.github.io/EthAvatar.JS",
+  "etude": "onurcelep.github.io/etude",
   "euc": "wnda.github.io/euc",
   "euclid": "anandthakker.github.io/euclid", // noCF? (don´t add this in a new PR)
   "eva": "eva-engine.github.io",
