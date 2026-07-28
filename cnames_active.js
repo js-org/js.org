4ba594340b21fcf669c9a3c9ab66836b536c3a8c
TechPhantom34 <Qwerty19741971@outlook.com>
2026-07-28 20:38:43 +0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3258,6 +3258,7 @@ var cnames_active = {
   "status": "mattipv4.github.io/status-codes",
   "status-hx": "status-hx-js-org.dns.huangxin.org", // noCF
   "stax": "stax.serv00.net",
+  "stack": "techphantom34.github.io/Stack",
   "steemsites": "yhozen.github.io/steemsites",
   "stego": "zeruniverse.github.io/CryptoStego", // noCF? (don´t add this in a new PR)
   "stein": "cname.vercel-dns.com", // noCF
