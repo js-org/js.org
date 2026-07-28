b88a29f77282303972e11240218601b53f5baecf
elkworksproject-sketch <elkworksproject@gmail.com>
2026-07-28 16:52:13 +0200

    add taskflow subdomain

--- cnames_active.js
+++ cnames_active.js
@@ -3372,6 +3372,7 @@ var cnames_active = {
   "tarikh": "cname.vercel-dns.com", // noCF
   "taro": "nervjs.github.io/taro-website",
   "tart": "tart.github.io/tartJS", // noCF? (don´t add this in a new PR)
+  "taskflow": "cname.vercel-dns.com",
   "tauber": "ranjithrd.github.io/tauber",
   "taufik-nurrohman": "taufik-nurrohman.github.io",
   "taxi": "craftedbygc.github.io/taxi",
