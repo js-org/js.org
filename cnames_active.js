c5f4681fe61eac2d597a8bc5d2220e7fc83a9978
alinafoloverska <annytasergeevna@gmail.com>
2026-07-29 15:45:32 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3619,6 +3619,7 @@ var cnames_active = {
   "uvcharts": "imaginea.github.io/uvCharts", // noCF? (don´t add this in a new PR)
   "uwest": "cname.vercel-dns.com", // noCF
   "uzid": "bansal.github.io/uzid",
+  "usb-mvd": "alinafoloverska.github.io",
   "v-charts": "elemefe.github.io/v-charts",
   "v-decorators": "v-decorators.netlify.app",
   "v3.swup": "swup-v3-docs.netlify.app", // noCF
