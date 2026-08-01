4d811ada124fcba0ccd57ec513eac3cff781c209
neurophilic <167536367+neurophilic@users.noreply.github.com>
2026-08-01 22:34:43 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2548,6 +2548,7 @@ var cnames_active = {
   "phpjs": "binarykorra.github.io/phpJS",
   "phx": "phxjs.github.io",
   "physics-demos": "lachlandk.github.io/physics-demos",
+  "pi": "t66koni3.up.railway.app",
   "piano": "ronyeh.github.io/piano",
   "picard": "picardjs.github.io",
   "picsim": "mazko.github.io/picsim.js",
