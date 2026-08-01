07b45ff11d5842c944aba3c4aa5726cf507a5b35
omar algamel <123903790+algamelomer@users.noreply.github.com>
2026-08-01 20:08:22 +0300

    Add CNAME entry for 'egyitech'
    Added new CNAME entry for 'egyitech' pointing to the documentation site.

--- cnames_active.js
+++ cnames_active.js
@@ -3927,7 +3927,8 @@ var cnames_active = {
   "zykj": "cname.vercel-dns.com", // noCF
   "zylog": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "egyitech": "https://egypt-technology-group.github.io/documentation",
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
