e45b2632c33e6ce1c0d526fdfffac9c50691e072
marwanjouiet0-code <marwanjouiet0@gmail.com>
2026-08-02 19:55:37 +0200

    Add krypt entry for Vercel DNS

--- cnames_active.js
+++ cnames_active.js
@@ -1796,6 +1796,7 @@ var cnames_active = {
   "krdscn-ui": "krdscn-ui.github.io/ui",
   "kremling": "canopytax.github.io/kremling.js.org",
   "krida": "elvisdsz.github.io/krida",
+  "krypt": "://vercel-dns.com", // noCF
   "kshitij": "kshitij98.github.io",
   "kst": "lucaelin.github.io/KST", // noCF
   "ktm": "developers-nepal.github.io/ktmjs",
