3ce07996e8c5956a04722b6dc5268c89cc18b456
AnderC18 <anderson131207@gmail.com>
2026-08-02 23:08:24 -0400

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2243,7 +2243,7 @@ var cnames_active = {
   "myurl": "marvnet.github.io/myurl",
   "mzaini30": "mzaini30.github.io",
   "mzplayer": "prince3661.github.io/Mzplayer",
-  "n": "cyb.github.io/n",
+  "n": "nexvault": "anderc18.github.io/NexVault",
   "nabin": "nabin6246.github.io",
   "nacl-check": "magnusmarx.github.io/NaCL",
   "naja": "naja-js.github.io/naja",
