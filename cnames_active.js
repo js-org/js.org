e1e79e6de20d2ac5569b3dd3f735014f0e6287ee
Pranav Somalinga <somalip@icloud.com>
2026-08-03 21:40:39 +0530

    Add dailyNPM to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -782,6 +782,7 @@ var cnames_active = {
   "dawn": "dawnelixir.github.io/dawn.js.org",
   "dawood": "mohamedawood.github.io/Dawood",
   "day": "dayjs.github.io/website",
+  "daily": "somalip.github.io/dailyNPM",
   "daybreak": "mokunshao.github.io/daybreak",
   "daysfromnow": "pedrokost.github.io/daysfromnow", // noCF? (don´t add this in a new PR)
   "dbar": "dbarjs.github.io",
