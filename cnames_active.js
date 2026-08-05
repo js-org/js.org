fd2c366cb8912d5466d6bccbc6337a1f9d24c0b7
Pranav Somalinga <somalip@icloud.com>
2026-08-05 16:19:00 +0530

    Update daily NPM URL to new Vercel deployment

--- cnames_active.js
+++ cnames_active.js
@@ -782,7 +782,7 @@ var cnames_active = {
   "dawn": "dawnelixir.github.io/dawn.js.org",
   "dawood": "mohamedawood.github.io/Dawood",
   "day": "dayjs.github.io/website",
-  "daily": "somalip.github.io/dailyNPM",
+  "daily": "daily-npm.vercel.app",
   "daybreak": "mokunshao.github.io/daybreak",
   "daysfromnow": "pedrokost.github.io/daysfromnow", // noCF? (don´t add this in a new PR)
   "dbar": "dbarjs.github.io",
