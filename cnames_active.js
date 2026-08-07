a511e956de29bea7703d3878a0fa47179a7504bf
sarphitegamestudio <sarphitegamestudio@gmail.com>
2026-08-07 19:32:31 +0530

    Add sarphitegamestudio to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2976,6 +2976,7 @@ var cnames_active = {
   "sanddance": "danmarshall.github.io/SandDance",
   "sandeepkhandewale": "learner1024.github.io",
   "sandmine": "spreehertz.github.io/Sandmine.js",
+  "sarphitegamestudio": "sarphitegamestudio.github.io/sarphite-game-studios",
   "saso": "huruji.github.io/saso",
   "sass": "medialize.github.io/playground.sass.js", // noCF? (don´t add this in a new PR)
   "satori": "satorijs.github.io/docs",
