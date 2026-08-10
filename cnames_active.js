6845b1e4670e7e83c327231cf6f35e0da8cbac29
Mizar Nevelli <46935430+MizarNevelli@users.noreply.github.com>
2026-08-10 16:42:42 +0800

    Add cname for mizarnevelli

--- cnames_active.js
+++ cnames_active.js
@@ -2144,6 +2144,7 @@ var cnames_active = {
   "mitu": "godow.github.io/mitu",
   "mixery": "nahkd123.github.io/Mixery",
   "mixi2": "otnc.github.io/mixi2-js",
+  "mizarnevelli": "cname.vercel-dns.com",
   "mjy": "majianyu2007.github.io",
   "mm": "1ocalhost.github.io/mm",
   "mmzdupz": "mmzdupz.github.io",
