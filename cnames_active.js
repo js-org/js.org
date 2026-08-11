8ae2b0c4c76986b1c5eb19a9e9a45a511daf62a5
Bahadır Yüksel <bahadir.yuksel@stu.rumeli.edu.tr>
2026-08-11 18:36:25 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1063,6 +1063,7 @@ var cnames_active = {
   "empathy": "the94air.github.io/empathy-docs",
   "emre": "emrekara37.github.io",
   "emulisp": "grahack.github.io/EmuLisp", // noCF? (don´t add this in a new PR)
+  "emyapps": "Bahadir-Yuksel.github.io"
   "emz": "emzjs.github.io",
   "enclave": "eanplatter.github.io/enclave", // noCF? (don´t add this in a new PR)
   "encrypt-rsa": "miladezzat.github.io/encrypt-rsa",
