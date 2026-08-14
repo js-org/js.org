4b27ec543f5f1b9bec449fd7ee8eb0b90e2a5f3a
Lio Schimanko <229825009+schimanko@users.noreply.github.com>
2026-08-14 16:15:04 -0300

    Reorder portfolito entry in cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2617,8 +2617,8 @@ var cnames_active = {
   "portafoliorufino": "rufino-hdez.github.io/portafolio",
   "portainer-update": "flweber.github.io/portainer-stack-updater",
   "porter": "ahopkins.github.io/porterjs", // noCF? (don´t add this in a new PR)
-  "portfolito": "sleelin.github.io/portfolito",
   "portfoliable": "portfoliablejs.github.io/portfoliable",
+  "portfolito": "sleelin.github.io/portfolito",
   "portpilot": "portpilot.pages.dev",
   "poru": "cname.vercel-dns.com", // noCF
   "post-type-discovery": "twozeroone.github.io/post-type-discovery",
