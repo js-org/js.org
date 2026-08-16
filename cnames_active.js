233312b5de0c96a71aba101102a556ca48396648
jimmykcjob <79395777+jimmykcjob@users.noreply.github.com>
2026-08-16 15:30:52 +0800

    Fix formatting for fandom entry in cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1158,7 +1158,7 @@ var cnames_active = {
   "fanbook-mp-sdk": "fanbook-mp-sdk.netlify.app",
   "fancy-pants": "fancy-pants.tritarget.org",
   "fandb-mall": "cname.vercel-dns.com", // noCF
-  "fandom": "fandomjs.netlify.app", // noCF  
+  "fandom": "fandomjs.netlify.app", // noCF
   "fantas": "fantastiser.github.io",
   "farfetch": "websitebeaver.github.io/far-fetch",
   "farfetchd": "achannarasappa.github.io/farfetchd", // noCF? (don´t add this in a new PR)
