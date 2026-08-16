5fe413b2857fca9a8db2959b2028ede527e4d44e
Yangchengen-hub <3565583431@qq.com>
2026-08-16 13:09:58 +0800

    jifeng: add jifeng.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1657,6 +1657,7 @@ var cnames_active = {
   "jetflow": "jetflowjs.pages.dev",
   "jets": "nexts.github.io/Jets.js",
   "jetup": "aldhosutra.github.io/jetup",
+  "jifeng": "yangchengen-hub.github.io/jifeng",
   "jimple": "fjorgemota.github.io/jimple",
   "jinada": "redsplit.github.io/jinada",
   "jjlc": "k-yak.github.io/JJLC", // noCF? (don´t add this in a new PR)
