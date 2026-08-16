9da5ce87d76a283cf70085865f7172f556de94ea
jimmykcjob <79395777+jimmykcjob@users.noreply.github.com>
2026-08-16 15:26:00 +0800

    Fixed order of fandb-mall and added no CF

--- cnames_active.js
+++ cnames_active.js
@@ -1157,8 +1157,8 @@ var cnames_active = {
   "fanbook-api-sdk": "fanbook-api-node-sdk.netlify.app",
   "fanbook-mp-sdk": "fanbook-mp-sdk.netlify.app",
   "fancy-pants": "fancy-pants.tritarget.org",
-  "fandom": "fandomjs.netlify.app", // noCF
-  "fandb-mall":"cname.vercel-dns.com",
+  "fandb-mall": "cname.vercel-dns.com", // noCF
+  "fandom": "fandomjs.netlify.app", // noCF  
   "fantas": "fantastiser.github.io",
   "farfetch": "websitebeaver.github.io/far-fetch",
   "farfetchd": "achannarasappa.github.io/farfetchd", // noCF? (don´t add this in a new PR)
