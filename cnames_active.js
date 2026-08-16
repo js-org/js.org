ee4dbe2a24b9ea995f9ed4784b2bf764b117d904
jimmykcjob <79395777+jimmykcjob@users.noreply.github.com>
2026-08-16 15:03:35 +0800

    Add cname for fandb-mall

--- cnames_active.js
+++ cnames_active.js
@@ -1158,6 +1158,7 @@ var cnames_active = {
   "fanbook-mp-sdk": "fanbook-mp-sdk.netlify.app",
   "fancy-pants": "fancy-pants.tritarget.org",
   "fandom": "fandomjs.netlify.app", // noCF
+  "fandb-mall":"cname.vercel-dns.com",
   "fantas": "fantastiser.github.io",
   "farfetch": "websitebeaver.github.io/far-fetch",
   "farfetchd": "achannarasappa.github.io/farfetchd", // noCF? (don´t add this in a new PR)
