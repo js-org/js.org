6789425481834cdb48a9b9ddcf2c823578e6753a
ilyoukhinsndjjddj <ilyoukhinsndjjddj@users.noreply.github.com>
2026-08-18 05:33:46 +0000

    Fix target to ilyoukhinsndjjddj.github.io

--- cnames_active.js
+++ cnames_active.js
@@ -3043,7 +3043,7 @@ var cnames_active = {
   "semantic-api": "semantic-api.github.io/docs",
   "semo": "semojs.github.io/semo",
   "senil": "mrfarhad.github.io/senilwebsite",
-  "seosee": "seosee.github.io",
+  "seosee": "ilyoukhinsndjjddj.github.io",
   "seoul": "seouljs.github.io/seoul.js.org",
   "sequel": "sequeljs.github.io",
   "sequelize-guard": "cname.vercel-dns.com", // noCF
