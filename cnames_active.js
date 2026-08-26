8b337199ba44b182235aad647d31d975dac52a15
Lattymoy <98724681+Lattymoy@users.noreply.github.com>
2026-08-26 18:38:00 -0400

    Add daggerfall.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -758,6 +758,7 @@ var cnames_active = {
   "cyris": "icyris.github.io",
   "d-patterns": "alias.zeit.co", // noCF
   "d4": "joelburget.github.io/d4",
+  "daggerfall": "lattymoy.github.io/project-dagger",
   "daily.tmr": "tmr-daily.netlify.app", // noCF
   "daisy": "warengonzaga.github.io/daisy.js",
   "daksh0225": "daksh0225.github.io",
