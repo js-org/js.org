dc72c63359669f8932c9e4eebd6deb465c0ee9b7
Nguyễn Việt Anh <147018069+VietAnh-2481@users.noreply.github.com>
2026-08-27 11:22:02 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3774,6 +3774,7 @@ var cnames_active = {
   "vulkava": "hosting.gitbook.io", // noCF
   "vxt": "noah227.github.io/vxt-docs",
   "vxv": "vxv.netlify.app",
+  "vietanhnguyen-2481": "vietanh-2481.github.io/VietAnh2481-deploy-demo",
   "w2d": "bananahackers.github.io/w2d",
   "w4ctech": "w4ctech.github.io",
   "w4j1e": "w4j1e.github.io",
