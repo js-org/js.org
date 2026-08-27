bb0dc31fdf3bce68811dbf24520b98f5d73f2a96
Kang0234 <18980762558@163.com>
2026-08-27 23:37:48 +0800

    Switch kmenta entry to CNAME (NS delegation is not supported)

--- cnames_active.js
+++ cnames_active.js
@@ -1777,7 +1777,7 @@ var cnames_active = {
   "klepto": "mwjaworski.github.io/klepto",
   "kloner": "fivefifteen.github.io/kloner",
   "kmeans": "jeff-tian.github.io/k-means",
-  "kmenta": ["olivia.ns.cloudflare.com", "rommy.ns.cloudflare.com"],
+  "kmenta": "kc.kang0234.top",
   "knc": "chaituknag.github.io",
   "ko": "ko25july.github.io/ko.js.org",
   "ko.mobx": "cname.vercel-dns.com", // noCF
