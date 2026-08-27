99e9dc40b7fe0f2fc2d4e8681704507908a23a49
Olyster <muhammetemindadak676@gmail.com>
2026-08-27 14:08:05 +0300

    Clarify CNAME entry for 'mamicikle'
    Added comment to clarify the CNAME entry for 'mamicikle'.

--- cnames_active.js
+++ cnames_active.js
@@ -1999,7 +1999,7 @@ var cnames_active = {
   "maker": "microsoft.github.io/maker.js",
   "makes": "makesjs.github.io",
   "mali": "malijs.github.io",
-  "mamicikle": "cname.vercel-dns.com",
+  "mamicikle": "cname.vercel-dns.com", // noCF
   "mangascrape": "tzurs11.github.io/MangaScrape",
   "manic": "manicjs.github.io",
   "mansiart": "mansiart.github.io/mansiart",
