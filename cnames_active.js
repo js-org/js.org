14615e4e0cca6ef4d6d3851b456f068748feec36
Jasoen110207 <Bintangabrisamanaf@gmail.com>
2026-08-27 16:58:37 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -339,6 +339,7 @@ var cnames_active = {
   "bane": "kemosaf.github.io/bane",
   "baoshuo": "renbaoshuo.github.io/baoshuo.css",
   "bara": "barajs.github.io/bara",
+  "barka": "mybarka.vercel.app",
   "barba": "barbajs.github.io/barba",
   "barbagrigia": "barbagrigia.github.io",
   "bard-ai": "evanzhoudev.github.io/bard-ai",
