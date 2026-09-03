f3204e1967f4b9237a173aa700639e11fbe7aa04
dangreen <danon0404@gmail.com>
2026-09-03 15:45:07 +0400

    Add srcset.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -3174,6 +3174,7 @@ var cnames_active = {
   "squircle": "cname.vercel-dns.com", // noCF
   "squirrelly": "squirrellyjs.netlify.app", // noCF
   "sr": "samrobbins85.github.io",
+  "srcset": "trigensoftware.github.io/srcset",
   "sri": "jackub.github.io/subresource-integrity-fallback",
   "sri-maker": "rohit-chouhan.github.io/sri-maker",
   "sri-shasum": "imcotton.github.io/sri", // noCF
