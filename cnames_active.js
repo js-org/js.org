d58d8cd765f2de2b123d13f3d410fe939412b1e3
mazo <gr7ytqht6y@privaterelay.appleid.com>
2026-09-13 00:28:06 +0300

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -108,6 +108,7 @@ var cnames_active = {
   "adon988": "adon988.github.io",
   "adv": "advjs.github.io",
   "advancedrpc": "advancedrpc.github.io",
+  "aegis": "gr7ytqht6y-crypto.github.io",
   "aerogel": "noeldemartin.github.io/aerogel",
   "aesthetic": "esthetic-docs.netlify.app",
   "affiliate": "russellsteadman.github.io/affiliate",
