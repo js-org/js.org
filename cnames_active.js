beb46241b0dcb991c12369fc9c668f5fb0e773d5
stefaceriani <stefano.ceriani@blu.it>
2026-09-14 14:22:50 +0200

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -550,7 +550,7 @@ var cnames_active = {
   "christopher": "marekkobida.github.io/christopher",
   "chronos": "espinielli.github.io/chronos", // noCF? (don´t add this in a new PR)
   "chronosis": "jack-weilage.github.io/chronosis",
-  "chromashift": "chromashift-web.vercel.app",
+  "chromashift": "cname.vercel-dns.com", // noCF
   "citation": "citation-js.github.io/site",
   "citeapa": "natsuozawa.github.io/citeapa",
   "city": "city.github.io",
