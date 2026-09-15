2b39a72716bdf4639cae84a4710960718f2667b5
Akshay <rekulaakshay@gmail.com>
2026-09-15 21:07:15 +0530

    Add hostelmate.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -1469,6 +1469,7 @@ var cnames_active = {
   "hooloo": "hooloo.github.io", // noCF? (don´t add this in a new PR)
   "hoshii": "reinhello.github.io/hoshii-docs",
   "hoshimi": "cname.vercel-dns.com", // noCF
+  "hostelmate":"axhayco.github.io/hostelmate",
   "houp": "houpjs.github.io/houp-docs",
   "hours": "cname.vercel-dns.com", // noCF
   "how-to-mithril": "stephanhoyer.github.io/how-to-mithril",
