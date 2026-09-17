59cc2a1e83ff037d5f25303fe28ddbcb4d43892c
Pedro Povoas <131079352+braindeadpt@users.noreply.github.com>
2026-09-17 17:33:59 +0100

    Add aocentimo.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -212,6 +212,7 @@ var cnames_active = {
   "antropodigital": "antropodigital.github.io/CirculodeEstudio",
   "antwar": "antwarjs.github.io/antwar",
   "anukul": "anukul.github.io",
+  "aocentimo": "braindeadpt.github.io/bruto",
   "aoi": "aoijs.github.io/website",
   "aom": "scarych.github.io/aom",
   "aomex": "aomex.github.io",
