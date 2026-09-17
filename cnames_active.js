d464e9e4883484bb4f7896c3c3d730b4ac72fbc4
vivek-R <vivekmallukl@gmail.com>
2026-09-17 22:29:54 +0530

    Add cname for cisf-quiz subdomain

--- cnames_active.js
+++ cnames_active.js
@@ -3877,6 +3877,7 @@ var cnames_active = {
   "zylog": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "cisf-quiz": "vivek-r-kl.github.io",
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
