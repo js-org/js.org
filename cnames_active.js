fa3b6ccd043c8b7e7bacde3995338f8424b2b3e9
VortexElite8845 <hanyi.yang@kinkaid.org>
2026-09-22 22:10:59 -0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1949,6 +1949,7 @@ var cnames_active = {
   "magickwand": "mmomtchev.github.io/magickwand.js",
   "magidoc": "magidoc-org.github.io/magidoc",
   "magnet": "magnetjs.github.io/Magnet", // noCF? (don´t add this in a new PR)
+  "map2model": "cname.vercel-dns.com",
   "mahmoud": "mahmoud-sagharjoughi.github.io/mahmoud",
   "maid": "maidjs.github.io",
   "mailbox": "vsdparibas.github.io/mailboxjs-doc",
