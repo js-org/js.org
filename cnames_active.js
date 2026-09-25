1702eccc65a77e78dee1deceb6dd0a67140d53cd
NovaLang Dev <ahmet.ensar6606@hotmail.com>
2026-09-25 11:15:50 +0300

    Add nova-lang entry to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2329,6 +2329,7 @@ var cnames_active = {
   "notion-cms": "cname.vercel-dns.com", // noCF
   "nougat": "nougatlang.github.io",
   "novasheets": "novasheets.netlify.app",
+  "nova-lang": "novalang-dev.github.io/NovaLang-Developer-Hub",
   "now": "leodog896.github.io/jsnow",
   "noypi": "noypi.alwaysdata.net",
   "npkill": "voidcosmos.github.io/npkill-web",
