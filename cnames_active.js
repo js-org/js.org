1ed6bd26cd83047baf9f2312a6e789ccb47e571d
sqezx3d <sqezx3d@gmail.com>
2026-09-25 15:18:07 +0530

    Rename 'salvattore' to 'salvatore' in cnames

--- cnames_active.js
+++ cnames_active.js
@@ -2925,9 +2925,9 @@ var cnames_active = {
   "sakiv": "sakiv.github.io",
   "salary": "whz11.github.io/salarytools",
   "salvacam": "salvacam.github.io",
-  "salvattore": "rnmp.github.io/salvattore",
+  "salvatore": "rnmp.github.io/salvatore",
   "sqezx3d": "sqezx3d.github.io",
-  "sam": "jdubray.github.io/sam", // noCF? (don´t add this in a new PR)
+  "sam": "jdubray.github.io/sam", // noCF? (don't add this in a new PR)
   "samathgen": "sadykhzadeh.github.io/samathgen",
   "samengine": "cname.vercel-dns.com", // noCF
   "samlify": "tngan.github.io/samlify",
