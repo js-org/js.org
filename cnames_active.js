f16ba4387380383870e5f18ca8a2ef1be1a07e34
sqezx3d <sqezx3d@gmail.com>
2026-09-25 18:25:32 +0530

    Add new CNAME entry for sqezx3d

--- cnames_active.js
+++ cnames_active.js
@@ -3064,6 +3064,7 @@ var cnames_active = {
   "simplecounter": "tomkiernan120.github.io/simplecounter",
   "simplecrypto": "danang-id.github.io/simple-crypto-js",
   "simplelightbox": "andreknieriem.github.io/simplelightbox",
+  "sqezx3d": "sqezx3d.github.io",
   "simpler-state": "arnelenero.github.io/simpler-state",
   "simples": "micnic.github.io/simples",
   "simplesnake": "aktindo.github.io/Simple_Snake_Game",
