9bdb52890be0fc11491759a44ed82ea85b394db3
sqezx3d <sqezx3d@gmail.com>
2026-09-26 18:03:19 +0530

    Add new CNAME entry for sqezx3d

--- cnames_active.js
+++ cnames_active.js
@@ -3180,6 +3180,7 @@ var cnames_active = {
   "spud": "nikop100.github.io/spud",
   "spx": "spxjs.netlify.app",
   "spyter": "cname.vercel-dns.com", // noCF
+  "sqezx3d": "sqezx3d.github.io"
   "sql": "sql-js.github.io/sql.js",
   "sql2struct": "ymlair.github.io/sql2struct",
   "sqlboot": "cname.vercel-dns.com", // noCF
