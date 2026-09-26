ad37b2c12c1b06ddb1186cd21985e5997c0bcfb4
sqezx3d <sqezx3d@gmail.com>
2026-09-26 17:22:46 +0530

    Remove 'sqezx3d' from cnames_active.js
    Removed 'sqezx3d' entry from the cnames list.

--- cnames_active.js
+++ cnames_active.js
@@ -3181,7 +3181,6 @@ var cnames_active = {
   "spud": "nikop100.github.io/spud",
   "spx": "spxjs.netlify.app",
   "spyter": "cname.vercel-dns.com", // noCF
-  "sqezx3d": "sqezx3d.github.io",
   "sql": "sql-js.github.io/sql.js",
   "sql2struct": "ymlair.github.io/sql2struct",
   "sqlboot": "cname.vercel-dns.com", // noCF
